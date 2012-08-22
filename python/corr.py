eb55f2d62962a6ef1a69b389f08ba390ee88f371
Kevin Bandura <kbandura@gmail.com>
2012-08-21 23:22:00 -0400

    getting closer to working

--- python/corr.py
+++ python/corr.py
@@ -57,34 +57,85 @@ corr_kernel = corr_program.corr
 mf = cl.mem_flags
 input_buffer = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=data_block )
 
+#size of one block is 32x32
 s1_blk = 32
+#number of blocks is n(n+1)/2 where n is number of antennas divided by block side size
 n_blk = (N_ANT / s1_blk) * (N_ANT / s1_blk + 1) / 2.
+#output is then number of blocks * block size * 2(real and imaginary)
 zeros = np.zeros(n_blk*(s1_blk*s1_blk)*2, dtype=np.int32)
 
+#create correlation output buffer, zero memory
 corr_buffer = cl.Buffer(ctx, mf.WRITE_ONLY | mf.COPY_HOST_PTR, hostbuf=zeros)
 #corr_buffer = cl.Buffer(ctx, mf.WRITE_ONLY | mf.COPY_HOST_PTR, size=n_blk)???
 
 #too lazy to make this better and actually figure out what I'm writing.  Is calculating
-# the blocks to share kernels efficiently.
+# the blocks to share kernels efficiently. gives the x and y position of a given block.
+#Commented out Keiths way, done below possibly more obvious (no square roots!)
+# global_id_x_map = np.zeros(n_blk, dtype=np.uint)
+# global_id_y_map = np.zeros(n_blk, dtype=np.uint)
+# for i in np.arange(n_blk):
+#     t = int((np.sqrt(1 + 8*(n_blk-i-1))-1)/2)
+#     y = N_ANT/s1_blk-t-1
+#     x = (t+1)*(t+2)/2 + (i - n_blk)+y
+#     global_id_x_map[i] = x
+#     global_id_y_map[i] = y
+#     print t
+#     print x
+#     print y
+
 global_id_x_map = np.zeros(n_blk, dtype=np.uint)
 global_id_y_map = np.zeros(n_blk, dtype=np.uint)
-for i in np.arange(n_blk):
-    t = (np.sqrt(1 + 8*(n_blk-i-1))-1)/2
-    y = N_ANT/s1_blk-t-1
-    x = (t+1)*(t+2)/2 + (i - n_blk)+y
-    global_id_x_map[i] = x
-    global_id_y_map[i] = y
+cont = 0
+blocks_per_side = N_ANT/s1_blk
+for i in np.arange(blocks_per_side):
+    for j in np.arange(i,blocks_per_side):
+        global_id_x_map[cont] = j
+        global_id_y_map[cont] = i
+        cont += 1
 
 id_x_map = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=global_id_x_map)
 id_y_map = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=global_id_y_map)
 
 unpacked = cl.LocalMemory(4*16*4*4)
 
-event = corr_kernel(queue, zeros.shape[::-1], input_buffer, corr_buffer, id_x_map, id_y_map, unpacked )
+count = 5
+for i in range(count):
+    #maybe should be using cl.enqueue_nd_range_kernel(queue, kernel, global_work_size, local_work_size, global_work_offset=None, wait_for=None, g_times_l=True)
+    #to be the same as keiths.  
+    event = corr_kernel(queue, zeros.shape[::-1], input_buffer, corr_buffer, id_x_map, id_y_map, unpacked )
+
 event.wait()
+
+  # int nkern=1000;
+  # unsigned int n_caccum=N_ITER/256;
+  # size_t gws_corr[3]={16,8,n_blk*n_caccum};
+  # size_t lws_corr[3]={16,4,1};
+  # for (int i=0; i<nkern; i++)
+  #   err=clEnqueueNDRangeKernel(queue, corr_kernel, 3, NULL, gws_corr, lws_corr, 0, NULL, NULL);
+  # if (err) printf("Error enqueueing! %i\n",err);
+
+
+# cl_int clEnqueueNDRangeKernel ( cl_command_queue command_queue,
+#     cl_kernel kernel,
+#     cl_uint work_dim,
+#     const size_t *global_work_offset,
+#     const size_t *global_work_size,
+#     const size_t *local_work_size,
+#     cl_uint num_events_in_wait_list,
+#     const cl_event *event_wait_list,
+#     cl_event *event)
+
 
 cl.enqueue_copy(queue, zeros, corr_buffer)
 
 print zeros.shape
 print zeros
+
+realp = zeros[::2]
+imagp = zeros[1::2]
+outgpu = realp + 1.0j*imagp
+
+outgpumat = np.zeros(out.shape)
+
+
 		
