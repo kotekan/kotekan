2da9c9904c0e20706be61e8ac070eab7816121df
Jacob Taylor <jacotay7@orange.lwlab>
2017-08-08 12:16:47 -0400

    Added hsaInput distinction between vdif and non vdif

--- lib/hsa/hsaInputData.cpp
+++ lib/hsa/hsaInputData.cpp
@@ -21,10 +21,14 @@ hsaInputData::~hsaInputData() {
 
 void hsaInputData::apply_config(const uint64_t& fpga_seq) {
     hsaCommand::apply_config(fpga_seq);
+    int header_size = 0;
     _num_elements = config.get_int(unique_name, "num_elements");
     _num_local_freq = config.get_int(unique_name, "num_local_freq");
     _samples_per_data_set = config.get_int(unique_name, "samples_per_data_set");
-    input_frame_len =  (_num_elements * _num_local_freq + 64) * _samples_per_data_set;
+    if(_num_elements <= 2){ //TODO FIND BETTER WORK AROUND FOR DISTINGUISHING VDIF/CHIME INPUT LENGTH
+	header_size = 32;
+    }
+    input_frame_len =  (_num_elements * (_num_local_freq + header_size)) * _samples_per_data_set;
 }
 
 void hsaInputData::wait_on_precondition(int gpu_frame_id)
