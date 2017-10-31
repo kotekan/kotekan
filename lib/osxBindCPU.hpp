a54390bedbef1645cc23d67fe232f35f53b29d72
Kiyoshi Masui <kiyo@physics.ubc.ca>
2017-10-31 14:39:52 -0700

    More functions included in extern C.

--- lib/osxBindCPU.hpp
+++ lib/osxBindCPU.hpp
@@ -14,6 +14,10 @@ typedef struct cpu_set {
   uint32_t    count;
 } cpu_set_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 static inline void
 CPU_ZERO(cpu_set_t *cs) { cs->count = 0; }
 
@@ -25,9 +29,10 @@ CPU_ISSET(int num, cpu_set_t *cs) { return (cs->count & (1 << num)); }
 
 int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set);
 
-#ifdef __cplusplus
-extern "C"
-#endif
 int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t *cpu_set);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
