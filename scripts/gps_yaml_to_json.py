3f92a2d20ee7bc278e7807028eff15767b774744
Andre Renard <andre@renard.io>
2017-11-29 17:24:12 -0500

    Add initial support for GPS timing from ch_master

---
+++ scripts/gps_yaml_to_json.py
@@ -0,0 +1,17 @@
+import yaml, json, sys
+import requests
+
+with open(sys.argv[1], 'r') as stream:
+    try:
+        config_json = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        sys.stderr.write(exc)
+
+try:
+    gps_request = requests.get('http://carillon.chime:54321/get-frame-time')
+    config_json['gps_time'] = gps_request.json()
+except requests.exceptions.RequestException as rex:
+    config_json['gps_time'] = {}
+    config_json['gps_time']['error'] = rex
+
+sys.stdout.write(json.dumps(config_json))
