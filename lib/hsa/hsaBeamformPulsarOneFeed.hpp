715567a7770a35d241951dc96a8149848a7bcf1a
Cherry Ng <cherrywyng@gmail.com>
2018-01-17 12:19:57 -0500

    Pulsar bf (One feed)

---
+++ lib/hsa/hsaBeamformPulsarOneFeed.hpp
@@ -0,0 +1,35 @@
+#ifndef HSA_BEAMFORM_PULSAR_ONE_FEED_H
+#define HSA_BEAMFORM_PULSAR_ONE_FEED_H
+
+#include "hsaCommand.hpp"
+
+class hsaBeamformPulsarOneFeed: public hsaCommand
+{
+public:
+    hsaBeamformPulsarOneFeed(const string &kernel_name, const string &kernel_file_name,
+                        hsaDeviceInterface &device, Config &config,
+                        bufferContainer &host_buffers,
+                        const string &unique_name);
+
+    virtual ~hsaBeamformPulsarOneFeed();
+
+    void apply_config(const uint64_t& fpga_seq) override;
+
+    hsa_signal_t execute(int gpu_frame_id, const uint64_t& fpga_seq,
+                         hsa_signal_t precede_signal) override;
+
+private:
+    int32_t input_frame_len;
+    int32_t output_frame_len;
+    int32_t phase_len;
+
+    float * host_phase;
+
+    int32_t _num_elements;
+    int32_t _num_pulsar;
+    int32_t _num_pol;
+    int32_t _one_feed;
+    int32_t _samples_per_data_set;
+};
+
+#endif
--- lib/hsa/hsaCommandFactory.cpp
+++ lib/hsa/hsaCommandFactory.cpp
@@ -16,6 +16,7 @@
 #include "hsaBarrier.hpp"
 #include "hsaBeamformKernel.hpp"
 #include "hsaBeamformPulsar.hpp"
+#include "hsaBeamformPulsarOneFeed.hpp"
 #include "hsaPulsarUpdatePhase.hpp"
 #include "hsaBeamformReorder.hpp"
 #include "hsaBeamformTranspose.hpp"
@@ -73,6 +74,10 @@ hsaCommandFactory::hsaCommandFactory(Config& config_,
                     device, config, host_buffers, unique_name));
         } else if (commands[i]["name"] == "hsa_beamform_pulsar") {
             list_commands.push_back(new hsaBeamformPulsar("pulsarbf",
+                    commands[i]["kernel"].get<string>(),
+                    device, config, host_buffers, unique_name));
+        } else if (commands[i]["name"] == "hsa_beamform_pulsar_one_feed") {
+            list_commands.push_back(new hsaBeamformPulsarOneFeed("pulsarbf",
                     commands[i]["kernel"].get<string>(),
                     device, config, host_buffers, unique_name));
         } else if (commands[i]["name"] == "hsa_pulsar_update_phase") {
