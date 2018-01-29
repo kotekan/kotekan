34d804e90c84db2af6b426e120b3b6d4840e7c58
Jacob Taylor <jacob.taylor@mail.utoronto.ca>
2018-01-29 11:32:31 -0500

    Reverting to originals

--- lib/buffer.h
+++ lib/buffer.h
@@ -129,24 +129,14 @@ void mark_frame_full(struct Buffer * buf, const char * producer_name, const int 
  *  @param [in] buf The buffer object
  *  @param [in] ID The id of the frame to mark as empty.
  */
-
-
 void mark_frame_empty(struct Buffer* buf, const char * consumer_name, const int ID);
 
-/**
- * Blocks until the frame requested by frame_id is empty.
- *
- * This blocking function will return only when the frame_id request is marked
- * as empty internally, or the function @c send_shutdown_signal() is called, which
- * causes the function to return a @c NULL pointer.
- *
- * @param[in] buf The buffer object
- * @param[in] producer_name The name of the registered producer requesting the frame_id
- * @param[in] frame_id The id of the frame wait for.
- * @returns A pointer to the frame, or NULL if the buffer is shutting down.
- * @warning You may only make this call once per registered producer.
+/** @brief Blocks until the frame requested is empty.
+ *  This function is thread safe.
+ *  @param [in] buf The buffer
+ *  @param [in] ID The id of the frame wait for.
  */
-uint8_t * wait_for_empty_frame(struct Buffer* buf, const char * producer_name, const int frame_id);
+uint8_t * wait_for_empty_frame(struct Buffer* buf, const char * producer_name, const int ID);
 
 /** @brief Waits for the buffer frame given by ID to be full.
  *  This function is thread safe.
