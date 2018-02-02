8456b7747e5ca145cae10aa7d90c7fdca137d160
Mateus Fandino <fandinomat@gmail.com>
2018-02-01 16:05:10 -0800

    Added doxygen comments to lib/processes/freqSlicer.hpp

--- lib/processes/freqSlicer.hpp
+++ lib/processes/freqSlicer.hpp
@@ -1,3 +1,10 @@
+/*****************************************
+@file
+@brief Processes for splitting and subsetting visibility data by frequency.
+- freqSplit : public KotekanProcess
+
+@todo Add process for frequency subsetting
+*****************************************/
 #ifndef FREQ_SLICER_HPP
 #define FREQ_SLICER_HPP
 
@@ -12,34 +19,51 @@
 
 
 // Split frequencies into upper and lower halves
+/**
+ * @class freqSplit
+ * @brief Separate a visBuffer stream into two by selecting frequencies in the upper and lower half of the band.
+ *
+ * This task takes data coming out of a visBuffer stream and separates it into
+ * two streams. It selects which frames to copy to which buffer by assigning
+ * frequencies in the upper and lower half of the CHIME band to different buffer
+ * streams.
+ *
+ * @par Buffers
+ * @buffer output_buffers The two buffers containing the respective upper or lower band frequencies
+ *         @buffer_format visBuffer structured
+ *         @buffer_metadata visMetadata
+ * @buffer input_buffer The buffer to be split
+ *         @buffer_format visBuffer structured
+ *         @buffer_metadata visMetadata
+ *
+ * @conf  num_elements      Int. The number of elements (i.e. inputs) in the
+ *                          correlator data (read from "/")
+ * @conf  num_eigenvectors  Int. The number of eigenvectors to be stored
+ *
+ * @author Mateus Fandino
+ */
 class freqSplit : public KotekanProcess {
 
 public:
+
+    // Default constructor
     freqSplit(Config &config,
               const string& unique_name,
               bufferContainer &buffer_container);
 
-    // This might be something standard for all KotekanProcesses
-    // Doesn't seem to be implemented
     void apply_config(uint64_t fpga_seq);
 
-    void main_thread(); // This is where the main functionality lives
+    // Main loop for the process
+    void main_thread();
 
 private:
 
     // Parameters saved from the config files
     size_t num_elements, num_eigenvectors;
-// TODO: delete.
-//    size_t block_size;
 
-    // I may have to modify this one depending on what buffers I use in my process
     // Vector of the buffers we are using and their current frame ids.
     std::vector<std::pair<Buffer*, unsigned int>> output_buffers;
-    Buffer * input_buffer; // Pointer to a Buffer object
-
-    // TODO: I don't hink I need this. Should try to comment out
-    // The mapping from buffer element order to output file element ordering
-//    std::vector<uint32_t> input_remap;
+    Buffer * input_buffer;
 
 };
 
