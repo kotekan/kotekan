8351df5cd261a7c0415ed762357a926b6702a83a
Keith Vanderlinde <kvand@Keiths-MacBook-Pro.local>
2018-02-08 17:00:17 -0500

    Clarified bug in networkPowerStream

--- lib/processes/networkPowerStream.hpp
+++ lib/processes/networkPowerStream.hpp
@@ -40,7 +40,7 @@
  * @conf   dest_protocol          String. Should be @c "TCP" or @c "UDP"
  *
  * @warning UDP stream doesn't work at the moment.
- * @bug     Kotekan exits when TCP receiver goes away. Figure out!
+ * @bug     On a mac, exits silently when TCP receiver disconnects. (Ok on linux.)
  * @note    Lots of updating required once buffers are typed...
  *
  * @author Keith Vanderlinde
