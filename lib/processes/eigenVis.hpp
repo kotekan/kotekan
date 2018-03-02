377d277bbad53f8d6db8197b32dda96dba25503d
Kiyoshi Masui <kiyo@physics.ubc.ca>
2018-03-02 14:52:00 -0800

    Documented eigenVis.

--- lib/processes/eigenVis.hpp
+++ lib/processes/eigenVis.hpp
@@ -4,6 +4,34 @@
 #include "buffer.h"
 #include "KotekanProcess.hpp"
 
+/**
+ * @class eigenVis
+ * @brief Perform eigen factorization of the visibilities
+ *
+ * This task performs the factorization of the visibility matrix into 
+ * ``num_eigenvector`` eigenvectors and eigenvalues and stores them in reserve space
+ * in the ``visBuffer``
+ *
+ * @par Buffers
+ * @buffer in_buf The set of buffers coming out the GPU buffers
+ *         @buffer_format visBuffer structured
+ *         @buffer_metadata visMetadata
+ * @buffer out_buf The merged and transformed buffer
+ *         @buffer_format visBuffer structured
+ *         @buffer_metadata visMetadata
+ *
+ * @conf  num_elements          Int. The number of elements (i.e. inputs) in the
+ *                              correlator data.
+ * @conf  block_size            Int. The block size of the packed data.
+ * @conf  num_eigenvectors      Int. The number of eigenvectors to be calculated as
+ *                              an approximation to the visibilities.
+ * @conf  num_diagonals_filled  Int. Number of diagonals to fill with the previouse
+ *                              time step's solution prior to factorization. For
+ *                              example, setting to 1 will replace the main diagonal
+ *                              only. Filled with zero on the first time step.
+ *
+ * @author Kiyoshi Masui
+ */
 class eigenVis : public KotekanProcess {
 
 public:
