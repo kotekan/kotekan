574c05e45a523f318487c172855c4b5ba0bfbd83
Jacob Taylor <jacob.taylor@mail.utoronto.ca>
2018-05-31 12:25:46 -0400

    Fixed comment changes

--- lib/processes/rfiBroadcast.hpp
+++ lib/processes/rfiBroadcast.hpp
@@ -13,6 +13,31 @@
 #include "KotekanProcess.hpp"
 #include "chimeMetadata.h"
 
+/*
+ * @struct RFIHeader
+ * @brief A structure that contains the header attached to each rfiBroadcast packet.
+ */
+struct __attribute__ ((packed)) RFIHeader {
+    /// uint8_t indicating whether or not the SK value was summed over inputs.
+    uint8_t rfi_combined;
+    /// uint32_t indicating the time intergration length of the SK values.
+    uint32_t sk_step;
+    /// uint32_t indicating the number of inputs in the input data.
+    uint32_t num_elements;
+    /// uint32_t indicating the number of timesteps in each frame.
+    uint32_t samples_per_data_set;
+    /// uint32_t indicating the total number of frequencies under consideration (1024 by default).
+    uint32_t num_total_freq;
+    /// uint32_t indicating the number of frequencies in the packet.
+    uint32_t num_local_freq;
+    /// uint32_t indicating the number of frames which were averaged over.
+    uint32_t frames_per_packet;
+    /// int64_t containing the FPGA sequence number of the first packet in the average.
+    int64_t seq_num;
+    /// uint16_t holding the current stream ID value.
+    uint16_t streamID;
+};
+
 /**
  * @class rfiBroadcast
  * @brief Consumer ``KotekanProcess`` which consumer a buffer filled with spectral kurtosis estimates.
@@ -27,28 +52,6 @@
  * @buffer rfi_in	The kotekan buffer containing spectral kurtosis estimates to be read by the process.
  * 	@buffer_format	Array of @c floats
  * 	@buffer_metadata chimeMetadata
- *
- * @struct RFIHeader
- * @brief A structure that contains the header attached to each rfiBroadcast packet.
- * @var RFIHeader::rfi_combined
- * Member 'rfi_combined' contains a uint8_t indicating whether or not the SK value was summed over inputs.
- * @var RFIHeader::sk_step
- * Member 'sk_step' contains a uint32_t indicating the time intergration length of the SK values.
- * @var RFIHeader::num_elements
- * Member 'num_elements' contains a uint32_t indicating the number of inputs in the input data.
- * @var RFIHeader::samples_per_data_set
- * Member 'samples_per_data_set' contains a uint32_t indicating the number of timesteps in each frame.
- * @var RFIHeader::num_total_freq
- * Member 'num_total_freq' contains a uint32_t indicating the total number of frequencies under consideration (1024 by default).
- * @var RFIHeader::num_local_freq
- * Member 'num_local_freq' contains a uint32_t indicating the number of frequencies in the packet.
- * @var RFIHeader::frames_per_packet
- * Member 'frames_per_packet' contains a uint32_t indicating the number of frames which were averaged over.
- * @var RFIHeader::seq_num
- * Member 'seq_num' contains a int64_t containing the FPGA sequence number of the first packet in the average.
- * @var RFIHeader::streamID
- * Member 'streamID' contains a uint16_t holding the current stream ID value.
- *
  *
  * @conf   num_elements         Int (default 2048). Number of elements.
  * @conf   num_local_freq       Int (default 1). Number of local freq.
@@ -65,19 +68,6 @@
  *
  * @author Jacob Taylor
  */
-
-struct __attribute__ ((packed)) RFIHeader {
-    uint8_t rfi_combined;
-    uint32_t sk_step;
-    uint32_t num_elements;
-    uint32_t samples_per_data_set;
-    uint32_t num_total_freq;
-    uint32_t num_local_freq;
-    uint32_t frames_per_packet;
-    int64_t seq_num;
-    uint16_t streamID;
-};
-
 class rfiBroadcast : public KotekanProcess {
 public:
     //Constructor, intializes config variables via apply_config
