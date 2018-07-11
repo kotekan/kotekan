bb007fc2c3b4d3a7ff7ba1fcc7b3301b016fab59
Rick Nitsche <rick@wind.chime>
2018-07-11 13:51:31 -0700

    truncate.hpp: fast_pow: documentation

--- lib/utils/truncate.hpp
+++ lib/utils/truncate.hpp
@@ -57,6 +57,8 @@ inline int32_t count_zeros(int32_t x) {
 /**
  * @brief Fast power of two float.
  *
+ * @param   e   Exponent
+ *
  * @returns The result of 2^e
  */
 inline float fast_pow(int8_t e) {
