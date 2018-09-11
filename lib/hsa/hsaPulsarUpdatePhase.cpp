b9f329ffdea69a99a4ea445d5821f629c0854ec8
Cherry Ng <cherrywyng@gmail.com>
2018-09-11 14:55:35 -0400

    Psr gain endpt rename

--- lib/hsa/hsaPulsarUpdatePhase.cpp
+++ lib/hsa/hsaPulsarUpdatePhase.cpp
@@ -79,15 +79,15 @@ hsaPulsarUpdatePhase::hsaPulsarUpdatePhase(Config& config, const string &unique_
     rest_server.register_post_callback(endpoint_psrcoord,
                                         std::bind(&hsaPulsarUpdatePhase::pulsar_grab_callback, this, _1, _2));
 
-    //Piggy-back on FRB to listen for gain updates
-    endpoint_gains = unique_name + "/frb/update_gains/"+std::to_string(device.get_gpu_id());
-    rest_server.register_post_callback(endpoint_gains,
+    //listen for gain updates
+    endpoint_gains_psr = unique_name + "/update_gains_psr/"+std::to_string(device.get_gpu_id());
+    rest_server.register_post_callback(endpoint_gains_psr,
                                         std::bind(&hsaPulsarUpdatePhase::update_gains_callback, this, _1, _2));
 }
 
 hsaPulsarUpdatePhase::~hsaPulsarUpdatePhase() {
     restServer::instance().remove_json_callback(endpoint_psrcoord);
-    restServer::instance().remove_json_callback(endpoint_gains);
+    restServer::instance().remove_json_callback(endpoint_gains_psr);
     hsa_host_free(host_phase_0);
     hsa_host_free(host_phase_1);
     hsa_host_free(bankID);
