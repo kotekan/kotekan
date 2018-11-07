e4ed8783e15c26aabd954290a5ea99648a3ef7f9
Jacob Taylor <jacob.taylor@mail.utoronto.ca>
2018-11-06 22:34:47 -0500

    Added updateable config enpoint for toggling RFI zeroing (#250)
    * Added updateable config endpoint for toggling RFI zeroing
    * Added flag to CHIME metadata object signifying whether or not the buffer had rfi zeroing enabled

--- lib/hsa/hsaRfiMaskOutput.cpp
+++ lib/hsa/hsaRfiMaskOutput.cpp
@@ -1,4 +1,5 @@
 #include "hsaRfiMaskOutput.hpp"
+#include "chimeMetadata.h"
 
 REGISTER_HSA_COMMAND(hsaRfiMaskOutput);
 
@@ -15,7 +16,6 @@ hsaRfiMaskOutput::hsaRfiMaskOutput(Config& config, const string &unique_name,
     _samples_per_data_set = config.get<uint32_t>(unique_name, "samples_per_data_set");
     //Rfi paramters
     _sk_step = config.get_default<uint32_t>(unique_name, "sk_step", 256);
-    _rfi_add_lostsamples = config.get<bool>(unique_name, "rfi_add_lostsamples");
     //Initialize ID's
     _network_buf_id = 0;
     _rfi_mask_output_buf_id = 0;
@@ -55,7 +55,7 @@ hsa_signal_t hsaRfiMaskOutput::execute(int gpu_frame_id, const uint64_t& fpga_se
 void hsaRfiMaskOutput::finalize_frame(int frame_id) {
     hsaCommand::finalize_frame(frame_id);
 
-    if(_rfi_add_lostsamples){
+    if(get_rfi_zeroed(_network_buf,_network_buf_id)){
         uint8_t * frame_mask = _rfi_mask_output_buf->frames[_rfi_mask_output_buf_id];
         uint32_t total_lost = 0;
         //Copy RFI mask to array
