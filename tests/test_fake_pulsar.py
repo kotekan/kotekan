a997f1750d0ef39ac8bcbd9031685e0464314aea
Keith Vanderlinde <vanderlinde@dunlap.utoronto.ca>
2018-11-17 10:44:23 -0700

    Merging down to a useful state...

--- tests/test_fake_pulsar.py
+++ tests/test_fake_pulsar.py
@@ -10,7 +10,7 @@ pulsar_params = {
     'samples_per_data_set': 4000,  # 10 ms frames
     'num_frames': 4,  # One extra sample to ensure we actually get 256
     'block_size': 2,
-    'pattern': 'pulsar',
+    'mode': 'pulsar',
     'freq': 777,
     'coeff': [0., 0.],
     'dm': 0.,
