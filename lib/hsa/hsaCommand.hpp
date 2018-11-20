902dae7d753cf168fcc8724f70cce5f159280335
Keith Vanderlinde <keith.vanderlinde@gmail.com>
2018-11-20 11:18:47 -0500

    Continuing base-class-ing of hsa. hsaCommand finished and working now.

--- lib/hsa/hsaCommand.hpp
+++ lib/hsa/hsaCommand.hpp
@@ -21,6 +21,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "gpuCommand.hpp"
+
 #include "hsa/hsa.h"
 #include "hsa/hsa_ext_finalize.h"
 #include "hsa/hsa_ext_amd.h"
@@ -37,6 +39,7 @@ struct kernelParams {
     uint16_t group_segment_size;
 };
 
+// TODO: GET RID OF ME! USE BASE GPU ENUM!
 enum class CommandType {COPY_IN, BARRIER, KERNEL, COPY_OUT, NOT_SET};
 
 // Note there are _gpu_buffer_depth frames, which can be thought of as distinct
@@ -50,7 +53,7 @@ enum class CommandType {COPY_IN, BARRIER, KERNEL, COPY_OUT, NOT_SET};
 // This allow us to have host->gpu copies, kernels, and gpu->host copies
 // running co-currently, at the expense of some complexity and extra memory.
 
-class hsaCommand: public kotekanLogging
+class hsaCommand: public gpuCommand
 {
 public:
     // Kernel file name is optional.
@@ -59,13 +62,6 @@ public:
                const string &default_kernel_command="",
                const string &default_kernel_file_name="");
     virtual ~hsaCommand();
-    string &get_name();
-
-    // This function blocks on whatever resource is required by this command
-    // for example if this command requires a full buffer frame to copy
-    // then it should block on that.  It should also block on having any
-    // free output buffers that might be referenced by this command.
-    virtual int wait_on_precondition(int gpu_frame_id);
 
     // Adds either a copy or kernel to one of the hardware queues.
     virtual hsa_signal_t execute(int gpu_frame_id, const uint64_t& fpga_seq,
@@ -75,9 +71,6 @@ public:
     // Note that as a byproduct of this, one shouldn't use the signal after this
     // function has been cased.
     virtual void finalize_frame(int frame_id);
-
-    // Get the profiling time for the last completed signal
-    double get_last_gpu_execution_time();
 
     CommandType get_command_type();
 
@@ -96,17 +89,6 @@ protected:
     // kernel arguments.
     hsa_signal_t enqueue_kernel(const kernelParams &dims, const int gpu_frame_id);
 
-    // The command or kernel name
-    string kernel_command;
-
-    // The file which containts the kernel, if applicable.
-    string kernel_file_name;
-
-    Config &config;
-    string unique_name;
-    bufferContainer host_buffers;
-    hsaDeviceInterface &device;
-
     // The subclass must set the type.
     CommandType command_type = CommandType::NOT_SET;
 
@@ -114,6 +96,8 @@ protected:
     // Note a value of zero for one of the signals means that
     // it isn't currently set.
     hsa_signal_t * signals;
+
+    hsaDeviceInterface &device;
 
     // Pointers to kernel args
     // Note, not used for all commands, only kernel commands.
@@ -123,12 +107,6 @@ protected:
 
     // Pointer to the kernel
     uint64_t kernel_object = 0;
-
-    // Config variables
-    int _gpu_buffer_depth;
-
-    // Profiling time for the last signal
-    double last_gpu_execution_time = 0;
 
     // Helper functions from HSA docs for kernel packet queueing.
     // Some (or all) of this could likely be moved into enqueue_kernel()
