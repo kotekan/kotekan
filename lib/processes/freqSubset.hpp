5ee05fb38105ca0945c644667ea286a4cdb69abc
Rick Nitsche <rick@phas.ubc.ca>
2018-11-23 18:38:02 -0800

    split up freqSlicer (closes #259) * make split_freq a config value (#253) * freqSplit:   * add unit test   * dataset management improvements

--- lib/processes/freqSubset.hpp
+++ lib/processes/freqSubset.hpp
@@ -1,69 +1,15 @@
 /*****************************************
 @file
-@brief Processes for splitting and subsetting visibility data by frequency.
-- freqSplit : public KotekanProcess
+@brief Process for subsetting visibility data by frequency.
 - freqSubset : public KotekanProcess
 *****************************************/
-#ifndef FREQ_SLICER_HPP
-#define FREQ_SLICER_HPP
+#ifndef FREQ_SUBSET_HPP
+#define FREQ_SUBSET_HPP
 
-#include <unistd.h>
 #include <future>
 #include "buffer.h"
 #include "KotekanProcess.hpp"
 #include "datasetManager.hpp"
-
-
-/**
- * @class freqSplit
- * @brief Separate a visBuffer stream into two by selecting frequencies in the upper and lower half of the band.
- *
- * This task takes data coming out of a visBuffer stream and separates it into
- * two streams. It selects which frames to copy to which buffer by assigning
- * frequencies in the upper and lower half of the CHIME band to different buffer
- * streams.
- *
- * @par Buffers
- * @buffer in_buf The buffer to be split
- *         @buffer_format visBuffer structured
- *         @buffer_metadata visMetadata
- * @buffer out_bufs The two buffers containing the respective upper or lower band frequencies
- *         @buffer_format visBuffer structured
- *         @buffer_metadata visMetadata
- *
- * @metric kotekan_dataset_manager_dropped_frame_count
- *         The number of frames dropped while attempting to write.
- *
- * @todo Generalise to arbitary frequency splits.
- * @author Mateus Fandino
- */
-class freqSplit : public KotekanProcess {
-
-public:
-
-    // Default constructor
-    freqSplit(Config &config,
-              const string& unique_name,
-              bufferContainer &buffer_container);
-
-    // Main loop for the process
-    void main_thread() override;
-
-private:
-    /// adds states and datasets and gets new output dataset IDs from manager
-    static std::array<dset_id_t, 2>
-    change_dataset_state(dset_id_t input_dset_id);
-
-    // Vector of the buffers we are using and their current frame ids.
-    std::vector<std::pair<Buffer*, unsigned int>> out_bufs;
-    Buffer * in_buf;
-
-    std::future<std::array<dset_id_t, 2>> _output_dset_id;
-
-    // config values
-    bool _use_dataset_manager;
-};
-
 
 
 /**
