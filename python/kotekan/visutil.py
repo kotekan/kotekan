889309ea243c92066da2d2662043d5402ae4dc81
Richard Shaw <richard@phas.ubc.ca>
2018-11-30 18:15:24 -0800

    Added documentation.

--- python/kotekan/visutil.py
+++ python/kotekan/visutil.py
@@ -9,7 +9,13 @@ from builtins import (ascii, bytes, chr, dict, filter, hex, input,
 
 
 class prod_ctype(object):
-    """Store a product type."""
+    """Store a product type.
+
+    Attributes
+    ----------
+    input_a, input_b : int
+        Indices of the correlated inputs.
+    """
 
     def __init__(self, ipa, ipb):
         # Index of input A
@@ -19,12 +25,38 @@ class prod_ctype(object):
 
 
 def cmap(i, j, n):
-    """Map row column to triangular index."""
+    """Map row column to triangular index.
+
+    Parameters
+    ----------
+    i, j : int or np.ndarray
+        Indices of inputs.
+    n : int
+        Total numver of inputs.
+
+    Returns
+    -------
+    index : int or np.ndarray
+        The index into the upper triangle.
+    """
     return (n * (n + 1) // 2) - ((n - i) * (n - i + 1) // 2) + (j - i)
 
 
 def icmap(k, n):
-    """Map from an upper triangular index to row column."""
+    """Map from an upper triangular index to row column.
+
+    Parameters
+    ----------
+    k : int
+        Index into upper triangle.
+    n : int
+        Total number of inputs.
+
+    Returns
+    -------
+    i, j : int
+        Feed indices.
+    """
     ii = 0
     for ii in range(n):
         if cmap(ii, n - 1, n) >= k:
@@ -35,7 +67,18 @@ def icmap(k, n):
 
 
 def ts_to_double(ts):
-    """Calculate a double precision UNIX time from a timespec."""
+    """Calculate a double precision UNIX time from a timespec.
+
+    Parameters
+    ----------
+    ts : timespec
+        Time as timespec.
+
+    Returns
+    -------
+    time : float
+        Time as UNIX time float.
+    """
     return ts.tv + 1e-9 * ts.tv_nsec
 
 
