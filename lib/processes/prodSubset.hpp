31d54f455ec94dd436f4868f827c2fc09a861561
Richard Shaw <jrichardshaw@gmail.com>
2018-12-10 21:06:47 -0800

    Small bug fixes (#339)
    - Need to skip all prod shaped datasets
    - Removed incorrect test for ev space.
    - Fixed a bug when the eigenVis process gets bad data.

--- lib/processes/prodSubset.hpp
+++ lib/processes/prodSubset.hpp
@@ -24,7 +24,7 @@
 /**
  * @class prodSubset
  * @brief ``KotekanProcess`` that extracts a subset of the products.
- * 
+ *
  * This task consumes a full set of visibilities from a ``visBuffer`` and
  * passes on a subset of products to an output ``visBuffer``. The subset
  * extracted depends on the parameter 'prod_subset_type'. Here is a list of
@@ -34,13 +34,13 @@
  * - 'autos': no extra arameters needed
  *   - The subset are all the auto-correlations.
  * - 'baseline': max_ew_baseline, max_ns_baseline
- *   - Selects a subset of products whose baseline length is smaller than the 
+ *   - Selects a subset of products whose baseline length is smaller than the
  *   maximum values given by config parameters in the EW and NS directions.
  * - 'have_inputs': input_list
- *   - The subset are all the correlations containing at leat one of the 
+ *   - The subset are all the correlations containing at leat one of the
  *   inputs in the input_list.
  * - 'only_inputs': input_list
- *   - The subset are all the correlations containing only inputs from the 
+ *   - The subset are all the correlations containing only inputs from the
  *   input_list.
  *
  * @par Buffers
@@ -66,7 +66,7 @@
  *
  * @warning This will only work correctly if the full correlation triangle is
  * passed in as input.
- * 
+ *
  * @author  Tristan Pinsonneault-Marotte and Mateus Fandino
  *
  */
@@ -89,9 +89,6 @@ private:
                                    std::vector<prod_ctype>& prod_subset,
                                    std::vector<size_t>& prod_ind,
                                    size_t& subset_num_prod);
-
-    /// Parameters saved from the config files
-    size_t num_elements, num_eigenvectors;
 
     /// Number of products in subset
     size_t subset_num_prod;
