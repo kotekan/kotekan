128b3f650de4c2cb4c9cd6512fa9b50cd62ae54b
Rick Nitsche <rick@phas.ubc.ca>
2019-05-21 16:20:49 -0700

    feat(gateSpec): add all member values to to_dm_json()
    Add all information we have about a pulsar gating configuration to the
    gateSpec serialisation and thus to the data field in the serialization
    of the datasetState for gating. This will mean all this information
    get's dumped by the comet dataset-broker.

--- lib/utils/gateSpec.hpp
+++ lib/utils/gateSpec.hpp
@@ -178,6 +178,7 @@ private:
     float _seg;                     // length of polyco segments in s
     std::vector<double> _tmid;      // in MJD
     std::vector<double> _phase_ref; // in number of rotations
+    std::vector<std::vector<float>> _coeff;
     SegmentedPolyco _polycos;
 };
 
