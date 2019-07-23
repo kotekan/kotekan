69007aa4954bd8fe8cc9dcffd9998e3acbb751be
james-s-willis <james_willis@hotmail.co.uk>
2019-07-23 11:42:28 -0400

    Check return code of kotekan.

--- config/tests/run_verification_tests.sh
+++ config/tests/run_verification_tests.sh
@@ -3,34 +3,37 @@
 # Script to run each of the CHIME verification tests
 #
 
-# Exit upon error, avoiding cascading errors
-set -o errexit
+# Return codes
+TEST_FAILED=1
+TEST_PASSED=2
+
+print_message () {
+    echo " "
+    echo "#################################################"
+    echo "# $1"
+    echo "#################################################"
+    echo " "
+}
+
+print_message "Running $0 script"
 
 # Run each test in the directory
-for config_file in ./*
+for config_file in ./*.yaml
 do
-  echo " "
-  echo "#################################################"
-  echo "# Running $config_file test."
-  echo "#################################################"
-  echo " "
+  print_message "Running $config_file test"
 
   # Run test and exit if the test fails
-  {
-    ../../build/kotekan/kotekan -c $config_file
-  } || {
-    echo " "
-    echo "#################################################"
-    echo "# $config_file TEST FAILED"
-    echo "#################################################"
-    echo " "
+  ../../build/kotekan/kotekan -c $config_file
+
+  RETURN_CODE=$?
+
+  if [[ $RETURN_CODE == $TEST_FAILED ]]; then
+    print_message "$config_file TEST FAILED"
     exit
-  }
-  
-  echo " "
-  echo "#################################################"
-  echo "# $config_file TEST PASSED"
-  echo "#################################################"
-  echo " "
+  elif [[ $RETURN_CODE == $TEST_PASSED ]]; then
+    print_message "$config_file TEST PASSED"
+  else
+    print_message "$config_file returned unknown code"
+  fi
 
 done
