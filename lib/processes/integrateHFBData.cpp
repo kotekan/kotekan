ac04f02e51f864d339dd266457a083758ec79689
james-s-willis <james_willis@hotmail.co.uk>
2019-08-29 09:57:32 -0400

    Take into account missing frames when normalising integrated data.

--- lib/processes/integrateHFBData.cpp
+++ lib/processes/integrateHFBData.cpp
@@ -20,6 +20,7 @@ integrateHFBData::integrateHFBData(Config& config_, const string& unique_name,
     _num_frb_total_beams = config.get<uint32_t>(unique_name, "num_frb_total_beams");
     _num_frames_to_integrate = config.get<uint32_t>(unique_name, "num_frames_to_integrate");
     _num_sub_freqs = config.get<uint32_t>(unique_name, "num_sub_freqs");
+    _samples_per_data_set = config.get<uint32_t>(unique_name, "samples_per_data_set");
 
     in_buf = get_buffer("hfb_input_buf");
     register_consumer(in_buf, unique_name.c_str());
@@ -38,8 +39,10 @@ void integrateHFBData::main_thread() {
     uint8_t* in_frame;
     int out_buffer_ID = 0;
     uint frame = 0;
-    const int32_t total_timesamples = _num_frb_total_beams * _num_sub_freqs * _num_frames_to_integrate;
+    //const int32_t total_timesamples = _num_frb_total_beams * _num_sub_freqs * _num_frames_to_integrate;
+    const int32_t total_timesamples = _samples_per_data_set * _num_frames_to_integrate;
     int32_t total_lost_timesamples = 0;
+    int64_t fpga_seq_num = 0;
     
     // Get the first output buffer which will always be id = 0 to start.
     uint8_t* out_frame = wait_for_empty_frame(out_buf, unique_name.c_str(), out_buffer_ID);
@@ -62,8 +65,19 @@ void integrateHFBData::main_thread() {
       // output buffer frame so that we don't need to zero the frame
       if(frame == 0) {
         memcpy(&sum_data[0], &input_data[0], _num_frb_total_beams * _num_sub_freqs * sizeof(float));
+        
+        // Get the first FPGA sequence no. to check for missing frames
+        fpga_seq_num = get_fpga_seq_num(in_buf, in_buffer_ID);
       }
       else {
+
+        // TODO:JSW Account for large gaps between missing frames
+        // TODO:JSW Ensure output frames are aligned
+        // TODO:JSW Store the amount of renormalisation used in the frame
+        // Increment the no. of lost frames if there are missing frames
+        if(get_fpga_seq_num(in_buf, in_buffer_ID) != fpga_seq_num + _samples_per_data_set)
+          total_lost_timesamples += get_fpga_seq_num(in_buf, in_buffer_ID) - fpga_seq_num;
+
         // Integrates data from the input buffer to the output buffer.
         for (uint beam = 0; beam < _num_frb_total_beams; beam++) {
           for (uint freq = 0; freq < _num_sub_freqs; freq++) {
@@ -73,13 +87,16 @@ void integrateHFBData::main_thread() {
       }
 
       frame++;
-      if (frame == _num_frames_to_integrate) { // last frame
+      fpga_seq_num += _samples_per_data_set;
+
+      // When all frames have been integrated output the result
+      if (frame == _num_frames_to_integrate) {
 
         INFO("Summed %d frames of data. buf size: %d, frame size: %d", _num_frames_to_integrate, out_buf->frame_size / 4 * out_buf->num_frames, out_buf->frame_size);
         INFO("No. of lost samples: %d", total_lost_timesamples);
 
         // Normalise data
-        const float normalise_frac = (float)total_lost_timesamples / total_timesamples;
+        const float normalise_frac = (float)total_timesamples / (total_timesamples - total_lost_timesamples);
 
         for (uint beam = 0; beam < _num_frb_total_beams; beam++) {
           for (uint freq = 0; freq < _num_sub_freqs; freq++) {
