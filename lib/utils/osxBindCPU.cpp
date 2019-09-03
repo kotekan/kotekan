kotekan/kotekan

commit 56f614e1d5055402fe6f791bd18ac2dddf9618d7

Use fmt for all string formatting and logging (#483)
- Use {fmt} for logging
- Replaced string formatting with fmt
- Make some more classes inherit from `kotekanLogging`.
- Split up global log level and member variable for log_level for code readability
- Created separate logging macros
  - `*_F` with old printf format to be used from C code
  - `*_NON_OO` whenever no log_level member variable available
- Updated {fmt} to version 6.0.0
- Updated CI server build script to work with updated ROCm

lib/utils/osxBindCPU.cpp
@@ -9,7 +9,7 @@ int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t* cpu_set) {
     size_t len = sizeof(core_count);
     int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
     if (ret) {
-        ERROR("error while get core count %d\n", ret);
+        ERROR("error while get core count {:d}\n", ret);
         return -1;
     }
     cpu_set->count = 0;
@@ -30,8 +30,8 @@ int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t* cpu_set
     }
 
     if (core > INT_MAX)
-        ERROR("Overflow error in osxBindCPU");
-    INFO("binding to core %d\n", core);
+        ERROR_NON_OO("Overflow error in osxBindCPU");
+    INFO_NON_OO("binding to core {:d}\n", core);
     thread_affinity_policy_data_t policy = {(int)core};
     mach_thread = pthread_mach_thread_np(thread);
     thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);

commit 18d01d3eb6a52edf8bec8030c1e2b00997f05ba3

Fix compile errors in osxBindCPU.cpp
This was probably caused by #483, which didn't update this OSX-only file for
changes in the logging API.

lib/utils/osxBindCPU.cpp
@@ -1,5 +1,7 @@
 #include "osxBindCPU.hpp"
 
+#include "kotekanLogging.hpp"
+
 int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t* cpu_set) {
     // Unused parameters, FIXME: can we remove them?
     (void)pid;
@@ -9,7 +11,7 @@ int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t* cpu_set) {
     size_t len = sizeof(core_count);
     int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
     if (ret) {
-        ERROR("error while get core count {:d}\n", ret);
+        ERROR_NON_OO("error while get core count {:d}\n", ret);
         return -1;
     }
     cpu_set->count = 0;

lib/utils/osxBindCPU.hpp
@@ -11,8 +11,6 @@
 #ifndef OSXBINDCPU_H
 #define OSXBINDCPU_H
 
-#include "errors.h"
-
 #include <mach/thread_act.h>
 #include <mach/thread_policy.h>
 #include <pthread.h>