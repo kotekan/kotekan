80630ecc28d2a68fe87aa0d8c5eb0b34f38ce79a
Andre Renard <andre@renard.io>
2019-09-26 12:47:32 -0400

    Switch rfiRecord to updatableConfig, and cleanup stage

--- lib/stages/rfiRecord.cpp
+++ lib/stages/rfiRecord.cpp
@@ -1,21 +1,17 @@
 #include "rfiRecord.hpp"
 
 #include "chimeMetadata.h"
+#include "configUpdater.hpp"
 #include "errors.h"
 #include "gpsTime.h"
-#include "util.h"
 
-#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <functional>
-#include <mutex>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
@@ -24,10 +20,6 @@
 using kotekan::bufferContainer;
 using kotekan::Config;
 using kotekan::Stage;
-
-using kotekan::connectionInstance;
-using kotekan::HTTP_RESPONSE;
-using kotekan::restServer;
 
 REGISTER_KOTEKAN_STAGE(rfiRecord);
 
@@ -39,51 +31,35 @@ rfiRecord::rfiRecord(Config& config, const string& unique_name, bufferContainer&
     register_consumer(rfi_buf, unique_name.c_str());
 
     // General config parameters
-    _num_freq = config.get_default<uint32_t>(unique_name, "num_total_freq", 1024);
-    _num_local_freq = config.get<uint32_t>(unique_name, "num_local_freq");
-    _num_elements = config.get<uint32_t>(unique_name, "num_elements");
     _samples_per_data_set = config.get<uint32_t>(unique_name, "samples_per_data_set");
-    // RFI config parameters
-    _sk_step = config.get_default<uint32_t>(unique_name, "sk_step", 256);
-    _rfi_combined = config.get_default<bool>(unique_name, "rfi_combined", true);
-    // Stage-specific parameters
-    _total_links = config.get_default<uint32_t>(unique_name, "total_links", 1);
-    _write_to = config.get<std::string>(unique_name, "write_to");
-    _write_to_disk = config.get_default<bool>(unique_name, "write_to_disk", false);
     _frames_per_file = config.get_default<uint32_t>(unique_name, "frames_per_file", 1024);
 
     // Initialize rest server endpoint
     using namespace std::placeholders;
-    restServer& rest_server = restServer::instance();
-    endpoint = unique_name + "/rfi_record";
-    rest_server.register_post_callback(endpoint,
-                                       std::bind(&rfiRecord::rest_callback, this, _1, _2));
+    kotekan::configUpdater::instance().subscribe(
+        this, std::bind(&rfiRecord::config_callback, this, _1));
 }
 
-rfiRecord::~rfiRecord() {
-    restServer::instance().remove_json_callback(endpoint);
-}
+rfiRecord::~rfiRecord() {}
 
-void rfiRecord::rest_callback(connectionInstance& conn, json& json_request) {
-    // Notify request was received
-    WARN("RFI Record Callback Received... Changing Parameters")
-    // Lock callback mutex
+bool rfiRecord::config_callback(json& json) {
+
+    string output_dir;
+    bool write_to_disk;
+    try {
+        output_dir = json["output_dir"].get<string>();
+        write_to_disk = json["write_to_disk"].get<bool>();
+    } catch (std::exception& e) {
+        ERROR("Failure parsing message. Error: {:s}, Request JSON: {:s}", e.what(),
+              json.dump());
+        return false;
+    }
     rest_callback_mutex.lock();
-    // Update parameters
-    _write_to = json_request["write_to"].get<string>();
-    WARN("write_to {:s}", _write_to)
-    _write_to_disk = json_request["write_to_disk"].get<bool>();
-    WARN("write_to_disk: {:d}", _write_to_disk)
-    // This will trigger main process to update directories
-    file_num = 0;
-    //    file_num = 2048*(int)((file_num + 2048)/2048);
-    // Update Config Values
-    config.update_value(unique_name, "write_to", _write_to);
-    config.update_value(unique_name, "write_to_disk", _write_to_disk);
-    // Send reply indicating success
-    conn.send_empty_reply(HTTP_RESPONSE::OK);
-    // Unlock mutex
+    _output_dir = output_dir;
+    _write_to_disk = write_to_disk;
     rest_callback_mutex.unlock();
+
+    return true;
 }
 
 void rfiRecord::main_thread() {
@@ -105,7 +81,7 @@ void rfiRecord::main_thread() {
         // Lock mutex
         rest_callback_mutex.lock();
 
-        if (_write_to_disk) {
+        if (_write_to_disk || started) {
 
             fpga_seq_num = get_fpga_seq_num(rfi_buf, frame_id);
             stream_id = get_stream_id_t(rfi_buf, frame_id);
@@ -115,6 +91,24 @@ void rfiRecord::main_thread() {
 
                 started = true;
 
+                // Close the old file if it's open
+                if (fd != -1) {
+                    INFO("Closing last file");
+                    close(fd);
+
+                    // If we set `_write_to_disk` to false, but are currently writing
+                    // i.e. `started` is true, then we want to finish writing the current
+                    // file.   So only we only stop writing on the close state.
+                    if (!_write_to_disk) {
+                        started = false;
+                        fd = -1;
+                        rest_callback_mutex.unlock();
+                        // Since both started and write to disk are false, the next time
+                        // _write_to_disk is true, we start at a new edge with a new name.
+                        continue;
+                    }
+                }
+
                 char data_time[64];
                 struct timespec gps_time = get_gps_time(rfi_buf, frame_id);
                 struct tm timeinfo;
@@ -122,14 +116,10 @@ void rfiRecord::main_thread() {
                     ERROR("Cannot gerate time info");
                 }
                 strftime(data_time, sizeof(data_time), "%Y%m%dT%H%M%S", &timeinfo);
-                snprintf(file_name, sizeof(file_name), "%s/%sN%09ld_%d.rfi", _write_to.c_str(),
+                snprintf(file_name, sizeof(file_name), "%s/%sN%09ld_%d.rfi", _output_dir.c_str(),
                          data_time, gps_time.tv_nsec, bin_number_chime(&stream_id));
 
-                // Close the old file if it's open
-                if (fd != -1) {
-                    INFO("Closing last file");
-                    close(fd);
-                }
+                rest_callback_mutex.unlock();
 
                 // Open the new file
                 INFO("Opening file: %s", file_name);
@@ -143,6 +133,8 @@ void rfiRecord::main_thread() {
                 if (bytes_writen != sizeof(int64_t)) {
                     ERROR("Failed to write seq_num to disk");
                 }
+            } else {
+                rest_callback_mutex.unlock();
             }
 
             if (started) {
@@ -151,9 +143,10 @@ void rfiRecord::main_thread() {
                     ERROR("Failed to write buffer to disk");
                 }
             }
+        } else {
+            rest_callback_mutex.unlock();
         }
-        // Unlock callback mutex
-        rest_callback_mutex.unlock();
+
         // Mark Frame Empty
         mark_frame_empty(rfi_buf, unique_name.c_str(), frame_id);
         // Move forward one frame/link/file
