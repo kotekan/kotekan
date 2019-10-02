2d53aff39e9f611ae2c501958c9813c497960bdf
Andre Renard <andre@renard.io>
2019-10-02 13:06:05 -0400

    Typo

--- lib/stages/bufferMerge.cpp
+++ lib/stages/bufferMerge.cpp
@@ -34,7 +34,7 @@ bufferMerge::bufferMerge(Config& config, const string& unique_name,
             // Assuming each array entry has only one key.
             json::iterator it = buffer.begin();
             internal_name = it.key();
-            buffer_name = in.value().get<std::string>();
+            buffer_name = it.value().get<std::string>();
             in_buf = buffer_container.get_buffer(buffer_name);
             assert(in_buf != nullptr);
         } else if (buffer.is_string()) {
