d0b1bc63ff0d87c7c8326de6931f276c5f81b640
calvin <calvinl@mit.edu>
2019-10-02 15:22:29 +0000

    10.10.10.2 is the GPS time server for pathfinder, not carillon

--- scripts/gps_yaml_to_json.py
+++ scripts/gps_yaml_to_json.py
@@ -8,7 +8,8 @@ with open(sys.argv[1], 'r') as stream:
         sys.stderr.write(exc)
 
 try:
-    gps_request = requests.get('http://carillon.chime:54321/get-frame0-time')
+    gps_request = requests.get('http://10.10.10.2:54321/get-frame0-time')
+    #gps_request = requests.get('http://carillon.chime:54231/get-frame0-time')
     config_json['gps_time'] = gps_request.json()
 except requests.exceptions.RequestException as rex:
     config_json['gps_time'] = {}
