b5e1d7cf8389a65d409823da4ed9ca3f7e0b1b86
Andre Renard <andre@renard.io>
2019-10-02 12:48:40 -0400

    key() is a string already.

--- lib/stages/bufferMerge.cpp
+++ lib/stages/bufferMerge.cpp
@@ -33,7 +33,7 @@ bufferMerge::bufferMerge(Config& config, const string& unique_name,
         if (buffer.is_object()) {
             // Assuming each array entry has only one key.
             json::iterator it = buffer.begin();
-            internal_name = it.key().get<std::string>();
+            internal_name = it.key();
             buffer_name = in.value().get<std::string>();
             in_buf = buffer_container.get_buffer(buffer_name);
             assert(in_buf != nullptr);
