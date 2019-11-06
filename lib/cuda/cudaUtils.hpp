6b24b53eee2e3b4dc1171cc225e9c3b7aea3542f
Andre Renard <andre@renard.io>
2019-11-06 13:32:20 -0500

    clang-format

--- lib/cuda/cudaUtils.hpp
+++ lib/cuda/cudaUtils.hpp
@@ -7,8 +7,9 @@
 // can be wrapped around any runtime API call. No-op in release builds.
 #define CHECK_CUDA_ERROR(result)                                                                   \
     if (result != cudaSuccess) {                                                                   \
-        FATAL_ERROR(LOG_ERR, "Error at {:s}:{:d}; Error type: {:s}", __FILE__, __LINE__,            \
-                         cudaGetErrorString(result));                                              \                          \
+        FATAL_ERROR(LOG_ERR, "Error at {:s}:{:d}; Error type: {:s}", __FILE__, __LINE__,           \
+                    cudaGetErrorString(result));                                                   \
+        \                                                                                          \
     }
 
 #endif // CUDA_UTILS_H
