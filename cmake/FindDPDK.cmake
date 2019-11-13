32ed5449b356f833041d208e400fbe0a18acd31d
Andre Renard <andre@renard.io>
2019-11-13 09:06:24 -0500

    Backwards compatibility

--- cmake/FindDPDK.cmake
+++ cmake/FindDPDK.cmake
@@ -1,26 +1,42 @@
 # Tries to find DPDK on local system
 # If found it defines DPDK_FOUND, DPDK_INCLUDE_DIR, and DPDK_LIBRARIES
+# If DPDK is installed from source then this requires
+# ${RTE_SDK} and ${RTE_TARGET}
 
-set(DPDK_SEARCH_PATHS /usr/include /usr/local/include)
+include(FindPackageHandleStandardArgs)
 
-find_path(DPDK_INCLUDE_DIR rte_config.h
-          PATHS ${DPDK_SEARCH_PATHS}
+# Default install locations
+set(DPDK_SEARCH_PATHS /usr/include
+                      /usr/local/include)
+
+# Note the ${RTE_SDK}/${RTE_TARGET} path is for backwards compatibility
+# with non-standard DPDK install locations
+if (RTE_SDK)
+    set(DPDK_LINK_HINT "${RTE_SDK}/${RTE_TARGET}/lib")
+    message("DPDK Path set to: ${DPDK_LINK_HINT}")
+else()
+    set(DPDK_LINK_HINT "")
+endif()
+
+find_path(DPDK_INCLUDE_DIR rte_common.h
+          PATHS ${DPDK_SEARCH_PATHS} ${RTE_SDK}/${RTE_TARGET}/include
           PATH_SUFFIXES dpdk)
-find_library(rte_hash_LIBRARY rte_hash)
-find_library(rte_kvargs_LIBRARY rte_kvargs)
-find_library(rte_mbuf_LIBRARY rte_mbuf)
-find_library(rte_ethdev_LIBRARY rte_ethdev)
-find_library(rte_mempool_LIBRARY rte_mempool)
-find_library(rte_ring_LIBRARY rte_ring)
-find_library(rte_eal_LIBRARY rte_eal)
-find_library(rte_cmdline_LIBRARY rte_cmdline)
-find_library(rte_pmd_bond_LIBRARY rte_pmd_bond)
-find_library(rte_pmd_ixgbe_LIBRARY rte_pmd_ixgbe)
-find_library(rte_pmd_i40e_LIBRARY rte_pmd_i40e)
-find_library(rte_pmd_ring_LIBRARY rte_pmd_ring)
-find_library(rte_pmd_af_packet_LIBRARY rte_pmd_af_packet)
 
-set(dpdk_list_LIBRARIES
+find_library(rte_hash_LIBRARY NAMES rte_hash PATHS ${DPDK_LINK_HINT})
+find_library(rte_kvargs_LIBRARY NAMES rte_kvargs PATHS ${DPDK_LINK_HINT})
+find_library(rte_mbuf_LIBRARY NAMES rte_mbuf PATHS ${DPDK_LINK_HINT})
+find_library(rte_ethdev_LIBRARY NAMES rte_ethdev PATHS ${DPDK_LINK_HINT})
+find_library(rte_mempool_LIBRARY NAMES rte_mempool PATHS ${DPDK_LINK_HINT})
+find_library(rte_ring_LIBRARY NAMES rte_ring PATHS ${DPDK_LINK_HINT})
+find_library(rte_eal_LIBRARY NAMES rte_eal PATHS ${DPDK_LINK_HINT})
+find_library(rte_cmdline_LIBRARY NAMES rte_cmdline PATHS ${DPDK_LINK_HINT})
+find_library(rte_pmd_bond_LIBRARY NAMES rte_pmd_bond PATHS ${DPDK_LINK_HINT})
+find_library(rte_pmd_ixgbe_LIBRARY NAMES rte_pmd_ixgbe PATHS ${DPDK_LINK_HINT})
+find_library(rte_pmd_i40e_LIBRARY NAMES rte_pmd_i40e PATHS ${DPDK_LINK_HINT})
+find_library(rte_pmd_ring_LIBRARY NAMES rte_pmd_ring PATHS ${DPDK_LINK_HINT})
+find_library(rte_pmd_af_packet_LIBRARY NAMES rte_pmd_af_packet PATHS ${DPDK_LINK_HINT})
+
+set(DPDK_LIST_LIBRARIES
     ${rte_hash_LIBRARY}
     ${rte_kvargs_LIBRARY}
     ${rte_mbuf_LIBRARY}
@@ -36,9 +52,8 @@ set(dpdk_list_LIBRARIES
     ${rte_pmd_ring_LIBRARY}
     ${rte_pmd_af_packet_LIBRARY})
 
-include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(dpdk DEFAULT_MSG
-                                  dpdk_list_LIBRARIES
+                                  DPDK_LIST_LIBRARIES
                                   DPDK_INCLUDE_DIR)
 
 if(DPDK_FOUND)
@@ -46,4 +61,19 @@ if(DPDK_FOUND)
             -Wl,--whole-archive ${dpdk_list_LIBRARIES} -lpthread -Wl,--no-whole-archive)
 endif(DPDK_FOUND)
 
-mark_as_advanced(DPDK_INCLUDE_DIR dpdk_list_LIBRARIES)
+mark_as_advanced(DPDK_INCLUDE_DIR
+                 DPDK_LIBRARIES
+                 DPDK_LIST_LIBRARIES
+                 rte_hash_LIBRARY
+                 rte_kvargs_LIBRARY
+                 rte_mbuf_LIBRARY
+                 rte_ethdev_LIBRARY
+                 rte_mempool_LIBRARY
+                 rte_ring_LIBRARY
+                 rte_eal_LIBRARY
+                 rte_cmdline_LIBRARY
+                 rte_pmd_bond_LIBRARY
+                 rte_pmd_ixgbe_LIBRARY
+                 rte_pmd_i40e_LIBRARY
+                 rte_pmd_ring_LIBRARY
+                 rte_pmd_af_packet_LIBRARY)
