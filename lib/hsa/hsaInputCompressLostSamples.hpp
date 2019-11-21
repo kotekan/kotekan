10f9434e8974c60bad7b8c6d5f68913a27dd2f3b
james-s-willis <james_willis@hotmail.co.uk>
2019-11-21 10:23:14 -0500

    Created a command object to copy the compressed lost samples buffer onto the GPU.

---
+++ lib/hsa/hsaInputCompressLostSamples.hpp
@@ -0,0 +1,60 @@
+/*
+ * @file
+ * @brief Copy compressed lost sample buffer from host to gpu
+ *  - hsaInputCompressLostSamples : public hsaCommand
+ */
+#ifndef HSA_INPUT_COMPRESS_LOST_SAMPLES_H
+#define HSA_INPUT_COMPRESS_LOST_SAMPLES_H
+
+#include "hsaCommand.hpp"
+
+/*
+ * @class hsaInputCompressLostSamples
+ * @brief hsaCommand for copying compressed lost sample buffer from host to gpu
+ *
+ * A kotekan hsa command to copy the compressed lost samples buffer from host memory to GPU memory. The command
+ * waits for a full frame, async copies the frame contents to GPU memroy, and marks the frame empty.
+ * The compressed lost samples buffer is used in the HFB pipeline to exclude samples when summing across time.
+ *
+ * @par GPU Memory
+ * @gpu_mem  compressed_lost_samples Array indicating if a given timestep was zeroed, size:
+ * samples_per_data_set / num_sub_freqs
+ *     @gpu_mem_type         staging
+ *     @gpu_mem_format       Array of @c uint8_t
+ *     @gpu_mem_metadata     chimeMetadata
+ *
+ * @author James Willis
+ */
+class hsaInputCompressLostSamples : public hsaCommand {
+public:
+    /// Constructor, applies config, initializes variables
+    hsaInputCompressLostSamples(kotekan::Config& config, const string& unique_name,
+                        kotekan::bufferContainer& host_buffers, hsaDeviceInterface& device);
+    // Destructor, cleans up local allocs
+    virtual ~hsaInputCompressLostSamples();
+    /// Wait for full metadata frame and keep track of precondition_id
+    int wait_on_precondition(int gpu_frame_id) override;
+    /// Allocates kernel arguments, places kernel in queue
+    hsa_signal_t execute(int gpu_frame_id, hsa_signal_t precede_signal) override;
+    /// Marks frame empty for re-fill
+    void finalize_frame(int frame_id) override;
+
+private:
+    /// Frame ID
+    uint32_t compressed_lost_samples_buffer_id;
+    /// Frame ID for pre condition
+    uint32_t compressed_lost_samples_buffer_precondition_id;
+    /// Frame ID for finilazation
+    uint32_t compressed_lost_samples_buffer_finalize_id;
+    /// Lost samples buffer
+    Buffer* compressed_lost_samples_buf;
+    /// Length of compressed lost samples frame
+    uint32_t input_frame_len;
+    /// Kotekan Config Variables
+    /// Samples per data set, used for computing frame length
+    uint32_t _samples_per_data_set;
+    /// Total number of sub frequencies, should be 128
+    uint32_t _num_sub_freqs;
+};
+
+#endif /*HSA_INPUT_COMPRESS_LOST_SAMPLES_H*/
