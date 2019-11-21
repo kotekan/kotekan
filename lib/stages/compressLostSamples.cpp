bfb5e8b157f3e389b30b6e5713efb2689cfb880b
james-s-willis <james_willis@hotmail.co.uk>
2019-11-21 14:59:35 -0500

    Reduce size of compressed lost samples frame even further by a factor of 1/3.

--- lib/stages/compressLostSamples.cpp
+++ lib/stages/compressLostSamples.cpp
@@ -51,11 +51,11 @@ void compressLostSamples::main_thread() {
         uint8_t* compressed_lost_samples_frame = out_buf->frames[out_buffer_ID];
 
         // Compress lost samples buffer by checking each sample for a flag
-        for (uint sample = 0; sample < _samples_per_data_set; sample += _num_sub_freqs) {
-            compressed_lost_samples_frame[sample % _num_sub_freqs] = 0;
-            for (uint freq = 0; freq < _num_sub_freqs; freq++) {
+        for (uint sample = 0; sample < _samples_per_data_set; sample += 3 * _num_sub_freqs) {
+            compressed_lost_samples_frame[sample / (3 * _num_sub_freqs)] = 0;
+            for (uint freq = 0; freq < 3 * _num_sub_freqs; freq++) {
                 if (lost_samples_frame[sample + freq]) {
-                    compressed_lost_samples_frame[sample % _num_sub_freqs] = 1;
+                    compressed_lost_samples_frame[sample / (3 * _num_sub_freqs)] = 1;
                     break;
                 }
             }
