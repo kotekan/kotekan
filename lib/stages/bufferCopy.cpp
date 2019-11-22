a4871ce6e476d7b4d2e2606adf4b9bedaaf44346
james-s-willis <james_willis@hotmail.co.uk>
2019-11-22 15:25:03 -0500

    Addressed review comments.

--- lib/stages/bufferCopy.cpp
+++ lib/stages/bufferCopy.cpp
@@ -18,10 +18,10 @@ bufferCopy::bufferCopy(Config& config, const string& unique_name,
                          bufferContainer& buffer_container) :
     Stage(config, unique_name, buffer_container, std::bind(&bufferCopy::main_thread, this)) {
 
-    _timeout = config.get_default<double>(unique_name, "timeout", -1.0);
-
     in_buf = get_buffer("in_buf");
     register_consumer(in_buf, unique_name.c_str());
+
+    _copy_metadata = config.get_default<bool>(unique_name, "copy_metadata", false);
 
     json buffer_list = config.get<std::vector<json>>(unique_name, "out_bufs");
     Buffer* out_buf = nullptr;
@@ -36,12 +36,16 @@ bufferCopy::bufferCopy(Config& config, const string& unique_name,
             internal_name = it.key();
             buffer_name = it.value().get<std::string>();
             out_buf = buffer_container.get_buffer(buffer_name);
-            assert(out_buf != nullptr);
+            if(out_buf == nullptr)
+              throw std::runtime_error(
+                  fmt::format(fmt("Output buffer is null: {:s}"), buffer.dump()));
         } else if (buffer.is_string()) {
             buffer_name = buffer.get<std::string>();
             internal_name = "";
             out_buf = buffer_container.get_buffer(buffer_name);
-            assert(out_buf != nullptr);
+            if(out_buf == nullptr)
+              throw std::runtime_error(
+                  fmt::format(fmt("Output buffer is null: {:s}"), buffer.dump()));
         } else {
             throw std::runtime_error(
                 fmt::format(fmt("Unknown value in out_bufs: {:s}"), buffer.dump()));
@@ -59,22 +63,9 @@ bufferCopy::bufferCopy(Config& config, const string& unique_name,
     }
 }
 
-bool bufferCopy::select_frame(const std::string& internal_name, Buffer* out_buf,
-                               uint32_t frame_id) {
-    (void)internal_name;
-    (void)out_buf;
-    (void)frame_id;
-    return true;
-}
-
 void bufferCopy::main_thread() {
 
     frameID in_frame_id(in_buf);
-
-    //if (get_num_producers(out_buf) != 1) {
-    //    FATAL_ERROR("Cannot merge into a buffer with more than one producer");
-    //    return;
-    //}
 
     while (!stop_thread) {
       uint8_t* input_frame =
@@ -83,9 +74,15 @@ void bufferCopy::main_thread() {
         break;
 
       for (auto& buffer_info : out_bufs) {
+        
         const std::string& internal_buffer_name = std::get<0>(buffer_info);
         Buffer* out_buf = std::get<1>(buffer_info);
         frameID& out_frame_id = std::get<2>(buffer_info);
+
+        if (get_num_producers(out_buf) != 1) {
+          FATAL_ERROR("Cannot copy into buffer: {:s} as it has more than one producer.", internal_buffer_name);
+          return;
+        }
 
         /// Wait for an output frame
         DEBUG2("Waiting for {:s}[{:d}]", out_buf->buffer_name, out_frame_id);
@@ -94,17 +91,15 @@ void bufferCopy::main_thread() {
         if (output_frame == NULL)
           goto exit_loop; // Shutdown condition
 
-        if (select_frame(internal_buffer_name, in_buf, in_frame_id)) {
-
-          // Move the metadata over to the new frame
+        // Move the metadata over to the new frame if flag set
+        if(_copy_metadata)
           pass_metadata(in_buf, in_frame_id, out_buf, out_frame_id);
 
-          // Copy the frame.
-          std::memcpy(output_frame, input_frame, in_buf->frame_size);
+        // Copy the frame.
+        std::memcpy(output_frame, input_frame, in_buf->frame_size);
 
-          mark_frame_full(out_buf, unique_name.c_str(), out_frame_id);
-          out_frame_id++;
-        }
+        mark_frame_full(out_buf, unique_name.c_str(), out_frame_id);
+        out_frame_id++;
       }
 
       // We always release the input buffer even if it isn't selected.
--- lib/stages/bufferCopy.hpp
+++ lib/stages/bufferCopy.hpp
@@ -34,10 +34,6 @@
  *                 of this function requires internal names to select frame.
  *        @buffer_format any, but all must be the same type.
  *        @buffer_metadata any, but all must be the same type.
-*
- * @conf timeout       Double. Default -1.0   Timeout in seconds for waiting
- *                       for a frame on any of the input buffers.
- *                       Set to a negative number for no timeout.
  *
  * @author James Willis
  */
@@ -50,20 +46,6 @@ public:
     /// Destructor
     ~bufferCopy() = default;
 
-    /**
-     * @brief This function can be overridden to implement different selection
-     *        methods.  By default it just accepts everything.
-     *
-     * @param internal_name The name given in the config to the buffer
-     *                      (not the buffer name)
-     * @param in_buf        The buffer pointers
-     * @param frame_id      The frame_id for the current frame to accept or reject.
-     *
-     * @return true if the frame should be swapped into the out_buf, false if
-     *         it should be dropped.
-     */
-    virtual bool select_frame(const std::string& internal_name, Buffer* in_buf, uint32_t frame_id);
-
     /// Thread for merging the frames.
     void main_thread() override;
 
@@ -71,12 +53,13 @@ protected:
     /// The input buffer to copy frames from
     struct Buffer* in_buf;
 
+    /// Config variables
+    /// Flag to copy metadata or not
+    bool _copy_metadata;
+
     /// Array of output buffers to copy frames into
     /// Items are "internal_name", "buffer", "frame_id", "use_memcpy"
     std::vector<std::tuple<std::string, Buffer*, frameID>> out_bufs;
-
-    /// The in seconds to wait for a new frame on one of the input buffers.
-    double _timeout;
 };
 
 #endif
