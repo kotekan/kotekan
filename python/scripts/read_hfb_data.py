8e04ed032cc5737019537b8a976b4fd5d4b6e6bd
james-s-willis <james_willis@hotmail.co.uk>
2019-11-25 14:56:01 -0500

    Print metadata field names in output.

--- python/scripts/read_hfb_data.py
+++ python/scripts/read_hfb_data.py
@@ -4,16 +4,26 @@ import matplotlib.pyplot as plt
 
 print("Reading file: {}".format(sys.argv[1]))
 
-metadata_t = np.dtype([('metadata_size', np.uint32), ('fpga_seq_num', np.int64),  ('gps_time', [('', np.int64),('', np.int64)]), ('gps_time_flag', np.uint32), ('freq_bin_num', np.uint32), ('norm_frac', np.float32), ('num_samples_integrated', np.uint32), ('num_samples_expected', np.uint32), ('compressed_data_size', np.uint32) ])
+metadata_t = np.dtype([('metadata_size', np.uint32), ('fpga_seq_num', np.int64),  ('gps_time', [('s', np.int64),('ns', np.int64)]), ('gps_time_flag', np.uint32), ('freq_bin_num', np.uint32), ('norm_frac', np.float32), ('num_samples_integrated', np.uint32), ('num_samples_expected', np.uint32), ('compressed_data_size', np.uint32) ])
 
 dt = np.dtype([ ('metadata', metadata_t), ('data', np.float32, (1024 * 128,)) ])
 
 data = np.fromfile(sys.argv[1], dtype=dt)
 
+print("No. of frames in the file: %d" % len(data['metadata']))
+
 print "Metadata"
 print "--------"
+print metadata_t.names
 print data['metadata']
 
+#for element in data['metadata']:
+#  out = ""
+#  for field in metadata_t.names:
+#      out = out + field + ": " + str(element[field]) + ", "
+#
+#  print out 
+
 print data['data']
-plt.plot(data['data'][0])
-plt.show()
+#plt.plot(data['data'][0])
+#plt.show()
