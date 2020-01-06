ce03438da63a2cba54af52d44374dec95d505967
Andre Renard <andre@renard.io>
2020-01-06 13:41:48 -0500

    Increase alignment window

--- lib/dpdk/iceBoardHandler.hpp
+++ lib/dpdk/iceBoardHandler.hpp
@@ -90,7 +90,7 @@ protected:
         // We allow for the fact we might miss the first packet by upto 100 FPGA frames,
         // if this happens then the missing frames at the start of the buffer frame are filled
         // in as lost packets.
-        if ((seq % alignment) <= 100) {
+        if ((seq % alignment) <= 1000) {
 
             last_seq = seq - seq % alignment;
             cur_seq = seq;
