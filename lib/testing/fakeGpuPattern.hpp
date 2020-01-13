4781fced37d31363b9633a51c937bc6382761c9e
Rick Nitsche <rick@phas.ubc.ca>
2020-01-13 11:03:25 -0800

    fix iwyu

--- lib/testing/fakeGpuPattern.hpp
+++ lib/testing/fakeGpuPattern.hpp
@@ -11,6 +11,7 @@
 #ifndef FAKE_GPU_PATTERN_HPP
 #define FAKE_GPU_PATTERN_HPP
 
+#include "fakeGpuPattern.hpp"
 #include "Config.hpp"         // IWYU pragma: keep
 #include "factory.hpp"        // for REGISTER_NAMED_TYPE_WITH_FACTORY, CREATE_FACTORY, Factory
 #include "kotekanLogging.hpp" // for kotekanLogging
