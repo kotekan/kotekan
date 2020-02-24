d1c61aa56b7f61c2a2c40ba544987423edfa4a61
james-s-willis <james_willis@hotmail.co.uk>
2020-02-24 12:22:41 -0500

    Added dataset_id to HFB metadata.

--- lib/metadata/hfbMetadata.h
+++ lib/metadata/hfbMetadata.h
@@ -33,6 +33,8 @@ struct hfbMetadata {
     uint32_t num_beams;
     /// The number of sub-frequencies in the data.
     uint32_t num_subfreq;
+    /// ID of the dataset
+    dset_id_t dataset_id;
 };
 
 // Helper functions to save lots of pointer work
@@ -77,6 +79,21 @@ inline int64_t get_compressed_data_size_hfb(struct Buffer* buf, int ID) {
     return hfb_metadata->compressed_data_size;
 }
 
+inline uint32_t get_num_beams(struct Buffer* buf, int ID) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    return hfb_metadata->num_beams;
+}
+
+inline uint32_t get_num_subfreq(struct Buffer* buf, int ID) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    return hfb_metadata->num_subfreq;
+}
+
+inline dset_id_t get_dataset_id(struct Buffer* buf, int ID) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    return hfb_metadata->dataset_id;
+}
+
 // Setting functions
 
 inline void set_fpga_seq_num_hfb(struct Buffer* buf, int ID, int64_t seq) {
@@ -119,6 +136,20 @@ inline void set_compressed_data_size_hfb(struct Buffer* buf, int ID, uint32_t co
     hfb_metadata->compressed_data_size = compressed_size;
 }
 
+inline void set_num_beams(struct Buffer* buf, int ID, uint32_t num_beams) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    hfb_metadata->num_beams = num_beams;
+}
+
+inline void set_num_subfreq(struct Buffer* buf, int ID, uint32_t num_subfreq) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    hfb_metadata->num_subfreq = num_subfreq;
+}
+
+inline void set_dataset_id(struct Buffer* buf, int ID, dset_id_t dataset_id) {
+    struct hfbMetadata* hfb_metadata = (struct hfbMetadata*)buf->metadata[ID]->metadata;
+    hfb_metadata->dataset_id = dataset_id;
+}
 #ifdef __cplusplus
 }
 #endif
