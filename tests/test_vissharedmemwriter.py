e1a399a77a0a4e052d5d8f29819f725f6366a7cc
anjakefala <anja.kefala@gmail.com>
2020-02-29 12:31:49 -0800

    test(visSharedMemWriter): add test for stage visSharedMemWriter
    - ensures access to shared semaphore; access shared memory regions, and
    iterates through them

--- tests/test_vissharedmemwriter.py
+++ tests/test_vissharedmemwriter.py
@@ -1,7 +1,7 @@
 # === Start Python 2/3 compatibility
 from __future__ import absolute_import, division, print_function, unicode_literals
-from future.builtins import * #noqa pylint: disable=W0401, W0614
-from future.builtins.disabled import * #noqa pylint: disable=W0401, W0614
+from future.builtins import *  # noqa pylint: disable=W0401, W0614
+from future.builtins.disabled import *  # noqa pylint: disable=W0401, W0614
 
 # == End Python 2/3 compatibility
 
@@ -12,17 +12,19 @@ import os
 
 from kotekan import runner
 
-SEM_NAME = "kotekan"
-MEM_NAME = "ringBufferMetadata"
+sem_name = "kotekan"
+fname_access_record = "calBufferAccessRecord"
+fname_buf = "calBuffer"
 
 params = {
-        "num_elements": 7,
-        "num_ev": 0,
-        "total_frames": 128,
-        "cadence": 10.0,
-        "mode": "default",
-        "dataset_manager": {"use_dataset_broker": False},
+    "num_elements": 7,
+    "num_ev": 0,
+    "total_frames": 128,
+    "cadence": 10.0,
+    "mode": "default",
+    "dataset_manager": {"use_dataset_broker": False},
 }
+
 
 @pytest.fixture(scope="module")
 def vis_data(tmpdir_factory):
@@ -30,43 +32,57 @@ def vis_data(tmpdir_factory):
     # keeping all the data this test produced here (probably do not need it)
     # using FakeVisBuffer to produce fake data
     fakevis_buffer = runner.FakeVisBuffer(
-            num_frames=params["total_frames"],
-            mode=params["mode"],
+        num_frames=params["total_frames"], mode=params["mode"]
     )
 
     # KotekanStageTester is used to run kotekan with my config
     test = runner.KotekanStageTester(
-            stage_type="visSharedMemWriter",
-            stage_config={},
-            buffers_in=fakevis_buffer,
-            buffers_out=None,
-            global_config=params
+        stage_type="visSharedMemWriter",
+        stage_config={},
+        buffers_in=fakevis_buffer,
+        buffers_out=None,
+        global_config=params,
     )
 
     test.run()
 
+
 @pytest.fixture(scope="module")
 def semaphore():
-    sem = posix_ipc.Semaphore(SEM_NAME)
-    print("Test is waiting to acquire the semaphore")
-    sem.acquire()
-    print("Acquired!")
+    sem = posix_ipc.Semaphore(sem_name)
     yield sem
     sem.release()
     sem.unlink()
 
+
 @pytest.fixture(scope="module")
-def memory_map():
-    memory = posix_ipc.SharedMemory(MEM_NAME)
+def memory_map_buf():
+    memory = posix_ipc.SharedMemory(fname_buf)
     mapfile = mmap.mmap(memory.fd, memory.size, prot=mmap.PROT_READ)
     os.close(memory.fd)
     yield mapfile
     mapfile.close()
-    posix_ipc.unlink_shared_memory(MEM_NAME)
+    posix_ipc.unlink_shared_memory(fname_buf)
 
-def test_sharedmem(vis_data, semaphore, memory_map):
-    memory_map.seek(0)
+@pytest.fixture(scope="module")
+def mem_map_access_record():
+    memory = posix_ipc.SharedMemory(fname_access_record)
+    mapfile = mmap.mmap(memory.fd, memory.size, prot=mmap.PROT_READ)
+    os.close(memory.fd)
+    yield mapfile
+    mapfile.close()
+    posix_ipc.unlink_shared_memory(fname_access_record)
+
+
+def test_sharedmem(vis_data, semaphore, mem_map_access_record, memory_map_buf):
+    for memory_map in [mem_map_access_record, memory_map_buf]:
+        memory_map.seek(0)
     import struct
-    for i in range(0, 800, 8):
-        print(struct.unpack("<Q", memory_map.read(8))[0])
 
+    for i in range(0, 100):
+        semaphore.acquire()
+        print(struct.unpack("<c", memory_map_buf.read(1))[0])
+        print(struct.unpack("<c", memory_map_buf.read(1))[0])
+        print(struct.unpack("<c", memory_map_buf.read(1))[0])
+        semaphore.release()
+        print(struct.unpack("<Q", mem_map_access_record.read(8))[0])
