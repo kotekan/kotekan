9380de4086d7efd5a145ff5c1a5e843329af0cba
Rick Nitsche <rick@phas.ubc.ca>
2020-03-19 16:16:29 -0700

    test: read data

--- tests/test_vissharedmemreaderwriter.py
+++ tests/test_vissharedmemreaderwriter.py
@@ -6,6 +6,7 @@ from future.builtins.disabled import *  # noqa pylint: disable=W0401, W0614
 # == End Python 2/3 compatibility
 
 import logging
+import numpy as np
 import posix_ipc
 import pytest
 import threading
@@ -65,24 +66,56 @@ def vis_data_slow(tmpdir_factory):
 
 
 def test_shared_mem_buffer(vis_data_slow):
+    num_freq = len(params_fakevis["freq_ids"])
+    num_ev = params["num_ev"]
+    num_elements = params["num_elements"]
+
     threading.Thread(target=vis_data_slow.run).start()
     sleep(3)
     buffer = shared_memory_buffer.SharedMemoryReader(sem_name, fname_buf, 4)
 
     assert buffer.num_time == params_writer_stage["nsamples"]
-    assert buffer.num_freq == len(params_fakevis["freq_ids"])
+    assert buffer.num_freq == num_freq
 
     # access_record = []
     # for t in
     # assert buffer._access_record() == access_record
-    print(buffer._access_record())
+
+    n_times_to_read = 3
 
     i = 0
     with pytest.raises(shared_memory_buffer.SharedMemoryError):
         while True:
-            print("round {}".format(i))
             sleep(1)
             print(buffer._access_record())
-            print(buffer.read_last(3))
+            visraw = buffer.read_last(n_times_to_read)
+            assert visraw.num_freq == len(params_fakevis["freq_ids"])
+            assert visraw.num_time == n_times_to_read
+
+            ds = np.array(visraw.metadata["dataset_id"]).copy().view("u8,u8")
+            unique_ds = np.unique(ds)
+
+            evals = visraw.data["eval"]
+            evecs = visraw.data["evec"]
+            erms = visraw.data["erms"]
+
+            # Check datasets are present
+            assert evals.shape == (n_times_to_read, num_freq, num_ev)
+            assert evecs.shape == (n_times_to_read, num_freq, num_ev * num_elements)
+            assert erms.shape == (n_times_to_read, num_freq)
+
+            evecs = evecs.reshape(n_times_to_read, num_freq, num_ev, num_elements)
+
+            # Check that the datasets have the correct values
+            assert (evals == np.arange(num_ev)[np.newaxis, np.newaxis, :]).all()
+            assert (
+                evecs.real == np.arange(num_ev)[np.newaxis, np.newaxis, :, np.newaxis]
+            ).all()
+            assert (
+                evecs.imag
+                == np.arange(num_elements)[np.newaxis, np.newaxis, np.newaxis, :]
+            ).all()
+            assert (erms == 1.0).all()
+
             i += 1
     assert i >= 2
