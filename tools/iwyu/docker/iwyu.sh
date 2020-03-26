kotekan/kotekan

commit ac19489a1fc5b400ed329706aab9a9ce279d0cca

Run CI on Github actions (#699)
* Fix an issue when subscribing updatable_configs with no callbacks
* Add build option to not lock buffer memory
* Add build option to specify architecture
* Skip test_dataset_broker if redis is not available
* Refactor test_networkbuffer to no longer need comet to run
* Skip test_freqslicer_broker if redis is not available
* Refactor Github actions workflow to run entire CI process

.github/workflows/main.yml
@@ -1,4 +1,13 @@
-name: include-what-you-use
+# NOTE:
+# When we build kotekan on the Github VM we need to take into account a few
+# limitations: first we can't lock very much memory so we need to disable
+# mlock the buffers. Second, the VMs run across a range of CPU
+# architectures (from Haswell to Skylake-AVX512 as of 2020/03). Because our
+# use of ccache shares compiled objects we must target our build for the
+# lowest architecture available or we will occasionally crash with SIGILL
+# when a newer instruction is called than is available.
+
+name: kotekan-ci-test
 on:
   pull_request:
     branches:
@@ -9,18 +18,169 @@ on:
     - develop
     - master
 
-jobs:
-  build:
-   runs-on: ubuntu-latest
+env:
+  IMG_CORE: docker.pkg.github.com/kotekan/kotekan/kotekan-core:latest
+  IMG_IWYU: docker.pkg.github.com/kotekan/kotekan/kotekan-iwyu:latest
 
-   steps:
-   - uses: actions/checkout@v1
-   - name: Build docker image
-     run: |
-       docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}"
-       docker pull --disable-content-trust docker.pkg.github.com/kotekan/kotekan/kotekan:latest
-       docker build --cache-from=rocm/dev-ubuntu-18.04,docker.pkg.github.com/kotekan/kotekan/kotekan:latest -f tools/iwyu/docker/Dockerfile -t docker.pkg.github.com/kotekan/kotekan/kotekan:latest .
-       docker push docker.pkg.github.com/kotekan/kotekan/kotekan:latest
-   - name: Run iwyu
-     run: |
-       docker run --rm docker.pkg.github.com/kotekan/kotekan/kotekan:latest /code/tools/iwyu/docker/iwyu.sh
+jobs:
+
+  # Build the docker images
+  build-docker:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v1
+
+    - name: Build docker images
+      run: |
+        SECONDS=0
+        docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}" | while read line ; do echo "$SECONDS s | $line";  done;
+        docker pull --disable-content-trust ${IMG_CORE} | while read line ; do echo "$SECONDS s | $line";  done;
+        docker pull --disable-content-trust ${IMG_IWYU} | while read line ; do echo "$SECONDS s | $line";  done;
+        docker build --cache-from=rocm/dev-ubuntu-18.04,${IMG_CORE} -t ${IMG_CORE} - < tools/docker/Dockerfile | while read line ; do echo "$SECONDS s | $line";  done;
+        docker build --cache-from=rocm/dev-ubuntu-18.04,${IMG_CORE},${IMG_IWYU} -t ${IMG_IWYU} - < tools/iwyu/docker/Dockerfile | while read line ; do echo "$SECONDS s | $line";  done;
+        docker push ${IMG_CORE} | while read line ; do echo "$SECONDS s| $line";  done;
+        docker push ${IMG_IWYU} | while read line ; do echo "$SECONDS s| $line";  done;
+
+
+  # Build a basic version of kotekan
+  build-base:
+    runs-on: ubuntu-latest
+    needs: build-docker
+
+    steps:
+    - uses: actions/checkout@v1
+
+    - name: Cache ccache files
+      uses: actions/cache@v1
+      with:
+        path: .ccache
+        key: ccache-base-build-${{ github.sha }}
+        restore-keys: |
+          ccache-base-build
+          ccache-full-build
+
+    - name: Build kotekan
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_CORE})
+        docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}"
+        docker run "${OPTS[@]}" ccache -s
+        docker run "${OPTS[@]}" \
+          cmake -DCMAKE_BUILD_TYPE=Debug \
+            -DARCH=haswell \
+            -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
+        docker run "${OPTS[@]}" make -j 2
+        docker run "${OPTS[@]}" ccache -s
+
+
+  # Build a full version of kotekan and run the unit tests
+  build-full-test:
+    runs-on: ubuntu-latest
+    needs: build-docker
+
+    steps:
+    - uses: actions/checkout@v1
+
+    - name: Cache ccache files
+      uses: actions/cache@v1
+      with:
+        path: .ccache
+        key: ccache-full-build-${{ github.sha }}
+        restore-keys: |
+          ccache-full-build
+          ccache-base-build
+
+    - name: Build kotekan
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_CORE})
+        docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}"
+        docker run "${OPTS[@]}" ccache -s
+        docker run "${OPTS[@]}" \
+          cmake -DCMAKE_BUILD_TYPE=Debug \
+            -DUSE_HDF5=ON -DHIGHFIVE_PATH=/code/build/HighFive \
+            -DUSE_LAPACK=ON -DBLAZE_PATH=/code/build/blaze \
+            -DARCH=haswell \
+            -DNO_MEMLOCK=ON \
+            -DUSE_OMP=ON \
+            -DBOOST_TESTS=ON \
+            -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
+        docker run "${OPTS[@]}" make -j 2
+        docker run "${OPTS[@]}" ccache -s
+
+    - name: Run parallel python tests
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/tests/ -e PYTHONPATH=/code/kotekan/python/ ${IMG_CORE})
+        docker run "${OPTS[@]}" pytest -v -n auto --dist=loadfile -x -m 'not serial'
+
+    - name: Run serial python tests
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/tests/ -e PYTHONPATH=/code/kotekan/python/ ${IMG_CORE})
+        docker run "${OPTS[@]}" bash -c "redis-server --daemonize yes; pytest -v -x -m serial"
+
+    - name: Run boost tests
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/tests/ -e PYTHONPATH=/code/kotekan/python/ ${IMG_CORE})
+        docker run "${OPTS[@]}" pytest -v -x
+
+
+  # Build a full CHIME version of kotekan
+  build-chime:
+    runs-on: ubuntu-latest
+    needs: build-docker
+
+    steps:
+    - uses: actions/checkout@v1
+
+    - name: Cache ccache files
+      uses: actions/cache@v1
+      with:
+        path: .ccache
+        key: ccache-chime-build-${{ github.sha }}
+        restore-keys: |
+          ccache-chime-build
+          ccache-full-build
+
+    - name: Build kotekan
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_CORE})
+        docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}"
+        docker run "${OPTS[@]}" ccache -s
+        docker run "${OPTS[@]}" \
+          cmake -DCMAKE_BUILD_TYPE=Debug \
+            -DUSE_DPDK=ON \
+            -DUSE_HDF5=ON -DHIGHFIVE_PATH=/code/build/HighFive \
+            -DUSE_LAPACK=ON -DBLAZE_PATH=/code/build/blaze \
+            -DARCH=haswell \
+            -DNO_MEMLOCK=ON \
+            -DUSE_OMP=ON \
+            -DBOOST_TESTS=OFF \
+            -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache ..
+        docker run "${OPTS[@]}" make -j 2
+        docker run "${OPTS[@]}" ccache -s
+
+
+  iwyu:
+    runs-on: ubuntu-latest
+    needs: build-docker
+    if: github.base_ref == 'master'
+
+    steps:
+    - uses: actions/checkout@v1
+
+    - name: Configure kotekan
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_IWYU})
+        docker login docker.pkg.github.com -u ${GITHUB_REPOSITORY} -p "${{ secrets.GITHUB_TOKEN }}"
+        docker run "${OPTS[@]}" \
+          cmake -DCMAKE_BUILD_TYPE=Debug \
+            -DUSE_DPDK=ON \
+            -DUSE_HDF5=ON -DHIGHFIVE_PATH=/code/build/HighFive \
+            -DUSE_LAPACK=ON -DBLAZE_PATH=/code/build/blaze \
+            -DARCH=haswell \
+            -DNO_MEMLOCK=ON \
+            -DUSE_OMP=ON \
+            -DBOOST_TESTS=OFF \
+
+    - name: Run iwyu
+      run: |
+        OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_IWYU})
+        docker run "${OPTS[@]}" /code/tools/iwyu/docker/iwyu.sh

CMakeLists.txt
@@ -25,6 +25,7 @@ option(USE_DPDK "Enable DPDK Framework" OFF)
 option(USE_HDF5 "Build HDF5 output stages" OFF)
 option(USE_OMP "Enable OpenMP" OFF)
 option(USE_OLD_ROCM "Build for ROCm versions 2.3 or older" OFF)
+option(NO_MEMLOCK "Do not lock buffer memory (useful when running in Docker)" OFF)
 option(SUPERDEBUG "Enable extra debugging with no optimisation" OFF)
 option(SANITIZE "Enable clang sanitizers for testing" OFF)
 option(COMPILE_DOCS "Use Sphinx to compile documentation" OFF)
@@ -79,16 +80,20 @@ if (${USE_FFTW})
   include_directories(${FFTW_INCLUDES})
 endif()
 
+if (NOT DEFINED ARCH)
+  set(ARCH "native")
+endif()
+
+
 include(CheckIncludeFileCXX)
 if (${USE_LAPACK})
-  # Check OpenBLAS is installed
-  if (DEFINED OPENBLAS_PATH)
-    if (NOT EXISTS ${OPENBLAS_PATH})
-      MESSAGE( "OPENBLAS_PATH: \"${OPENBLAS_PATH}\" does not exist" )
-    endif()
-  endif()
-  find_package( OpenBLAS REQUIRED
-                HINTS ${OPENBLAS_PATH})
+
+  find_package( BLAS REQUIRED )
+  MESSAGE("Using BLAS ${BLAS_LIBRARIES}")
+  find_package( LAPACK REQUIRED )
+  MESSAGE("Using LAPACK ${LAPACK_LIBRARIES}")
+  find_package( LAPACKE REQUIRED )
+  MESSAGE("Using LAPACKE ${LAPACKE_LIBRARIES}")
 
   # Check Blaze is installed
   if (DEFINED BLAZE_PATH)
@@ -233,7 +238,7 @@ if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND NOT CMAKE_CXX_COMPILER_ID MATC
        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -lrt " )
 endif ()
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99 -O3 -D_GNU_SOURCE -Wall -Wextra -Werror -march=native -mtune=native -I/opt/rocm/include")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99 -O3 -D_GNU_SOURCE -Wall -Wextra -Werror -march=${ARCH} -mtune=${ARCH} -I/opt/rocm/include")
 
 # OpenMP flag
 if(${USE_OMP})
@@ -250,7 +255,7 @@ if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND NOT ${CMAKE_CXX_COMPILER_ID} M
        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lrt " )
 endif ()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GNU_SOURCE -O3 -Wall -Wextra -Werror -Wzero-as-null-pointer-constant -march=native -mtune=native -I/opt/rocm/include")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GNU_SOURCE -O3 -Wall -Wextra -Werror -Wzero-as-null-pointer-constant -march=${ARCH} -mtune=${ARCH} -I/opt/rocm/include")
 
 # dpdk uses the register keyword that got removed in C++17 because it wasn't doing anything
 #TODO remove this line when dpdk people fix this: https://mails.dpdk.org/archives/dev/2018-July/107950.html
@@ -300,6 +305,12 @@ if (${COMPILE_DOCS})
     add_subdirectory(docs EXCLUDE_FROM_ALL)
 endif()
 
+# Turn off memlocking of buffers. This is useful when running in restricted
+# environments e.g. Docker containers
+if (${NO_MEMLOCK})
+  MESSAGE("Do not lock buffer memory.")
+  add_definitions(-DWITH_NO_MEMLOCK)
+endif ()
 
 # Improve debugging by turning off all optimisations.
 if (${SUPERDEBUG})

LICENSE
@@ -37,8 +37,8 @@ THE SOFTWARE.
 ===== Formatting library for C++ =====
 
 Files:
-  include/ftm.hpp
-  include/ftm/*
+  include/fmt.hpp
+  include/fmt/*
 
 ===
 Formatting library for C++
@@ -172,7 +172,7 @@ DEALINGS WITH THE SOFTWARE.
 ===== FindHCC.cmake =====
 
 Files:
-  camke/FindHCC.cmake
+  cmake/FindHCC.cmake
 
 ===
 Copyright (C) 2016 Advanced Micro Devices, Inc. All rights reserved.
@@ -195,3 +195,17 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
 CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ===
 
+
+===== FindLAPACKE.cmake =====
+
+Files:
+    cmake/FindLAPACKE.cmake
+
+===
+Copyright 2016 Hans J. Johnson <hans-johnson@uiowa.edu>
+
+Distributed under the OSI-approved BSD License (the "License")
+
+This software is distributed WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+===

cmake/FindLAPACKE.cmake
@@ -1,0 +1,191 @@
+# FindLAPACKE
+# -------------
+#
+# Find the LAPACKE library
+#
+# Using LAPACKE:
+#
+# ::
+#
+#   find_package(LAPACKE REQUIRED)
+#   include_directories(${LAPACKE_INCLUDE_DIRS})
+#   add_executable(foo foo.cc)
+#   target_link_libraries(foo ${LAPACKE_LIBRARIES})
+#
+# This module sets the following variables:
+#
+# ::
+#
+#   LAPACKE_FOUND - set to true if the library is found
+#   LAPACKE_INCLUDE_DIRS - list of required include directories
+#   LAPACKE_LIBRARIES - list of libraries to be linked
+#   LAPACKE_VERSION_MAJOR - major version number
+#   LAPACKE_VERSION_MINOR - minor version number
+#   LAPACKE_VERSION_PATCH - patch version number
+#   LAPACKE_VERSION_STRING - version number as a string (ex: "0.2.18")
+
+#=============================================================================
+# Copyright 2016 Hans J. Johnson <hans-johnson@uiowa.edu>
+#
+# Distributed under the OSI-approved BSD License (the "License")
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#=============================================================================
+#
+set(LAPACKE_SEARCH_PATHS
+  ${LAPACKE_DIR}
+  $ENV{LAPACKE_DIR}
+  $ENV{CMAKE_PREFIX_PATH}
+  ${CMAKE_PREFIX_PATH}
+  /usr
+  /usr/openblas
+  /usr/local
+  /usr/local/opt/lapack  ## Mac Homebrew install path
+  /opt/local
+  /opt/LAPACKE
+)
+message(STATUS "LAPACKE_SEARCH_PATHS: ${LAPACKE_SEARCH_PATHS}")
+
+set(CMAKE_PREFIX_PATH ${LAPACKE_SEARCH_PATHS})
+list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
+
+## First try to find LAPACKE with NO_MODULE,
+## As of 20160706 version 0.2.18 there is limited cmake support for LAPACKE
+## that is not as complete as this version, if found, use it
+## to identify the LAPACKE_VERSION_STRING and improve searching.
+find_package(LAPACKE NO_MODULE QUIET)
+if(LAPACKE_FOUND)
+  if(EXISTS ${LAPACKE_DIR}/lapacke-config-version.cmake)
+    include(${LAPACKE_DIR}/lapacke-config-version.cmake)
+    set(LAPACKE_VERSION_STRING ${PACKAGE_VERSION})
+    unset(PACKAGE_VERSION) # Use cmake conventional naming
+  endif()
+  find_package(LAPACK NO_MODULE QUIET) #Require matching versions here!
+  find_package(BLAS NO_MODULE QUIET)   #Require matching versions here!
+endif()
+
+##################################################################################################
+### First search for headers
+MESSAGE(" MESSAGES " ${LAPACKE_SEARCH_PATHS})
+find_path(LAPACKE_CBLAS_INCLUDE_DIR 
+             NAMES cblas.h 
+             PATHS ${LAPACKE_SEARCH_PATHS} 
+             PATH_SUFFIXES include include/lapack include/openblas include/lapacke)
+find_path(LAPACKE_LAPACKE_INCLUDE_DIR 
+             NAMES lapacke.h 
+             PATHS ${LAPACKE_SEARCH_PATHS} 
+             PATH_SUFFIXES include include/lapack include/lapacke include/openblas)
+
+##################################################################################################
+### Second, search for libraries
+set(PATH_SUFFIXES_LIST
+  lib64
+  lib
+  lib/lapack
+)
+find_library(LAPACKE_LIB 
+                 NAMES lapacke
+                 PATHS ${LAPACKE_SEARCH_PATHS}
+                 PATH_SUFFIXES ${PATH_SUFFIXES_LIST})
+find_library(CBLAS_LIB 
+                 NAMES cblas
+                 PATHS ${LAPACKE_SEARCH_PATHS}
+                 PATH_SUFFIXES ${PATH_SUFFIXES_LIST})
+find_library(LAPACK_LIB 
+                 NAMES lapack
+                 PATHS ${LAPACKE_SEARCH_PATHS}
+                 PATH_SUFFIXES ${PATH_SUFFIXES_LIST})
+find_library(BLAS_LIB 
+                 NAMES blas
+                 PATHS ${LAPACKE_SEARCH_PATHS}
+                 PATH_SUFFIXES ${PATH_SUFFIXES_LIST})
+
+## TODO: Get version components
+# ------------------------------------------------------------------------
+#  Extract version information
+# ------------------------------------------------------------------------
+
+# WARNING: We may not be able to determine the version of some LAPACKE
+set(LAPACKE_VERSION_MAJOR 0)
+set(LAPACKE_VERSION_MINOR 0)
+set(LAPACKE_VERSION_PATCH 0)
+if(LAPACKE_VERSION_STRING)
+  string(REGEX REPLACE "([0-9]+).([0-9]+).([0-9]+)" "\\1" LAPACKE_VERSION_MAJOR "${LAPACKE_VERSION_STRING}")
+  string(REGEX REPLACE "([0-9]+).([0-9]+).([0-9]+)" "\\2" LAPACKE_VERSION_MINOR "${LAPACKE_VERSION_STRING}")
+  string(REGEX REPLACE "([0-9]+).([0-9]+).([0-9]+)" "\\3" LAPACKE_VERSION_PATCH "${LAPACKE_VERSION_STRING}")
+endif()
+
+#======================
+# Checks 'REQUIRED', 'QUIET' and versions.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LAPACKE FOUND_VAR LAPACKE_FOUND
+  REQUIRED_VARS LAPACKE_CBLAS_INCLUDE_DIR
+                LAPACKE_LAPACKE_INCLUDE_DIR
+                LAPACKE_LIB
+                LAPACK_LIB
+  VERSION_VAR LAPACKE_VERSION_STRING
+)
+
+if (LAPACKE_FOUND)
+  set(LAPACKE_INCLUDE_DIRS ${LAPACKE_CBLAS_INCLUDE_DIR} ${LAPACKE_CBLAS_INCLUDE_DIR})
+  list(REMOVE_DUPLICATES LAPACKE_INCLUDE_DIRS)
+  if("${CMAKE_C_COMPILER_ID}" MATCHES ".*Clang.*" OR
+     "${CMAKE_C_COMPILER_ID}" MATCHES ".*GNU.*" OR
+     "${CMAKE_C_COMPILER_ID}" MATCHES ".*Intel.*"
+      ) #NOT MSVC
+    set(MATH_LIB m)
+  endif()
+  list(APPEND LAPACKE_LIBRARIES ${LAPACKE_LIB} ${LAPACK_LIB} ${BLAS_LIB} )
+  # Check for a common combination, and find required gfortran support libraries
+
+  if(1)
+    if("${CMAKE_C_COMPILER_ID}" MATCHES ".*Clang.*" AND "${CMAKE_Fortran_COMPILER_ID}" MATCHES "GNU")
+          message(STATUS "\n\n WARNING: ${CMAKE_C_COMPILER} identified as ${CMAKE_C_COMPILER_ID}\n"
+                                   "AND: ${CMAKE_Fortran_COMPILER} identified as ${CMAKE_Fortran_COMPILER_ID}\n"
+                               "\n"
+                               "may be require special configurations.  The most common is the need to"
+                               "explicitly link C programs against the gfortran support library.")
+                              
+    endif()
+  else()
+    ## This code automated code is hard to determine if it is robust in many different environments.
+    # Check for a common combination, and find required gfortran support libraries
+    if("${CMAKE_C_COMPILER_ID}" MATCHES ".*Clang.*" AND "${CMAKE_Fortran_COMPILER_ID}" MATCHES "GNU")
+       include(FortranCInterface)
+       FortranCInterface_VERIFY() 
+       if(NOT FortranCInterface_VERIFIED_C)
+          message(FATAL_ERROR "C and fortran compilers are not compatible:\n${CMAKE_Fortran_COMPILER}:${CMAKE_C_COMPILER}")
+       endif()
+       
+       execute_process(COMMAND ${CMAKE_Fortran_COMPILER} -print-file-name=libgfortran.a OUTPUT_VARIABLE FORTRANSUPPORTLIB ERROR_QUIET)
+       string(STRIP ${FORTRANSUPPORTLIB} FORTRANSUPPORTLIB)
+       if(EXISTS "${FORTRANSUPPORTLIB}")
+         list(APPEND LAPACKE_LIBRARIES ${FORTRANSUPPORTLIB})
+         message(STATUS "Appending fortran support lib: ${FORTRANSUPPORTLIB}")
+       else()
+         message(FATAL_ERROR "COULD NOT FIND libgfortran.a support library:${FORTRANSUPPORTLIB}:")
+       endif()
+    endif()
+  endif()
+  list(APPEND LAPACKE_LIBRARIES ${MATH_LIB})
+endif()
+
+mark_as_advanced(
+  LAPACKE_FOUND
+  LAPACKE_INCLUDE_DIRS
+  LAPACKE_LIBRARIES
+  LAPACKE_VERSION_MAJOR
+  LAPACKE_VERSION_MINOR
+  LAPACKE_VERSION_PATCH
+  LAPACKE_VERSION_STRING
+)
+
+## For debugging
+message(STATUS "LAPACKE_FOUND                  :${LAPACKE_FOUND}:  - set to true if the library is found")
+message(STATUS "LAPACKE_INCLUDE_DIRS           :${LAPACKE_INCLUDE_DIRS}: - list of required include directories")
+message(STATUS "LAPACKE_LIBRARIES              :${LAPACKE_LIBRARIES}: - list of libraries to be linked")
+message(STATUS "LAPACKE_VERSION_MAJOR          :${LAPACKE_VERSION_MAJOR}: - major version number")
+message(STATUS "LAPACKE_VERSION_MINOR          :${LAPACKE_VERSION_MINOR}: - minor version number")
+message(STATUS "LAPACKE_VERSION_PATCH          :${LAPACKE_VERSION_PATCH}: - patch version number")
+message(STATUS "LAPACKE_VERSION_STRING         :${LAPACKE_VERSION_STRING}: - version number as a string")

lib/core/buffer.c
@@ -870,6 +870,7 @@ uint8_t* buffer_malloc(ssize_t len, int numa_node) {
     }
 #endif
 
+#ifndef WITH_NO_MEMLOCK
     // Ask that all pages be kept in memory
     err = mlock((void*)frame, len);
 
@@ -878,6 +879,9 @@ uint8_t* buffer_malloc(ssize_t len, int numa_node) {
         free(frame);
         return NULL;
     }
+#else
+    (void)err;
+#endif
 #endif
     // Zero the new frame
     memset(frame, 0x0, len);

lib/core/configUpdater.cpp
@@ -97,8 +97,15 @@ void configUpdater::subscribe(const Stage* subscriber, std::function<bool(json&)
 
 void configUpdater::subscribe(const Stage* subscriber,
                               std::map<std::string, std::function<bool(json&)>> callbacks) {
+
+    // If no callbacks are passed, then we don't need to find any updatable_config blocks
+    if (callbacks.size() == 0) return;
+
+    // Find the nearest updatable config block
     auto updatable_config_paths = _config->get<std::map<std::string, std::string>>(
         subscriber->get_unique_name(), "updatable_config");
+
+    // Extract the paths from that config block, it must contain all callbacks
     for (auto callback : callbacks) {
         std::string path;
         try {

lib/core/configUpdater.hpp
@@ -121,8 +121,9 @@ public:
      * The callback function has to return True on success and False
      * otherwise.
      * The block of the calling stage in the configuration file should have
-     * a key named "updatable_block" that defines the full path to the
-     * updatable block.
+     * a key named "updatable_config" that defines the full path to the
+     * updatable block. As usual if not found at that level, it will search up
+     * the tree.
      *
      * @param subscriber Reference to the subscribing stage.
      * @param callback   Callback function for attribute updates.
@@ -134,10 +135,12 @@ public:
      *
      * The callback functions have to return True on success and False
      * otherwise.
-     * The block of the calling stage in the configuration file should have
-     * an object named "updatable_block" with values that define the full
-     * path to an updatable block, each. The names in the callbacks map refer
-     * to the names of these values.
+     * The block of the calling stage in the configuration file should have an
+     * object named "updatable_config" with values that define the full path to
+     * an updatable block, each. The names in the callbacks map refer to the
+     * names of these values. If an "updatable_config" block is not found in
+     * the current stage it will search up the config tree, but all callback
+     * keys must be contained within this single block.
      *
      * @param subscriber Reference to the subscribing stage.
      * @param callbacks  Map of value names and callback functions.

lib/stages/CMakeLists.txt
@@ -73,8 +73,6 @@ endif ()
 
 # HDF5 stuff
 if (${USE_HDF5})
-  # Disable for the moment as requires too modern HDF5 not available in most computers
-  #set (KOTEKAN_PROCESS_LIB_SOURCES ${KOTEKAN_PROCESS_LIB_SOURCES} calibration_io.cpp)
   set (KOTEKAN_PROCESS_LIB_SOURCES ${KOTEKAN_PROCESS_LIB_SOURCES} basebandReadout.cpp applyGains.cpp)
 endif ()
 
@@ -112,8 +110,10 @@ if (${USE_EVENT})
 endif ()
 
 if (${USE_LAPACK})
-  include_directories (${OpenBLAS_INCLUDE_DIRS} ${LAPACK_INCLUDE_DIRS})
-  target_link_libraries ( kotekan_stages  ${OpenBLAS_LIBRARIES} ${LAPACK_LIBRARIES})
+  include_directories( ${LAPACKE_INCLUDE_DIRS} )
+  target_link_libraries( kotekan_stages ${BLAS_LIBRARIES} )
+  target_link_libraries( kotekan_stages ${LAPACK_LIBRARIES} )
+  target_link_libraries( kotekan_stages ${LAPACKE_LIBRARIES} )
 endif ()
 
 # Add atomic for building against GCC

commit e86996db2e50406d4a209d15971bfa18be2d3599

ci(github): fix iwyu action
The iqyu action broke in #699 and is fixed here:
- restore iwyu cmake options
- add path (..) to cmake command
- fix path to iwyu.sh
- iwyu.sh: fix path to project .imp

.github/workflows/main.yml
@@ -173,14 +173,16 @@ jobs:
         docker run "${OPTS[@]}" \
           cmake -DCMAKE_BUILD_TYPE=Debug \
             -DUSE_DPDK=ON \
+            -DUSE_HSA=ON \
             -DUSE_HDF5=ON -DHIGHFIVE_PATH=/code/build/HighFive \
             -DUSE_LAPACK=ON -DBLAZE_PATH=/code/build/blaze \
             -DARCH=haswell \
             -DNO_MEMLOCK=ON \
             -DUSE_OMP=ON \
-            -DBOOST_TESTS=OFF \
+            -DBOOST_TESTS=ON \
+            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
     - name: Run iwyu
       run: |
         OPTS=(--rm --mount type=bind,src=$(pwd),target=/code/kotekan -w /code/kotekan/build/ ${IMG_IWYU})
-        docker run "${OPTS[@]}" /code/tools/iwyu/docker/iwyu.sh
+        docker run "${OPTS[@]}" /code/kotekan/tools/iwyu/docker/iwyu.sh

tools/iwyu/docker/iwyu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-iwyu_tool -j 2 -p . -- -Xiwyu --no_fwd_decls -Xiwyu --mapping_file=/code/iwyu.kotekan.imp -Xiwyu --max_line_length=100 | tee iwyu.out
+iwyu_tool -j 2 -p . -- -Xiwyu --no_fwd_decls -Xiwyu --mapping_file=/code/kotekan/iwyu.kotekan.imp -Xiwyu --max_line_length=100 | tee iwyu.out
 python2 /usr/bin/fix_include --nosafe_headers --comments < iwyu.out