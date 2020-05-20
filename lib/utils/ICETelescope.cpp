20dad5781e6f30efb397e8f7ce99010b86e9b2b5
Richard Shaw <richard@phas.ubc.ca>
2020-05-20 16:00:33 -0700

    feat(Telescope): abstract telescope specific functionality
    This abstracts telescope specific functionality such as mapping
    stream_id -> freq_id -> physical freq; and mapping fpga_sequence numbers
    to times.

---
+++ lib/utils/ICETelescope.cpp
@@ -0,0 +1,152 @@
+#include "ICETelescope.hpp"
+
+#include "Telescope.hpp"
+#include "chimeMetadata.h"
+#include "visUtil.hpp"
+
+#include <cmath>
+#include <fmt.hpp>
+
+
+REGISTER_TELESCOPE(ICETelescope, "ice");
+
+#define GIGA 1000000000
+
+ICETelescope::ICETelescope(const kotekan::Config& config, const std::string& path) :
+    Telescope(config.get<std::string>(path, "log_level")) {
+
+    // TODO: rename this parameter to `num_freq_per_stream` in the config
+    _num_freq_per_stream = config.get<uint32_t>(path, "num_local_freq");
+
+    set_sampling_params(config, path);
+    set_gps(config, path);
+}
+
+
+void ICETelescope::set_sampling_params(const kotekan::Config& config, const std::string& path) {
+    set_sampling_params(config.get<double>(path, "sampling_rate"),
+                        config.get<uint32_t>(path, "frame_length"),
+                        config.get<uint8_t>(path, "nyquist_zone"));
+}
+
+void ICETelescope::set_sampling_params(double sample_rate, uint32_t length, uint8_t zone) {
+    // Set the physical frequency of id=0, and the spacing, taking into account
+    // the aliasing of each Nyquist zone
+    freq0_MHz = (zone / 2) * sample_rate;
+    df_MHz = (zone % 2 ? 1 : -1) * sample_rate / length;
+    nfreq = length / 2;
+
+    // TODO: revisit this if we think the length might ever not be an integer
+    // number of ns
+    dt_ns = 1e3 / sample_rate * length;
+}
+
+void ICETelescope::set_gps(const kotekan::Config& config, const std::string& path) {
+
+    auto require_gps = config.get_default<bool>(path, "require_gps", true);
+
+    if (!config.exists("/", "gps_time")) {
+        if (require_gps) {
+            FATAL_ERROR("GPS time section is required, but was not found in the config.");
+        } else {
+            WARN("No GPS time section found. Ignoring.");
+        }
+        return;
+    }
+
+    if (config.exists("/gps_time", "error")) {
+        auto error_message = config.get<std::string>("/gps_time", "error");
+
+        if (require_gps) {
+            FATAL_ERROR("Required GPS time lookup failed with reason: \n {:s}\n", error_message);
+        } else {
+            WARN("GPS time lookup failed with reason: \n {:s}\n", error_message);
+        }
+        return;
+    }
+
+    if (!config.exists("/gps_time", "frame0_nano")) {
+        if (require_gps) {
+            FATAL_ERROR("GPS frame0 time is required, but was not found in the config.");
+        } else {
+            WARN("No GPS frame0 time found. Ignoring.");
+        }
+        return;
+    }
+
+    time0_ns = config.get<uint64_t>("/gps_time", "frame0_nano");
+    gps_enabled = true;
+}
+
+freq_id_t ICETelescope::to_freq_id(stream_t stream, uint32_t /* ind */) const {
+    auto stream_id = extract_stream_id(stream);
+    return bin_number_chime(&stream_id);
+}
+
+
+double ICETelescope::to_freq(freq_id_t freq_id) const {
+
+    if (freq_id >= nfreq) {
+        throw std::invalid_argument(
+            fmt::format("Invalid frequency ID={}, accepted ranged 0 <= id < {}", freq_id, nfreq));
+    }
+
+    return freq0_MHz + freq_id * df_MHz;
+}
+
+
+uint32_t ICETelescope::num_freq_per_stream() const {
+    return _num_freq_per_stream;
+}
+
+uint32_t ICETelescope::num_freq() const {
+    return nfreq;
+}
+
+double ICETelescope::freq_width(freq_id_t freq_id) const {
+
+    if (freq_id >= nfreq) {
+        throw std::invalid_argument(
+            fmt::format("Invalid frequency ID={}, accepted ranged 0 <= id < {}", freq_id, nfreq));
+    }
+
+    return std::abs(df_MHz);
+}
+
+timespec ICETelescope::to_time(uint64_t seq) const {
+    auto time_ns = time0_ns + seq * dt_ns;
+    return {(time_t)(time_ns / GIGA), (long)(time_ns % GIGA)};
+}
+
+uint64_t ICETelescope::to_seq(timespec time) const {
+    return (time.tv_sec * GIGA + time.tv_nsec - time0_ns) / dt_ns;
+}
+
+
+bool ICETelescope::gps_time_enabled() const {
+    return true;
+}
+
+uint64_t ICETelescope::seq_length_nsec() const {
+    return dt_ns;
+}
+
+ice_stream_id_t ice_extract_stream_id(const uint16_t encoded_stream_id) {
+    ice_stream_id_t stream_id;
+
+    stream_id.link_id = encoded_stream_id & 0x000F;
+    stream_id.slot_id = (encoded_stream_id & 0x00F0) >> 4;
+    stream_id.crate_id = (encoded_stream_id & 0x0F00) >> 8;
+    stream_id.unused = (encoded_stream_id & 0xF000) >> 12;
+
+    return stream_id;
+}
+
+uint16_t ice_encode_stream_id(const ice_stream_id_t s_stream_id) {
+    uint16_t stream_id;
+
+    stream_id = (s_stream_id.link_id & 0xF) + ((s_stream_id.slot_id & 0xF) << 4)
+                + ((s_stream_id.crate_id & 0xF) << 8) + ((s_stream_id.unused & 0xF) << 12);
+
+    return stream_id;
+}
---
+++ lib/utils/Telescope.hpp
@@ -0,0 +1,203 @@
+#ifndef TELESCOPE_HPP
+#define TELESCOPE_HPP
+
+#include "Config.hpp" // for Config
+#include "buffer.h"
+#include "factory.hpp"        // for REGISTER_NAMED_TYPE_WITH_FACTORY, CREATE_FACTORY, Factory
+#include "kotekanLogging.hpp" // for kotekanLogging
+
+#include <memory>
+#include <stddef.h> // for size_t
+#include <stdint.h> // for int32_t, uint32_t
+#include <string>   // for string
+#include <time.h>
+
+// Create the abstract factory for generating patterns
+class Telescope;
+
+CREATE_FACTORY(Telescope, const kotekan::Config&, const std::string&);
+#define REGISTER_TELESCOPE(TelescopeType, name)                                                    \
+    REGISTER_NAMED_TYPE_WITH_FACTORY(Telescope, TelescopeType, name)
+
+
+using stream_t = uint64_t;
+using freq_id_t = uint32_t;
+
+/**
+ * @brief A class to hold telescope specific functionality.
+ *
+ *
+ **/
+class Telescope : public kotekan::kotekanLogging {
+
+public:
+    /**
+     * @brief Construct the telescope singleton.
+     *
+     * @param  config  Kotekan configuration.
+     *
+     * @returns        A reference to the singleton instance.
+     **/
+    static const Telescope& instance(const kotekan::Config&);
+
+    /**
+     * @brief Get a reference to the singleton Telescope instance.
+     *
+     * @returns   The telescope instance.
+     **/
+    static const Telescope& instance();
+
+
+    virtual ~Telescope() = default;
+
+    /**
+     * Get the frequency ID from the FPGA stream ID.
+     *
+     * @param  stream  The generic stream ID.
+     *
+     * @returns        The integer frequency ID.
+     **/
+    virtual freq_id_t to_freq_id(stream_t stream) const;
+
+
+    /**
+     * Get the frequency ID from the FPGA stream ID.
+     *
+     * @param  stream  The generic stream ID.
+     * @param  ind     The index for multifrequency streams.
+     *
+     * @returns        The integer frequency ID.
+     **/
+    virtual freq_id_t to_freq_id(stream_t stream, uint32_t ind) const = 0;
+
+    /**
+     * Get the frequency ID from the FPGA stream ID.
+     *
+     * @param  buf  The buffer object.
+     * @param  ID   Index of the frame in the buffer.
+     *
+     * @returns     The integer frequency ID.
+     **/
+    virtual freq_id_t to_freq_id(const Buffer* buf, int ID) const;
+
+    /**
+     * Get the frequency ID from the FPGA stream ID.
+     *
+     * @param  buf  The buffer object.
+     * @param  ID   Index of the frame in the buffer.
+     * @param  ind  The index for the multifrequency stream.
+     *
+     * @returns     The integer frequency ID.
+     **/
+    virtual freq_id_t to_freq_id(const Buffer* buf, int ID, uint32_t ind) const;
+
+    /**
+     * Get the physical frequency in MHz of the specified freq ID.
+     *
+     * @param  freq_id  The frequency ID.
+     *
+     * @returns         The central frequency in MHz.
+     **/
+    virtual double to_freq(freq_id_t freq_id) const = 0;
+
+
+    /**
+     * Get the physical frequency in MHz of the specified channel.
+     *
+     * The baseclass implementation just calls
+     * `to_freq(to_freq_id(args))`, override with a custom implementation
+     * to save a function call.
+     *
+     * @param  args  Any arguments accepted by `to_freq_id`.
+     *
+     * @returns      The central frequency in MHz.
+     **/
+    template<typename... Args>
+    double to_freq(Args... args) const {
+        return to_freq(to_freq_id(args...));
+    }
+
+
+    /**
+     * @brief Get the number of frequencies per stream.
+     *
+     * @return  The number of frequencies on a stream.
+     **/
+    virtual uint32_t num_freq_per_stream() const = 0;
+
+
+    /**
+     * @brief Get the total number of frequencies channels.
+     *
+     * This is the upper bound for freq_id.
+     *
+     * @return  The total number of frequency channels.
+     **/
+    virtual uint32_t num_freq() const = 0;
+
+    /**
+     * @brief Get the frequency width of a given channel.
+     *
+     * @return  The width of the frequency channel in MHz.
+     **/
+    virtual double freq_width(freq_id_t freq_id) const = 0;
+
+    /**
+     * Convert a sequence number into a UNIX epoch time.
+     *
+     * @param  seq  The sequence number.
+     *
+     * @return  The corresponding UNIX time.
+     **/
+    virtual timespec to_time(uint64_t seq) const = 0;
+
+
+    /**
+     * @brief Convert a UNIX epoch time into the nearest sequence number.
+     *
+     * @note When there is not an exact correspondence between the given time
+     *       and FPGA sequence numbers, this routine will return the latest valid
+     *       FPGA sequence number before the given timestamp.
+     *
+     * @param  time  The UNIX time.
+     *
+     * @return  The corresponding sequence number.
+     **/
+    virtual uint64_t to_seq(timespec time) const = 0;
+
+
+    /**
+     * @brief Is a precise time source available?
+     *
+     * @return  True if the GPS time is available.
+     **/
+    virtual bool gps_time_enabled() const = 0;
+
+
+    /**
+     * @brief Get the length of an FPGA sequence number tick.
+     *
+     * @return  Length of an FPGA sequence number tick.
+     **/
+    virtual timespec seq_length() const;
+
+    /**
+     * @brief Get the length of an FPGA sequence number tick.
+     *
+     * @return  Length of an FPGA sequence number tick.
+     **/
+    virtual uint64_t seq_length_nsec() const = 0;
+
+private:
+    static inline std::unique_ptr<Telescope> tel_instance = nullptr;
+
+protected:
+    /**
+     * This constructor sets up the logging. Implement a specific constructor
+     * in a derived class to parse the config, and call this one to make sure
+     * the logging is done correctly.
+     **/
+    Telescope(const std::string& log_level);
+};
+
+#endif // TELESCOPE_HPP
