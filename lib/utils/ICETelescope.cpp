fe5912a96c729d63a28702c9456424b6a45586e1
Richard Shaw <richard@phas.ubc.ca>
2020-05-26 17:20:47 -0700

    refactor(stream_t): explicitly type stream IDs

--- lib/utils/ICETelescope.cpp
+++ lib/utils/ICETelescope.cpp
@@ -144,22 +144,24 @@ uint64_t ICETelescope::seq_length_nsec() const {
     return dt_ns;
 }
 
-ice_stream_id_t ice_extract_stream_id(const uint16_t encoded_stream_id) {
+ice_stream_id_t ice_extract_stream_id(const stream_t encoded_stream_id) {
     ice_stream_id_t stream_id;
 
-    stream_id.link_id = encoded_stream_id & 0x000F;
-    stream_id.slot_id = (encoded_stream_id & 0x00F0) >> 4;
-    stream_id.crate_id = (encoded_stream_id & 0x0F00) >> 8;
-    stream_id.unused = (encoded_stream_id & 0xF000) >> 12;
+    uint16_t encoded_id = (uint16_t)(encoded_stream_id.id);
+
+    stream_id.link_id = encoded_id & 0x000F;
+    stream_id.slot_id = (encoded_id & 0x00F0) >> 4;
+    stream_id.crate_id = (encoded_id & 0x0F00) >> 8;
+    stream_id.unused = (encoded_id & 0xF000) >> 12;
 
     return stream_id;
 }
 
-uint16_t ice_encode_stream_id(const ice_stream_id_t s_stream_id) {
+stream_t ice_encode_stream_id(const ice_stream_id_t s_stream_id) {
     uint16_t stream_id;
 
     stream_id = (s_stream_id.link_id & 0xF) + ((s_stream_id.slot_id & 0xF) << 4)
                 + ((s_stream_id.crate_id & 0xF) << 8) + ((s_stream_id.unused & 0xF) << 12);
 
-    return stream_id;
+    return {(uint64_t)stream_id};
 }
