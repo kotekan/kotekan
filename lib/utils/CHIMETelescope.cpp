8c9730be2e0595f0d25514aa74e6a145ec03bec7
Andre Renard <andre@renard.io>
2020-08-28 12:57:21 -0400

    Update telescope objects to support more systems and query gps/fmap

--- lib/utils/CHIMETelescope.cpp
+++ lib/utils/CHIMETelescope.cpp
@@ -3,6 +3,7 @@
 #include "ICETelescope.hpp"   // for ice_stream_id_t, ice_encode_stream_id, ice_extract_stream_id
 #include "Telescope.hpp"      // for freq_id_t, REGISTER_TELESCOPE, _factory_aliasTelescope
 #include "kotekanLogging.hpp" // for ERROR, DEBUG2, WARN
+#include "restClient.hpp"
 
 #include "fmt.hpp"  // for format
 #include "json.hpp" // for json, basic_json<>::object_t, basic_json
@@ -15,7 +16,7 @@
 #include <vector>    // for vector
 
 
-REGISTER_TELESCOPE(CHIMETelescope, "chime");
+REGISTER_TELESCOPE(CHIMETelescope, "CHIMETelescope");
 
 CHIMETelescope::CHIMETelescope(const kotekan::Config& config, const std::string& path) :
     ICETelescope(config.get<std::string>(path, "log_level")) {
@@ -23,21 +24,64 @@ CHIMETelescope::CHIMETelescope(const kotekan::Config& config, const std::string&
     // This is always 1 for CHIME
     _num_freq_per_stream = 1;
 
+    set_sampling_params(800.0, 2048, 2);
+
+    // Get the GPS time, either from the config or fpga_master
+    bool require_gps = config.get_default<bool>(path, "require_gps", true);
+    _query_gps = config.get_default<bool>(path, "query_gps", false);
+    _gps_host = config.get_default<std::string>(path, "gps_host", "10.1.13.1");
+    _gps_port = config.get_default<uint32_t>(path, "gps_port", 54321);
+    _gps_endpoint = config.get_default<std::string>(path, "gps_endpoint", "/get-frame0-time");
+    if (_query_gps)
+        set_gps(_gps_host, _gps_port, _gps_endpoint);
+    if (!gps_enabled)
+        set_gps(config);
+
     _require_frequency_map = config.get_default<bool>(path, "require_frequency_map", false);
     _allow_default_frequency_map =
         config.get_default<bool>(path, "allow_default_frequency_map", true);
 
-    set_sampling_params(800.0, 2048, 2);
-    set_gps(config, path);
-    set_frequency_map(config, "fpga_frequency_map");
+    // Get the frequency map, either from config or fpga_master
+    _query_frequency_map = config.get_default<bool>(path, "query_frequency_map", false);
+    _frequency_map_host = config.get_default<std::string>(path, "frequency_map_host", "10.1.13.1");
+    _frequency_map_port = config.get_default<uint32_t>(path, "frequency_map_port", 54321);
+    _frequency_map_endpoint = config.get_default<std::string>(path, "frequency_map_endpoint",
+                                                              "/get-frequency-map");
+
+    nlohmann::json fpga_freq_map_json;
+    if (_query_frequency_map)
+        fpga_freq_map_json = get_frequency_map(_frequency_map_host, _frequency_map_port,
+                                               _frequency_map_endpoint);
+    else
+        fpga_freq_map_json = config.get_default<nlohmann::json>("/", "fpga_frequency_map", {});
+
+    set_frequency_map(fpga_freq_map_json);
+
+    if (require_gps && !gps_enabled) {
+        throw std::runtime_error("The system requires a GPS time, but none was found.");
+    }
 }
 
-void CHIMETelescope::set_frequency_map(const kotekan::Config& config, const std::string& path) {
-    nlohmann::json fpga_freq_map_json = config.get_default<nlohmann::json>("/", path, {});
+nlohmann::json CHIMETelescope::get_frequency_map(const std::string& host, const uint32_t port,
+                                                 const std::string& path) {
+    INFO("Requesting frequency map from server: {:s}:{:d}{:s} This might take some time...",
+         host, port, path);
+    auto reply = restClient::instance().make_request_blocking(path, {{"format", "s:b"}},
+                                                              host, port, 0, 30);
 
+    if (!reply.first) {
+        WARN("Failed to get frequency map from {:s}:{:d}{:s}");
+        return nlohmann::json();
+    }
+
+    auto json_reply = nlohmann::json::parse(reply.second);
+    return json_reply;
+}
+
+void CHIMETelescope::set_frequency_map(nlohmann::json &fpga_freq_map_json) {
     if (fpga_freq_map_json.empty()) {
         if (_require_frequency_map) {
-            std::string msg = fmt::format("Frequency map required by not found at /{:s}", path);
+            std::string msg = fmt::format("Frequency map required but not found");
             ERROR("{:s}", msg);
             throw std::runtime_error(msg);
         }
@@ -75,7 +119,7 @@ void CHIMETelescope::set_frequency_map(const kotekan::Config& config, const std:
                 fpga_freq_map_json.at("fmap").get<std::map<std::string, std::vector<int>>>();
         } catch (std::exception const& e) {
             std::string msg =
-                fmt::format("Could not read the fmap table at /{:s}/fmap: {:s}", path, e.what());
+                fmt::format("Could not read the fmap table: {:s}", e.what());
             ERROR("{:s}", msg);
             throw std::runtime_error(msg);
         }
@@ -107,6 +151,7 @@ void CHIMETelescope::set_frequency_map(const kotekan::Config& config, const std:
                 frequency_table[ice_encode_stream_id(post_shuffle_id).id] = bin;
             }
         }
+        INFO("Using custom frequency map.");
     }
 }
 
