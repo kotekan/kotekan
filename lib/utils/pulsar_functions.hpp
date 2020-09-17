f72dbcebf30a7ffc89633cb132b4be0af0c40be1
Emmanuel Fonseca <efonseca@physics.mcgill.ca>
2020-09-17 17:11:31 -0400

    first attempt at doxygen entry.

--- lib/utils/pulsar_functions.hpp
+++ lib/utils/pulsar_functions.hpp
@@ -3,6 +3,21 @@
 
 #include <inttypes.h>
 
+/**
+ * @struct PSRHeader
+ * @brief Header for CHIME/Pulsar UDP packet.
+ *
+ * Current version of the header used for transmission of beamformed
+ * data to the CHIME/Pulsar backend, same as VDIF definition. Indices
+ * of frequency channels, each 10-b numbers, are encoded in two places:
+ * PSRHeader.thread_id and PSRHeader.eud3, with the latter defined as
+ * PSRHeader.eud3 = freq_index[3]<<20 + freq_index[2]<<10 + freq_index[1]
+ * Also, position info is encoded into PSRHeader.eud4 as:
+ * PSRHeader.eud4 = (RA << 16) + (DEC)
+ *
+ * @author Cherry Ng
+**/
+
 struct PSRHeader {
     uint32_t seconds : 30;
     uint32_t legacy : 1;
@@ -14,14 +29,14 @@ struct PSRHeader {
     uint32_t log_num_chan : 5;
     uint32_t vdif_version : 3;
     uint32_t station_id : 16;
-    uint32_t thread_id : 10; // index of first packed frequency.
+    uint32_t thread_id : 10;
     uint32_t bits_depth : 5;
     uint32_t data_type : 1;
-    uint32_t eud1 : 24; // UD: beam number [0 to 9]
+    uint32_t eud1 : 24;
     uint32_t edv : 8;
-    uint32_t eud2 : 32; // _psr_scaling from metadata
-    uint32_t eud3 : 32; // 32-b number encoding three 10-b frequency indices: thread_id[3]<<20 + thread_id[2]<<10 + thread_id[1].
-    uint32_t eud4 : 32; // 32-b number encoding 16-b RA + 16-b Dec
+    uint32_t eud2 : 32;
+    uint32_t eud3 : 32;
+    uint32_t eud4 : 32;
 };
 
 #endif /* PULSAR_FUNCTIONS_HPP */
