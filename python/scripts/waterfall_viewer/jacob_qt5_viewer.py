5f1eed9072a152d23f17039b5969f506315abc7d
Rick Nitsche <rick@phas.ubc.ca>
2020-09-22 16:31:54 -0700

    style(jacob_qt5_viewer): remove unusd import

--- python/scripts/waterfall_viewer/jacob_qt5_viewer.py
+++ python/scripts/waterfall_viewer/jacob_qt5_viewer.py
@@ -32,14 +32,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
-import random
 import time
-import cmath
-import math
 import subprocess
-from astropy.coordinates import SkyCoord, EarthLocation, Angle, AltAz
+from astropy.coordinates import SkyCoord, EarthLocation, Angle
 from astropy import units as u
 from astropy.time import Time
 
