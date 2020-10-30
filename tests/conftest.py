d0e7e153890d1978b3e6eda8d80a611032d6b1b3
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2020-10-29 22:07:03 -0700

    test(test_transpose): Test dataset ID change.
    * Refactors the cal_broker fixture from test_applygains.py into
      conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,3 +37,60 @@ def pytest_runtest_setup(item):
     if envnames:
         if item.config.getoption("-E") not in envnames:
             pytest.skip("test requires env in {!r}".format(envnames))
+
+
+#
+# Shared fixtures for gain updates
+#
+from pytest_localserver.http import WSGIServer
+from flask import Flask, jsonify, request as flask_req
+import base64
+
+
+def encode_gains(gain, weight):
+    # encode base64
+    res = {
+        "gain": {
+            "dtype": "complex64",
+            "shape": gain.shape,
+            "data": base64.b64encode(gain.tobytes()).decode(),
+        },
+        "weight": {
+            "dtype": "bool",
+            "shape": weight.shape,
+            "data": base64.b64encode(weight.tobytes()).decode(),
+        },
+    }
+    return res
+
+
+@pytest.fixture(scope="module")
+def cal_broker(request, old_gains, new_gains):
+    # get updates IDs from module
+    new_update_id = getattr(request.module, "new_update_id", None)
+    old_update_id = getattr(request.module, "old_update_id", None)
+
+    # Create a basic flask server
+    app = Flask("cal_broker")
+
+    @app.route("/gain", methods=["POST"])
+    def gain_app():
+        content = flask_req.get_json()
+        update_id = content["update_id"]
+        if update_id == new_update_id:
+            gains = encode_gains(*new_gains)
+        elif update_id == old_update_id:
+            gains = encode_gains(*old_gains)
+        else:
+            raise Exception("Did not recognize update_id {}.".format(update_id))
+        print(f"Served gains with {update_id}")
+
+        return jsonify(gains)
+
+    # hand to localserver fixture
+    server = WSGIServer(application=app)
+    server.start()
+
+    yield server
+
+    server.stop()
