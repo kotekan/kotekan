d1b25505c4766f832521732b548318781de0a6c3
james-s-willis <james_willis@hotmail.co.uk>
2021-01-19 16:37:51 -0500

    Don't need to declare functions as well as define them.

--- lib/utils/h5_support.hpp
+++ lib/utils/h5_support.hpp
@@ -21,21 +21,6 @@ inline AtomicType<dset_id_str>::AtomicType() {
     H5Tset_size(_hid, DSET_ID_LEN);
 }
 // \endcond
-
-template<>
-DataType create_datatype<freq_ctype>();
-template<>
-DataType create_datatype<time_ctype>();
-template<>
-DataType create_datatype<input_ctype>();
-template<>
-DataType create_datatype<prod_ctype>();
-template<>
-DataType create_datatype<cfloat>();
-template<>
-DataType create_datatype<stack_ctype>();
-template<>
-DataType create_datatype<rstack_ctype>();
 }; // namespace HighFive
 
 // These templated functions are needed in order to tell HighFive how the
