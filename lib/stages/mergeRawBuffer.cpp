cc389bd32f9a1cb459f7aaafd4f40727053ed7de
Jing Luo <luojing1211@gmail.com>
2021-02-08 21:22:59 +0000

    add merged beam metadata to merged buffer

--- lib/stages/mergeRawBuffer.cpp
+++ lib/stages/mergeRawBuffer.cpp
@@ -60,16 +60,30 @@ void mergeRawBuffer::main_thread() {
             break;
 
 	BeamMetadata* in_metadata = (BeamMetadata*)get_metadata(in_buf, in_buffer_ID);
-
-        out_frame = (uint8_t*)wait_for_empty_frame(out_buf, unique_name.c_str(), out_buffer_ID);
-        if (out_frame == nullptr)
-             break;
         
-        // compute the frame index for the out_buf
-	uint32_t sub_frame_index = in_frame_counter % _raw_frames_per_merged_frame;	
-	// compute the size of the sub_frame of the out_frame
-	uint32_t sub_frame_size = sizeof(FreqIDBeamMetadata) + in_buf -> frame_size;
+        // Compute the frame index in the outbuffer
+        uint32_t sub_frame_index = in_frame_counter % _raw_frames_per_merged_frame;
+        // compute the size of the sub_frame of the out_frame
+        uint32_t FreqIDBeamMeta_size = sizeof(FreqIDBeamMetadata);
+        uint32_t sub_frame_size = FreqIDBeamMeta_size + in_buf -> frame_size;
+        
+	/// Get a new merged frame for a new set of sub frames. 
+	if (sub_frame_index == 0){
+            out_frame = (uint8_t*)wait_for_empty_frame(out_buf, unique_name.c_str(), out_buffer_ID);
+            if (out_frame == nullptr)
+                break;
+	    allocate_new_metadata_object(out_buf, out_buffer_ID);
+            MergedBeamMetadata* out_metadata = (MergedBeamMetadata*)get_metadata(out_buf, out_buffer_ID);
+	    // populate the merged_buffer/out_buffer frame header.
+	    out_metadata -> sub_frame_pre_frame = _raw_frames_per_merged_frame;
+            out_metadata -> sub_frame_metadata_size = FreqIDBeamMeta_size;
+            out_metadata -> sub_frame_data_size = in_buf -> frame_size;
+	}
+
 	// Get the start position of a sub_out_frame's meatadata
+	// Make sure out_frame is pointed to the right place.
+        if (out_frame == nullptr)
+            break;
 	uint32_t out_frame_metadata_pos = sub_frame_size * sub_frame_index;
 	// Get the memory block for the metadata of sub_out_frame.
 	FreqIDBeamMetadata* sub_frame_metadata = (FreqIDBeamMetadata *)&out_frame[out_frame_metadata_pos];
@@ -83,7 +97,7 @@ void mergeRawBuffer::main_thread() {
 	sub_frame_metadata -> ra = in_metadata -> ra;
 	sub_frame_metadata -> dec = in_metadata -> dec;
 	sub_frame_metadata -> scaling = in_metadata -> scaling;
-
+        
 	// Add the frequency id into the metadata, for chime one stream has on frequency.
 	// TODO Have the output buffer handle the stream with mulitple frequency
 	const uint32_t num_freq_per_stream = Telescope::instance().num_freq_per_stream();
@@ -97,11 +111,12 @@ void mergeRawBuffer::main_thread() {
 	// Get the start position of a sub_out_frame's data
 	uint32_t out_frame_data_pos = out_frame_metadata_pos + sizeof(FreqIDBeamMetadata);
 	// Get the memory block for the data of of a subframe.
-	uint8_t * sub_frame_data = &out_frame[out_frame_data_pos];
+	uint8_t* sub_frame_data = &out_frame[out_frame_data_pos];
 	// copy data from in frame to sub_out_frame
 	memcpy(sub_frame_data, in_frame, in_buf -> frame_size);
-
-	FreqIDBeamMetadata* debud_frame_metadata = (FreqIDBeamMetadata *)&out_frame[sub_frame_index * sub_frame_size];
+        
+	// Debug code. Check if the subframe matedata is the same. 
+   	FreqIDBeamMetadata* debud_frame_metadata = (FreqIDBeamMetadata *)&out_frame[sub_frame_index * sub_frame_size];
 	DEBUG2("Sub frame Beam RA: {:f}, Dec: {:f}, scaling: {:d}, , beam_num: {:d}, freq_id {:d}\n", debud_frame_metadata -> ra, debud_frame_metadata -> dec, debud_frame_metadata -> scaling, debud_frame_metadata-> beam_number,  debud_frame_metadata-> frequency_bin);
         
 	mark_frame_empty(in_buf, unique_name.c_str(), in_buffer_ID);
