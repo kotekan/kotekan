3389522d307d7070728ba97cd7b920b3c915c123
Rick Nitsche <2536635+nritsche@users.noreply.github.com>
2021-02-24 08:45:49 -0800

    fix(visAccumulate): Register base ds if ds id not set in frame (#926)

--- lib/stages/visAccumulate.cpp
+++ lib/stages/visAccumulate.cpp
@@ -32,6 +32,7 @@
 #include <iterator>  // for back_insert_iterator, begin, end, back_inserter
 #include <mutex>     // for lock_guard, mutex
 #include <numeric>   // for iota
+#include <optional>  // for optional
 #include <regex>     // for match_results<>::_Base_type
 #include <stdexcept> // for runtime_error, invalid_argument
 #include <time.h>    // for size_t, timespec
@@ -249,7 +250,7 @@ void visAccumulate::main_thread() {
 
     frameID in_frame_id(in_buf);
 
-    dset_id_t ds_id_in = dset_id_t::null;
+    std::optional<dset_id_t> ds_id_in = std::nullopt;
 
     // Hold the gated datasets that are enabled;
     std::vector<std::reference_wrapper<internalState>> enabled_gated_datasets;
@@ -275,13 +276,13 @@ void visAccumulate::main_thread() {
 
         // Check if dataset ID changed
         dset_id_t ds_id_in_new = get_dataset_id(in_buf, in_frame_id);
-        if (ds_id_in_new != ds_id_in) {
+        if (!ds_id_in || ds_id_in_new != *ds_id_in) {
             ds_id_in = ds_id_in_new;
 
             // Register base dataset. If no dataset ID was was set in the incoming frame,
             // ds_id_in will be dset_id_t::null and thus cause a root dataset to
             // be registered.
-            base_dataset_id = dm.add_dataset(base_dataset_states, ds_id_in);
+            base_dataset_id = dm.add_dataset(base_dataset_states, *ds_id_in);
             DEBUG("Registered base dataset: {}", base_dataset_id)
 
             // Set the output dataset ID for the main visibility accumulation
