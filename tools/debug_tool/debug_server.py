34e9cf350ea59dab960459463d9b4f5e85cbeed5
james-s-willis <james_willis@hotmail.co.uk>
2021-04-29 11:31:51 -0400

    Added file browser to server.

--- tools/debug_tool/debug_server.py
+++ tools/debug_tool/debug_server.py
@@ -1,6 +1,7 @@
-from flask import Flask, json, render_template, request, make_response
+from flask import Flask, json, render_template, request, make_response, send_file, abort
 from flask_cors import CORS, cross_origin
 from requests import get
+import os
 app = Flask(__name__)
 app.config['CORS_ORIGINS'] = ['*']
 CORS(app, support_credentials=True)
@@ -15,13 +16,35 @@ def proxy():
   if request.method == 'GET':
     data = get(f'{KOTEKAN_ADDRESS}/buffers')
     #return render_template('pipeline_tree.html', data=data)
-    return render_template('tree_example.html', data=data)
+    return render_template('examples/cola.html', data=data)
+    #return render_template('dtree_example.html', data=data)
+    #return render_template('index.html', data=data)
 
   # POST request
   if request.method == 'POST':
     print("Received a POST request")
     print(request.get_json())  # parse as JSON
     return 'Sucesss', 200
+
+@app.route('/', defaults={'req_path': ''})
+@app.route('/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = '/'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/update', methods=["GET", "POST"])
