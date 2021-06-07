f4bb6e218af433eb2e88c69a73c98ff1e1fa2ed5
Jintao Xu <jintaoxu@MacBook-Pro-2.local>
2021-06-07 09:35:04 -0400

    1

--- lib/core/cpuMonitor.cpp
+++ lib/core/cpuMonitor.cpp
@@ -28,55 +28,57 @@ void CpuMonitor::start() {
 }
 
 void* CpuMonitor::track_cpu(void *) {
-    ERROR_NON_OO("entered track_cpu");
+    while (1) {
+        ERROR_NON_OO("entered track_cpu");
 
-    // Read total CPU stat from /proc/stat first line
-    std::string stat;
-    std::ifstream cpu_stat("./proc/stat", std::ifstream::in);
-    getline(cpu_stat, stat);
-    std::istringstream iss(stat);
+        // Read total CPU stat from /proc/stat first line
+        std::string stat;
+        std::ifstream cpu_stat("./proc/stat", std::ifstream::in);
+        getline(cpu_stat, stat);
+        std::istringstream iss(stat);
 
-    // Parse and get total cpu time
-    char prefix[10];
-    iss >> prefix;
-    uint32_t num = 0;
-    uint32_t cpu_time = 0;
-    for (int i = 0; i < 10; i++) {
-        iss >> num;
-        cpu_time += num;
-    }
-
-    // Read CPU stat for each thread
-    for(auto element : thread_list) {
-        char fname[100];
-        snprintf(fname, sizeof(fname), "/proc/self/task/%d/stat", element.second);
-        FILE *fp = fopen(fname, "r");
-
-        if (fp) {
-            // Get the 14th (utime) and the 15th (stime) numbers
-            uint32_t utime = 0, stime = 0;
-            fscanf(fp, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %d %d", &utime, &stime);
-
-            auto itr = ult_list.find(element.first);
-            if (itr != ult_list.end()) {
-                // Compute usr and sys CPU usage
-                itr->second.utime_usage =
-                    100 * (utime - itr->second.prev_utime) / (cpu_time - prev_cpu_time);
-                itr->second.stime_usage =
-                    100 * (stime - itr->second.prev_stime) / (cpu_time - prev_cpu_time);
-                
-                ERROR_NON_OO("{:s}: utime= {:f}, stime={:f}", itr->first, itr->second.utime_usage, itr->second.stime_usage);
-            }
-
-            // Update thread usr and sys time
-            itr->second.prev_utime = utime;
-            itr->second.prev_stime = stime;
+        // Parse and get total cpu time
+        char prefix[10];
+        iss >> prefix;
+        uint32_t num = 0;
+        uint32_t cpu_time = 0;
+        for (int i = 0; i < 10; i++) {
+            iss >> num;
+            cpu_time += num;
         }
-    }
-    prev_cpu_time = cpu_time;
 
-    // Check each stage periodically
-    std::this_thread::sleep_for(2000ms);
+        // Read CPU stat for each thread
+        for(auto element : thread_list) {
+            char fname[100];
+            snprintf(fname, sizeof(fname), "/proc/self/task/%d/stat", element.second);
+            FILE *fp = fopen(fname, "r");
+
+            if (fp) {
+                // Get the 14th (utime) and the 15th (stime) numbers
+                uint32_t utime = 0, stime = 0;
+                fscanf(fp, "%*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %*s %d %d", &utime, &stime);
+
+                auto itr = ult_list.find(element.first);
+                if (itr != ult_list.end()) {
+                    // Compute usr and sys CPU usage
+                    itr->second.utime_usage =
+                        100 * (utime - itr->second.prev_utime) / (cpu_time - prev_cpu_time);
+                    itr->second.stime_usage =
+                        100 * (stime - itr->second.prev_stime) / (cpu_time - prev_cpu_time);
+                    
+                    ERROR_NON_OO("{:s}: utime= {:f}, stime={:f}", itr->first, itr->second.utime_usage, itr->second.stime_usage);
+                }
+
+                // Update thread usr and sys time
+                itr->second.prev_utime = utime;
+                itr->second.prev_stime = stime;
+            }
+        }
+        prev_cpu_time = cpu_time;
+
+        // Check each stage periodically
+        std::this_thread::sleep_for(2000ms);
+    }
 }
 
 void CpuMonitor::record_tid(pthread_t tid, std::string thread_name) {
