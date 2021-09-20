25c072d8d236f1b37fc98f30c0fa3a01c2e26f55
Adam Lanman <adamelanman@protonmail.com>
2021-09-20 15:25:43 -0400

    add install file

---
+++ install.sh
@@ -0,0 +1,22 @@
+# A script demonstrating building kotekan with tests and HDF5
+# Designed for the kotekan Docker image
+
+cd build
+cmake \
+        -DCMAKE_VERBOSE_MAKEFILE=ON\
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DCMAKE_CXX_FLAGS_DEBUG="-g -O0"\
+        -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+        -DSUPERDEBUG=ON \
+        -DBLAZE_PATH=/code/build/blaze \
+        -DBLAS_LIBRARIES=/usr/lib/x86_64-linux-gnu/libopenblas.so.0 \
+        -DLAPACK_LIBRARIES=/usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1 \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DUSE_HDF5=ON \
+        -DUSE_LAPACK=ON \
+        -DHIGHFIVE_PATH=/code/build/HighFive \
+        -DWITH_TESTS=ON \
+        ..
+make
+make install
+
