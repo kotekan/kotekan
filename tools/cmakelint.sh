f3b43fc4ad1248fca0652280d708f6f2fd6ced79
Andre Renard <andre@renard.io>
2022-04-20 07:49:12 -0600

    Exclude FindHIP.cmake from linter

--- tools/cmakelint.sh
+++ tools/cmakelint.sh
@@ -23,6 +23,10 @@ done
 # Run cmakelint on all .cmake files recursively.
 shopt -s nullglob
 for file in "$path"/cmake/*.cmake; do
+    # For some reason the FindHIP.cmake script from AMD breaks the cmakelint parser
+    if [[ $file =~ "FindHIP" ]]; then
+        continue
+    fi
     cmake-format -c "$path"/tools/cmake_format_config.py -i -- "$file"
     git diff --exit-code
     cmake-lint --suppress-decorations -c "$path"/tools/cmake_format_config.py -- "$file"
