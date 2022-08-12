8bb8b0e8630343bfc6f5cf90d49ac5f9de6e48c1
Adam Lanman <adamelanman@pm.me>
2022-08-12 03:27:22 +0000

    update dockerfile and readme

--- install.sh
+++ install.sh
@@ -1,23 +1,50 @@
-# A script demonstrating building kotekan with tests and HDF5
+# A script to simplify building kotekan
 # Designed for the kotekan Docker image
 
+tests="OFF"
+debug=0
+# Get the options
+while getopts ":t:d" option; do
+   case $option in
+      t) # Include tests
+         tests='ON';;
+      d) # Debug mode
+	 debug=1;;
+   esac
+done
+
+
 cd build
+
+if [ $debug -eq 1 ]
+then
 cmake \
         -DCMAKE_VERBOSE_MAKEFILE=ON\
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_FLAGS_DEBUG="-g -O0"\
         -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
-	    -DNO_MEMLOCK=ON \
+	-DNO_MEMLOCK=ON \
         -DSUPERDEBUG=ON \
         -DBLAZE_PATH=/code/build/blaze \
         -DBLAS_LIBRARIES=/usr/lib/x86_64-linux-gnu/libopenblas.so.0 \
         -DLAPACK_LIBRARIES=/usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1 \
-        -DCMAKE_BUILD_TYPE=Debug \
         -DUSE_HDF5=ON \
         -DUSE_LAPACK=ON \
         -DHIGHFIVE_PATH=/code/build/HighFive \
-        -DWITH_TESTS=ON \
+        -DWITH_TESTS=$tests \
         ..
-make
-make install
+else
+cmake \
+	-DNO_MEMLOCK=ON \
+        -DBLAZE_PATH=/code/build/blaze \
+        -DBLAS_LIBRARIES=/usr/lib/x86_64-linux-gnu/libopenblas.so.0 \
+        -DLAPACK_LIBRARIES=/usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1 \
+        -DUSE_HDF5=ON \
+        -DUSE_LAPACK=ON \
+        -DHIGHFIVE_PATH=/code/build/HighFive \
+        -DWITH_TESTS=$tests \
+        ..
+fi
 
+make -j4
+make install
