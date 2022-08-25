b0121abb47f39520187ce104272a65cad5d15659
Andre Renard <andre@renard.io>
2022-08-24 21:01:22 -0400

    Add names for cl command objects

--- lib/opencl/clOutputData.cpp
+++ lib/opencl/clOutputData.cpp
@@ -7,7 +7,7 @@ REGISTER_CL_COMMAND(clOutputData);
 
 clOutputData::clOutputData(Config& config, const std::string& unique_name,
                            bufferContainer& host_buffers, clDeviceInterface& device) :
-    clCommand(config, unique_name, host_buffers, device, "", "") {
+    clCommand(config, unique_name, host_buffers, device, "clOutputData", "") {
     _num_elements = config.get<int>(unique_name, "num_elements");
     _num_local_freq = config.get<int>(unique_name, "num_local_freq");
     _block_size = config.get<int>(unique_name, "block_size");
