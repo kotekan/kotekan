71b2624f469648dc98baeae2203d1b3abdcc4143
Dustin Lang <dstndstn@gmail.com>
2023-02-14 13:21:30 -0500

    black: absolutely ridiculous formatting

--- tests/bb_bf_onehot_post.py
+++ tests/bb_bf_onehot_post.py
@@ -1,10 +1,16 @@
-print('VOLTAGE    CPU(re,im)       CPU(re,im)sh     CPU       GPU')
+print("VOLTAGE    CPU(re,im)       CPU(re,im)sh     CPU       GPU")
 for i in range(18):
     if not i in onehot:
         break
-    print('0x%02x      (% 4i, % 4i)      (% 4i, % 4i)     0x%02x      0x%02x' %
-          (onehot[i][1],
-           bb[i][1][0], bb[i][1][1],
-           bb[i][2][0], bb[i][2][1],
-           sparse['simulated_formed_beams_buffer'][i][1],
-           sparse['host_formed_beams_buffer'][i][1]))
+    print(
+        "0x%02x      (% 4i, % 4i)      (% 4i, % 4i)     0x%02x      0x%02x"
+        % (
+            onehot[i][1],
+            bb[i][1][0],
+            bb[i][1][1],
+            bb[i][2][0],
+            bb[i][2][1],
+            sparse["simulated_formed_beams_buffer"][i][1],
+            sparse["host_formed_beams_buffer"][i][1],
+        )
+    )
