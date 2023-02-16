baf93d07b7fd5bebd270c12bc3bf89cbadbd00fb
Andre Renard <andre@renard.io>
2023-02-16 15:50:58 -0500

    Fix doc string

--- lib/cuda/cudaDeviceInterface.hpp
+++ lib/cuda/cudaDeviceInterface.hpp
@@ -45,13 +45,14 @@ public:
      * @param dst The GPU memory pointer
      * @param src The CPU memory pointer
      * @param len The amount of data to copy in bytes
+     * @param cuda_stream_id The stream to run the copy on
      * @param pre_event The event before this one to wait on, if NULL will not wait
-     * @param copy_pre_event The profiling event at the start of this copy
-     * @param copy_post_event The event at the end of the copy.
+     * @param copy_start_event The profiling event at the start of this copy
+     * @param copy_end_event The event at the end of the copy.
      */
     void async_copy_host_to_gpu(void* dst, void* src, size_t len, uint32_t cuda_stream_id,
-                                cudaEvent_t pre_event, cudaEvent_t& copy_pre_event,
-                                cudaEvent_t& copy_post_event);
+                                cudaEvent_t pre_event, cudaEvent_t& copy_start_event,
+                                cudaEvent_t& copy_end_event);
 
     /**
      * @brief Asynchronous Copies memory from the device GPU (global memory) to host (CPU RAM).
@@ -59,13 +60,14 @@ public:
      * @param dst The CPU memory pointer
      * @param src The GPU memory pointer
      * @param len The amount of data to copy in bytes
+     * @param cuda_stream_id The stream to run the copy on
      * @param pre_event The event before this one to wait on, if NULL will not wait
-     * @param copy_pre_event The profiling event at the start of this copy
-     * @param copy_post_event The event at the end of the copy.
+     * @param copy_start_event The profiling event at the start of this copy
+     * @param copy_end_event The event at the end of the copy.
      */
     void async_copy_gpu_to_host(void* dst, void* src, size_t len, uint32_t cuda_stream_id,
-                                cudaEvent_t pre_event, cudaEvent_t& copy_pre_event,
-                                cudaEvent_t& copy_post_event);
+                                cudaEvent_t pre_event, cudaEvent_t& copy_start_event,
+                                cudaEvent_t& copy_end_event);
 
     // Function overrides to cast the generic gpu_memory retulsts appropriately.
     void* get_gpu_memory_array(const std::string& name, const uint32_t index, const uint32_t len);
