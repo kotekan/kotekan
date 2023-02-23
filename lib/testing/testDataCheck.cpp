7daab6ab8f94d550d63bfe9eb3acf00e6d7cf12d
Dustin Lang <dstndstn@gmail.com>
2023-02-23 20:45:21 +0000

    testDataCheck: add float16 version

--- lib/testing/testDataCheck.cpp
+++ lib/testing/testDataCheck.cpp
@@ -1,12 +1,18 @@
 #include "testDataCheck.hpp"
 
 #include "StageFactory.hpp" // for REGISTER_KOTEKAN_STAGE, StageMakerTemplate
-
+#include "visUtil.hpp"
 
 using testDataCheckInt = testDataCheck<int32_t>;
 using testDataCheckUchar = testDataCheck<uint8_t>;
 using testDataCheckFloat = testDataCheck<float>;
+#if KOTEKAN_FLOAT16
+using testDataCheckFloat16 = testDataCheck<float16_t>;
+#endif
 
 REGISTER_KOTEKAN_STAGE(testDataCheckInt);
 REGISTER_KOTEKAN_STAGE(testDataCheckUchar);
 REGISTER_KOTEKAN_STAGE(testDataCheckFloat);
+#if KOTEKAN_FLOAT16
+REGISTER_KOTEKAN_STAGE(testDataCheckFloat16);
+#endif
