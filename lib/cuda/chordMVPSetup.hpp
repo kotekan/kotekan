a260a79cd71aabac29148e88d43d9881d7af00b5
Dustin Lang <dstndstn@gmail.com>
2023-03-25 04:05:23 +0000

    add ChordMVPSetup, which sets up GPU buffer views for the upchannelization-to-FRBbeamformer padding

---
+++ lib/cuda/chordMVPSetup.hpp
@@ -0,0 +1,22 @@
+#ifndef KOTEKAN_CHORD_MVP_SETUP_HPP
+#define KOTEKAN_CHORD_MVP_SETUP_HPP
+
+#include "cudaCommand.hpp"
+
+#include <vector>
+
+/**
+ * @class chordMVPSetup
+ * @brief A CUDA GPU command for setting up buffers for our Minimum Viable Pipeline demo.
+ *
+ * @author Dustin Lang
+ */
+class chordMVPSetup : public cudaCommand {
+public:
+    chordMVPSetup(kotekan::Config& config, const std::string& unique_name,
+                  kotekan::bufferContainer& host_buffers, cudaDeviceInterface& device);
+    ~chordMVPSetup();
+    virtual cudaEvent_t execute(int gpu_frame_id, const std::vector<cudaEvent_t>& pre_events) override;
+};
+
+#endif // KOTEKAN_CHORD_MVP_SETUP_HPP
