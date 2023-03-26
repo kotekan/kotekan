66f15adaa78c503922a2f53ddb313349ef059be9
Dustin Lang <dstndstn@gmail.com>
2023-03-26 06:36:36 +0000

    cudaQuantize: fix name of index array requested; size_t carefulness.  Update kernel code from Hans.

--- lib/cuda/cudaQuantize.cpp
+++ lib/cuda/cudaQuantize.cpp
@@ -29,8 +29,9 @@ cudaQuantize::cudaQuantize(Config& config, const std::string& unique_name,
         throw std::runtime_error("The num_chunks parameter must be a factor of 32");
     set_command_type(gpuCommandType::KERNEL);
 
-    size_t index_array_len = _num_chunks * 2 * sizeof(int32_t);
-    int32_t* index_array_memory = (int32_t*)device.get_gpu_memory(_gpu_mem_input, index_array_len);
+    std::string _gpu_mem_index = unique_name + "/index";
+    size_t index_array_len = (size_t)_num_chunks * 2 * sizeof(int32_t);
+    int32_t* index_array_memory = (int32_t*)device.get_gpu_memory(_gpu_mem_index, index_array_len);
 
     int32_t* index_array_host = (int32_t*)malloc(index_array_len);
     assert(index_array_host);
@@ -58,22 +59,25 @@ cudaQuantize::~cudaQuantize() {}
 cudaEvent_t cudaQuantize::execute(int gpu_frame_id, const std::vector<cudaEvent_t>& pre_events,
                                   bool* quit) {
     (void)pre_events;
+    (void)quit;
     pre_execute(gpu_frame_id);
 
-    size_t input_frame_len = _num_chunks * CHUNK_SIZE * sizeof(float16_t);
+    size_t input_frame_len = (size_t)_num_chunks * CHUNK_SIZE * sizeof(float16_t);
     void* input_memory = device.get_gpu_memory_array(_gpu_mem_input, gpu_frame_id, input_frame_len);
-
+    INFO("Input frame length: {:d} x {:d} x 2 = {:d}", _num_chunks, CHUNK_SIZE, input_frame_len);
     //  divide by 2 because of packed int4 outputs
-    size_t output_frame_len = _num_chunks * CHUNK_SIZE / 2;
+    size_t output_frame_len = (size_t)_num_chunks * CHUNK_SIZE / 2;
+    INFO("Output frame length: {:d} x {:d} / 2 = {:d}", _num_chunks, CHUNK_SIZE, output_frame_len);
     int32_t* output_memory =
         (int32_t*)device.get_gpu_memory_array(_gpu_mem_output, gpu_frame_id, output_frame_len);
 
-    size_t meanstd_frame_len = _num_chunks * 2 * sizeof(float16_t);
+    size_t meanstd_frame_len = (size_t)_num_chunks * 2 * sizeof(float16_t);
     void* meanstd_memory =
         device.get_gpu_memory_array(_gpu_mem_meanstd, gpu_frame_id, meanstd_frame_len);
 
-    size_t index_array_len = _num_chunks * 2 * sizeof(int32_t);
-    int32_t* index_array_memory = (int32_t*)device.get_gpu_memory(_gpu_mem_input, index_array_len);
+    std::string _gpu_mem_index = unique_name + "/index";
+    size_t index_array_len = (size_t)_num_chunks * 2 * sizeof(int32_t);
+    int32_t* index_array_memory = (int32_t*)device.get_gpu_memory(_gpu_mem_index, index_array_len);
 
     record_start_event(gpu_frame_id);
 
--- lib/cuda/quantize_kernel.cu
+++ lib/cuda/quantize_kernel.cu
@@ -1,5 +1,7 @@
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
+//#include "header.cuh"
+#include <stdio.h>      /*printf*/
 
 #define chunksPerFrame 32
 
@@ -11,10 +13,11 @@ __device__ unsigned int clamped_round_down(__half x)
     return __half2int_rd(x);
 }
 
-__global__ void quantize (const __half2 *in_base, __half2 *outf_base, unsigned int *outi_base, const int *index_array)
+__global__ void quantize (const __half2 *__restrict__ in_base, __half2 *__restrict__ outf_base, unsigned int *__restrict__ outi_base, const int *__restrict__ index_array)
 {
-    __half2 * outf = outf_base + index_array[64*blockIdx.x + 32];
-    unsigned int * outi = outi_base + index_array[64*blockIdx.x + 33];
+    __half2 * __restrict__ outf = outf_base + index_array[64*blockIdx.x + 32];
+    unsigned int * __restrict__ outi = outi_base + index_array[64*blockIdx.x + 33];
+    __half2 outf_temp = __float2half2_rn(0);
     for (int chunk = 0; chunk < chunksPerFrame; chunk++)
     {
         const __half2 * chunkIn = in_base + index_array[64*blockIdx.x + chunk]; //the memory for the current chunk
@@ -46,21 +49,18 @@ __global__ void quantize (const __half2 *in_base, __half2 *outf_base, unsigned i
         scale = __hadd(scale, __shfl_sync(0xffffffff, scale, threadIdx.x ^ 16));
         scale = __hdiv(hsqrt(__hdiv(scale, 255.0)), 2.3);
 
-        if (threadIdx.x == 0)
-        {
-            outf[chunk] = __halves2half2(mean, scale);
-        }
+	outf_temp = (threadIdx.x == chunk) ?  __halves2half2(mean, scale) : outf_temp; //for each thread, saving a single outf_temp value, then I'll merge them all later
         
         scale = (__heq(scale, 0)) ? __float2half(1.0) : scale; //avoid division by 0
         
         //computing i values
         unsigned int i = clamped_round_down(__hadd((__low2half(chunkIn[threadIdx.x]) - mean)/scale, __half(8.5)));
-        i = i | (clamped_round_down(__hadd((__high2half(chunkIn[threadIdx.x])      - mean)/scale, __half(8.5))) << 4);
-        i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 32]) - mean)/scale, __half(8.5))) << 8);
+	i = i | (clamped_round_down(__hadd((__high2half(chunkIn[threadIdx.x])      - mean)/scale, __half(8.5))) << 4);
+	i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 32]) - mean)/scale, __half(8.5))) << 8);
         i = i | (clamped_round_down(__hadd((__high2half(chunkIn[threadIdx.x + 32]) - mean)/scale, __half(8.5))) << 12);
-        i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 64]) - mean)/scale, __half(8.5))) << 16);
+	i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 64]) - mean)/scale, __half(8.5))) << 16);
         i = i | (clamped_round_down(__hadd((__high2half(chunkIn[threadIdx.x + 64]) - mean)/scale, __half(8.5))) << 20);
-        i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 96]) - mean)/scale, __half(8.5))) << 24);
+	i = i | (clamped_round_down(__hadd((__low2half( chunkIn[threadIdx.x + 96]) - mean)/scale, __half(8.5))) << 24);
         i = i | (clamped_round_down(__hadd((__high2half(chunkIn[threadIdx.x + 96]) - mean)/scale, __half(8.5))) << 28);
 
         unsigned int i_shfl = __shfl_sync(0xffffffff, i, threadIdx.x ^ 1);
@@ -72,4 +72,6 @@ __global__ void quantize (const __half2 *in_base, __half2 *outf_base, unsigned i
 
         outi[chunk*32 + threadIdx.x/4 + (threadIdx.x%4)*8] = i; //writing result to memory
     }
+    //writing the means and scales for this frame to global
+    outf[threadIdx.x] = outf_temp;
 }
