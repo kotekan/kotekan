7fe219b606d197f8400967cc7bc1048c0edd6c53
Erik Schnetter <schnetter@gmail.com>
2023-05-08 13:54:29 -0400

    Auto-generate C++ code for all Julia kernels

--- lib/cuda/cudaFRBBeamformer.cpp
+++ lib/cuda/cudaFRBBeamformer.cpp
@@ -1,199 +1,177 @@
-#include "cudaFRBBeamformer.hpp"
+/**
+ * @file
+ * @brief CUDA  kernel
+ *
+ * This file has been generated automatically.
+ * Do not modify this C++ file, your changes will be lost.
+ */
 
-#include "math.h"
+#include "cudaCommand.hpp"
+#include "cudaDeviceInterface.hpp"
 
-#include "fmt.hpp"
-
+#include <array>
+#include <fmt.hpp>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 using kotekan::bufferContainer;
 using kotekan::Config;
 
-REGISTER_CUDA_COMMAND(cudaFRBBeamformer);
+/**
+ * @class cuda
+ * @brief cudaCommand for
+ */
+class cuda : public cudaCommand {
+public:
+    cuda(Config& config, const std::string& unique_name, bufferContainer& host_buffers,
+         cudaDeviceInterface& device);
+    ~cuda();
+    cudaEvent_t execute(int gpu_frame_id, const std::vector<cudaEvent_t>& pre_events,
+                        bool* quit) override;
 
-static const size_t sizeof_float16_t = 2;
+private:
+    // Julia's `CuDevArray` type
+    template<typename T, int64_t N>
+    struct CuDeviceArray {
+        T* ptr;
+        int64_t maxsize; // bytes
+        int64_t dims[N]; // elements
+        int64_t len;     // elements
+        CuDeviceArray(void* const ptr, const std::size_t bytes) :
+            ptr(static_cast<T*>(ptr)), maxsize(bytes), dims{int64_t(maxsize / sizeof(T))},
+            len(maxsize / sizeof(T)) {}
+    };
+    using kernel_arg = CuDeviceArray<int32_t, 1>;
 
-cudaFRBBeamformer::cudaFRBBeamformer(Config& config, const std::string& unique_name,
-                                     bufferContainer& host_buffers, cudaDeviceInterface& device) :
-    cudaCommand(config, unique_name, host_buffers, device, "FRB_beamformer", "frb.ptx") {
-    _num_dishes = config.get<int>(unique_name, "num_dishes");
-    _dish_grid_size = config.get<int>(unique_name, "dish_grid_size");
-    _num_local_freq = config.get<int>(unique_name, "num_local_freq");
-    _samples_per_data_set = config.get<int>(unique_name, "samples_per_data_set");
-    _time_downsampling = config.get<int>(unique_name, "time_downsampling");
-    _gpu_mem_dishlayout = config.get<std::string>(unique_name, "gpu_mem_dishlayout");
-    _gpu_mem_voltage = config.get<std::string>(unique_name, "gpu_mem_voltage");
-    _gpu_mem_phase = config.get<std::string>(unique_name, "gpu_mem_phase");
-    _gpu_mem_beamgrid = config.get<std::string>(unique_name, "gpu_mem_beamgrid");
+    // Kernel design parameters:
+    static constexpr int cuda_beam_layout_M = 48;
+    static constexpr int cuda_beam_layout_N = 48;
+    static constexpr int cuda_dish_layout_M = 24;
+    static constexpr int cuda_dish_layout_N = 24;
+    static constexpr int cuda_downsampling_factor = 40;
+    static constexpr int cuda_number_of_complex_components = 2;
+    static constexpr int cuda_number_of_dishes = 512;
+    static constexpr int cuda_number_of_frequencies = 256;
+    static constexpr int cuda_number_of_polarizations = 2;
+    static constexpr int cuda_number_of_timesamples = 2064;
+
+    // Kernel compile parameters:
+    static constexpr int minthreads = 768;
+    static constexpr int blocks_per_sm = 1;
+
+    // Kernel call parameters:
+    static constexpr int threads_x = 32;
+    static constexpr int threads_y = 24;
+    static constexpr int blocks = 256;
+    static constexpr int shmem_bytes = 76896;
+
+    // Kernel name:
+    const char* const kernel_symbol =
+        "_Z15julia_frb_1030913CuDeviceArrayI7Int16x2Li1ELi1EES_I9Float16x2Li1ELi1EES_"
+        "I6Int4x8Li1ELi1EES_IS1_Li1ELi1EES_I5Int32Li1ELi1EE";
+
+    // Kernel arguments:
+    static constexpr std::size_t S_length = 2304UL;
+    static constexpr std::size_t W_length = 1179648UL;
+    static constexpr std::size_t E_length = 541065216UL;
+    static constexpr std::size_t I_length = 239616UL;
+    static constexpr std::size_t info_length = 786432UL;
+
+    // Runtime parameters:
+
+    // GPU memory:
+    const std::string S_memname;
+    const std::string W_memname;
+    const std::string E_memname;
+    const std::string I_memname;
+    const std::string info_memname;
+};
+
+REGISTER_CUDA_COMMAND(cuda);
+
+cuda::cuda(Config& config, const std::string& unique_name, bufferContainer& host_buffers,
+           cudaDeviceInterface& device) :
+    cudaCommand(config, unique_name, host_buffers, device, "", ".ptx"),
+    S_memname(config.get<std::string>(unique_name, "gpu_mem_dishlayout")),
+    W_memname(config.get<std::string>(unique_name, "gpu_mem_phase")),
+    E_memname(config.get<std::string>(unique_name, "gpu_mem_voltage")),
+    I_memname(config.get<std::string>(unique_name, "gpu_mem_beamgrid")),
+    info_memname(config.get<std::string>(unique_name, "gpu_mem_info")) {
+    const int num_dishes = config.get<int>(unique_name, "num_dishes");
+    if (num_dishes != (cuda_number_of_dishes))
+        throw std::runtime_error("The num_dishes config setting must be "
+                                 + std::to_string(cuda_number_of_dishes)
+                                 + " for the CUDA Baseband Beamformer");
+    const int dish_grid_size = config.get<int>(unique_name, "dish_grid_size");
+    if (dish_grid_size != (cuda_dish_layout_M))
+        throw std::runtime_error("The dish_grid_size config setting must be "
+                                 + std::to_string(cuda_dish_layout_M)
+                                 + " for the CUDA Baseband Beamformer");
+    const int num_local_freq = config.get<int>(unique_name, "num_local_freq");
+    if (num_local_freq != (cuda_number_of_frequencies))
+        throw std::runtime_error("The num_local_freq config setting must be "
+                                 + std::to_string(cuda_number_of_frequencies)
+                                 + " for the CUDA Baseband Beamformer");
+    const int samples_per_data_set = config.get<int>(unique_name, "samples_per_data_set");
+    if (samples_per_data_set != (cuda_number_of_timesamples))
+        throw std::runtime_error("The samples_per_data_set config setting must be "
+                                 + std::to_string(cuda_number_of_timesamples)
+                                 + " for the CUDA Baseband Beamformer");
+    const int time_downsampling = config.get<int>(unique_name, "time_downsampling");
+    if (time_downsampling != (cuda_downsampling_factor))
+        throw std::runtime_error("The time_downsampling config setting must be "
+                                 + std::to_string(cuda_downsampling_factor)
+                                 + " for the CUDA Baseband Beamformer");
+
 
     set_command_type(gpuCommandType::KERNEL);
-
-    std::vector<std::string> opts = {
+    const std::vector<std::string> opts = {
         "--gpu-name=sm_86",
         "--verbose",
     };
-    build_ptx({kernel_name}, opts);
-
-    if (_samples_per_data_set != cuda_samples_per_data_set)
-        throw std::runtime_error(fmt::format(
-            "The samples_per_data_set config setting must be {:d} for the CUDA FRB beamformer",
-            cuda_samples_per_data_set));
-    if (_time_downsampling != cuda_time_downsampling)
-        throw std::runtime_error(fmt::format(
-            "The time_downsampling config setting must be {:d} for the CUDA FRB beamformer",
-            cuda_time_downsampling));
-    if (_num_dishes != cuda_num_dishes)
-        throw std::runtime_error(
-            fmt::format("The num_dishes config setting must be {:d} for the CUDA FRB beamformer",
-                        cuda_num_dishes));
-    if (_dish_grid_size != cuda_dish_grid_size)
-        throw std::runtime_error(fmt::format(
-            "The dish_grid_size config setting must be {:d} for the CUDA FRB beamformer",
-            cuda_dish_grid_size));
-    if (_num_local_freq != cuda_num_local_freq)
-        throw std::runtime_error(fmt::format(
-            "The num_local_freq config setting must be {:d} for the CUDA FRB beamformer",
-            cuda_num_local_freq));
-
-    // Assumption:
-    int32_t dish_m = _dish_grid_size;
-    int32_t dish_n = _dish_grid_size;
-    int32_t beam_p = _dish_grid_size * 2;
-    int32_t beam_q = _dish_grid_size * 2;
-    if (dish_m * dish_n < _num_dishes)
-        throw std::runtime_error("Config parameter dish_grid_size^2 must be >= num_dishes");
-    int32_t Td = (_samples_per_data_set + _time_downsampling - 1) / _time_downsampling;
-
-    dishlayout_len = (size_t)dish_m * dish_n * 2 * sizeof(int16_t);
-    // 2 polarizations x 2 for complex
-    phase_len = (size_t)dish_m * dish_n * _num_local_freq * 2 * 2 * sizeof_float16_t;
-    // 2 polarizations
-    voltage_len = (size_t)_num_dishes * _num_local_freq * _samples_per_data_set * 2;
-    beamgrid_len = (size_t)beam_p * beam_q * _num_local_freq * Td * sizeof_float16_t;
-    info_len = (size_t)(threads_x * threads_y * blocks_x * sizeof(int32_t));
-
-    host_info.resize(info_len / sizeof(int32_t));
-
-    // Allocate GPU memory for dish-layout array, and fill from the config file entry!
-    int16_t* dishlayout_memory =
-        (int16_t*)device.get_gpu_memory(_gpu_mem_dishlayout, dishlayout_len);
-    std::vector<int> dishlayout_config =
-        config.get<std::vector<int>>(unique_name, "frb_beamformer_dish_layout");
-    if (dishlayout_config.size() != (size_t)(dish_m * dish_n * 2))
-        throw std::runtime_error(fmt::format("Config parameter frb_beamformer_dish_layout (length "
-                                             "{}) must have length = 2 * dish_grid_size^2 = {}",
-                                             dishlayout_config.size(), 2 * dish_m * dish_n));
-    int16_t* dishlayout_cpu_memory = (int16_t*)malloc(dishlayout_len);
-    for (size_t i = 0; i < dishlayout_len / sizeof(int16_t); i++)
-        dishlayout_cpu_memory[i] = dishlayout_config[i];
-    CHECK_CUDA_ERROR(cudaMemcpy(dishlayout_memory, dishlayout_cpu_memory, dishlayout_len,
-                                cudaMemcpyHostToDevice));
-    free(dishlayout_cpu_memory);
+    build_ptx({kernel_symbol}, opts);
 }
 
-cudaFRBBeamformer::~cudaFRBBeamformer() {}
+cuda::~cuda() {}
 
-// This struct is Erik's interpretation of what Julia is expecting for its "CuDevArray" type.
-template<typename T, int64_t N>
-struct CuDeviceArray {
-    T* ptr;
-    int64_t maxsize;
-    int64_t dims[N];
-    int64_t len;
-};
-typedef CuDeviceArray<int32_t, 1> kernel_arg;
-
-cudaEvent_t cudaFRBBeamformer::execute(int gpu_frame_id, const std::vector<cudaEvent_t>& pre_events,
-                                       bool* quit) {
-    (void)pre_events;
-    (void)quit;
+cudaEvent_t cuda::execute(const int gpu_frame_id, const std::vector<cudaEvent_t>& /*pre_events*/,
+                          bool* const /*quit*/) {
     pre_execute(gpu_frame_id);
 
-    void* dishlayout_memory = device.get_gpu_memory(_gpu_mem_dishlayout, dishlayout_len);
-    void* phase_memory = device.get_gpu_memory_array(_gpu_mem_phase, gpu_frame_id, phase_len);
-    void* voltage_memory = device.get_gpu_memory_array(_gpu_mem_voltage, gpu_frame_id, voltage_len);
-    void* beamgrid_memory =
-        device.get_gpu_memory_array(_gpu_mem_beamgrid, gpu_frame_id, beamgrid_len);
-    int32_t* info_memory = (int32_t*)device.get_gpu_memory(_gpu_mem_info, info_len);
+    void* const S_memory = device.get_gpu_memory_array(S_memname, gpu_frame_id, S_length);
+    void* const W_memory = device.get_gpu_memory_array(W_memname, gpu_frame_id, W_length);
+    void* const E_memory = device.get_gpu_memory_array(E_memname, gpu_frame_id, E_length);
+    void* const I_memory = device.get_gpu_memory_array(I_memname, gpu_frame_id, I_length);
+    void* const info_memory = device.get_gpu_memory_array(info_memname, gpu_frame_id, info_length);
 
     record_start_event(gpu_frame_id);
 
-    // Initialize info_memory return codes
-    CHECK_CUDA_ERROR(
-        cudaMemsetAsync(info_memory, 0xff, info_len, device.getStream(cuda_stream_id)));
+    const char* exc_arg = "exception";
+    kernel_arg S_arg(S_memory, S_length);
+    kernel_arg W_arg(W_memory, W_length);
+    kernel_arg E_arg(E_memory, E_length);
+    kernel_arg I_arg(I_memory, I_length);
+    kernel_arg info_arg(info_memory, info_length);
+    void* args[] = {&exc_arg, &S_arg, &W_arg, &E_arg, &I_arg, &info_arg};
 
-    // dishlayout (S), phase (W), voltage (E), beamgrid (I), info
-    const char* exc = "exception";
-    // kernel_arg arr[5];
-    kernel_arg arr[8];
-
-    arr[0].ptr = (int32_t*)dishlayout_memory;
-    arr[0].maxsize = dishlayout_len;
-    arr[0].dims[0] = dishlayout_len / sizeof(int16_t);
-    arr[0].len = dishlayout_len / sizeof(int16_t);
-
-    arr[1].ptr = (int32_t*)phase_memory;
-    arr[1].maxsize = phase_len;
-    arr[1].dims[0] = phase_len / sizeof_float16_t;
-    arr[1].len = phase_len / sizeof_float16_t;
-
-    arr[2].ptr = (int32_t*)voltage_memory;
-    arr[2].maxsize = voltage_len;
-    arr[2].dims[0] = voltage_len;
-    arr[2].len = voltage_len;
-
-    arr[3].ptr = (int32_t*)beamgrid_memory;
-    arr[3].maxsize = beamgrid_len;
-    arr[3].dims[0] = beamgrid_len / sizeof_float16_t;
-    arr[3].len = beamgrid_len / sizeof_float16_t;
-
-    arr[4].ptr = (int32_t*)info_memory;
-    arr[4].maxsize = info_len;
-    arr[4].dims[0] = info_len / sizeof(int32_t);
-    arr[4].len = info_len / sizeof(int32_t);
-
-    arr[5].ptr = (int32_t*)nullptr;
-    arr[5].maxsize = 0;
-    arr[5].dims[0] = 0;
-    arr[5].len = 0;
-
-    arr[6].ptr = (int32_t*)nullptr;
-    arr[6].maxsize = 0;
-    arr[6].dims[0] = 0;
-    arr[6].len = 0;
-
-    arr[7].ptr = (int32_t*)nullptr;
-    arr[7].maxsize = 0;
-    arr[7].dims[0] = 0;
-    arr[7].len = 0;
-
-    void* parameters[] = {
-        &(exc),    &(arr[0]), &(arr[1]), &(arr[2]), &(arr[3]),
-        &(arr[4]), &(arr[5]), &(arr[6]), &(arr[7]),
-    };
-
-    DEBUG("Kernel_name: {}", kernel_name);
-    CHECK_CU_ERROR(cuFuncSetAttribute(runtime_kernels[kernel_name],
+    DEBUG("kernel_symbol: {}", kernel_symbol);
+    DEBUG("runtime_kernels[kernel_symbol]: {}", static_cast<void*>(runtime_kernels[kernel_symbol]));
+    CHECK_CU_ERROR(cuFuncSetAttribute(runtime_kernels[kernel_symbol],
                                       CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
-                                      shared_mem_bytes));
+                                      shmem_bytes));
 
-    DEBUG("Running CUDA FRB Beamformer on GPU frame {:d}", gpu_frame_id);
-    CHECK_CU_ERROR(cuLaunchKernel(runtime_kernels[kernel_name], blocks_x, blocks_y, 1, threads_x,
-                                  threads_y, 1, shared_mem_bytes, device.getStream(cuda_stream_id),
-                                  parameters, NULL));
+    DEBUG("Running CUDA  on GPU frame {:d}", gpu_frame_id);
+    const CUresult err =
+        cuLaunchKernel(runtime_kernels[kernel_symbol], blocks, 1, 1, threads_x, threads_y, 1,
+                       shmem_bytes, device.getStream(cuda_stream_id), args, NULL);
 
-    // Copy "info" result code back to host memory
-    CHECK_CUDA_ERROR(cudaMemcpyAsync(host_info.data(), info_memory, info_len,
-                                     cudaMemcpyDeviceToHost, device.getStream(cuda_stream_id)));
+    if (err != CUDA_SUCCESS) {
+        const char* errStr;
+        cuGetErrorString(err, &errStr);
+        INFO("Error number: {}", err);
+        ERROR("cuLaunchKernel: {}", errStr);
+    }
 
     return record_end_event(gpu_frame_id);
-}
-
-void cudaFRBBeamformer::finalize_frame(int gpu_frame_id) {
-    cudaCommand::finalize_frame(gpu_frame_id);
-    for (size_t i = 0; i < host_info.size(); i++)
-        if (host_info[i] != 0)
-            ERROR("cudaFRBBeamformer returned 'info' value {:d} at index {:d} (zero indicates no "
-                  "error)",
-                  host_info[i], i);
 }
--- lib/cuda/cudaUpchannelizer_U16.cpp
+++ lib/cuda/cudaUpchannelizer_U16.cpp
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief CUDA BasebandBeamformer kernel
+ * @brief CUDA Upchannelizer_U16 kernel
  *
  * This file has been generated automatically.
  * Do not modify this C++ file, your changes will be lost.
@@ -19,14 +19,14 @@ using kotekan::bufferContainer;
 using kotekan::Config;
 
 /**
- * @class cudaBasebandBeamformer
- * @brief cudaCommand for BasebandBeamformer
+ * @class cudaUpchannelizer_U16
+ * @brief cudaCommand for Upchannelizer_U16
  */
-class cudaBasebandBeamformer : public cudaCommand {
+class cudaUpchannelizer_U16 : public cudaCommand {
 public:
-    cudaBasebandBeamformer(Config& config, const std::string& unique_name,
-                           bufferContainer& host_buffers, cudaDeviceInterface& device);
-    ~cudaBasebandBeamformer();
+    cudaUpchannelizer_U16(Config& config, const std::string& unique_name,
+                          bufferContainer& host_buffers, cudaDeviceInterface& device);
+    ~cudaUpchannelizer_U16();
     cudaEvent_t execute(int gpu_frame_id, const std::vector<cudaEvent_t>& pre_events,
                         bool* quit) override;
 
@@ -45,64 +45,60 @@ private:
     using kernel_arg = CuDeviceArray<int32_t, 1>;
 
     // Kernel design parameters:
-    static constexpr int cuda_number_of_beams = 96;
     static constexpr int cuda_number_of_complex_components = 2;
     static constexpr int cuda_number_of_dishes = 512;
     static constexpr int cuda_number_of_frequencies = 16;
     static constexpr int cuda_number_of_polarizations = 2;
+    static constexpr int cuda_number_of_taps = 4;
     static constexpr int cuda_number_of_timesamples = 32768;
-    static constexpr int cuda_shift_parameter_sigma = 3;
+    static constexpr int cuda_upchannelization_factor = 16;
 
     // Kernel compile parameters:
-    static constexpr int minthreads = 768;
-    static constexpr int blocks_per_sm = 1;
+    static constexpr int minthreads = 512;
+    static constexpr int blocks_per_sm = 2;
 
     // Kernel call parameters:
     static constexpr int threads_x = 32;
-    static constexpr int threads_y = 24;
-    static constexpr int blocks = 512;
-    static constexpr int shmem_bytes = 67712;
+    static constexpr int threads_y = 16;
+    static constexpr int blocks = 128;
+    static constexpr int shmem_bytes = 69888;
 
     // Kernel name:
-    const char* const kernel_symbol =
-        "_Z13julia_bb_363513CuDeviceArrayI6Int8x4Li1ELi1EES_I6Int4x8Li1ELi1EES_I5Int32Li1ELi1EES_"
-        "IS1_Li1ELi1EES_IS2_Li1ELi1EE";
+    const char* const kernel_symbol = "_Z17julia_upchan_405313CuDeviceArrayI9Float16x2Li1ELi1EES_"
+                                      "I6Int4x8Li1ELi1EES_IS1_Li1ELi1EES_I5Int32Li1ELi1EE";
 
     // Kernel arguments:
-    static constexpr std::size_t A_length = 3145728UL;
+    static constexpr std::size_t G_length = 512UL;
     static constexpr std::size_t E_length = 536870912UL;
-    static constexpr std::size_t s_length = 12288UL;
-    static constexpr std::size_t J_length = 100663296UL;
-    static constexpr std::size_t info_length = 1572864UL;
+    static constexpr std::size_t Ebar_length = 536870912UL;
+    static constexpr std::size_t info_length = 262144UL;
 
     // Runtime parameters:
+    std::vector<float> freq_gains;
 
     // GPU memory:
-    const std::string A_memname;
+    const std::string G_memname;
     const std::string E_memname;
-    const std::string s_memname;
-    const std::string J_memname;
+    const std::string Ebar_memname;
     const std::string info_memname;
 };
 
-REGISTER_CUDA_COMMAND(cudaBasebandBeamformer);
+REGISTER_CUDA_COMMAND(cudaUpchannelizer_U16);
 
-cudaBasebandBeamformer::cudaBasebandBeamformer(Config& config, const std::string& unique_name,
-                                               bufferContainer& host_buffers,
-                                               cudaDeviceInterface& device) :
-    cudaCommand(config, unique_name, host_buffers, device, "BasebandBeamformer",
-                "BasebandBeamformer.ptx"),
-    A_memname(config.get<std::string>(unique_name, "gpu_mem_phase")),
-    E_memname(config.get<std::string>(unique_name, "gpu_mem_voltage")),
-    s_memname(config.get<std::string>(unique_name, "gpu_mem_output_scaling")),
-    J_memname(config.get<std::string>(unique_name, "gpu_mem_formed_beams")),
+cudaUpchannelizer_U16::cudaUpchannelizer_U16(Config& config, const std::string& unique_name,
+                                             bufferContainer& host_buffers,
+                                             cudaDeviceInterface& device) :
+    cudaCommand(config, unique_name, host_buffers, device, "Upchannelizer_U16",
+                "Upchannelizer_U16.ptx"),
+    G_memname(config.get<std::string>(unique_name, "gpu_mem_gain")),
+    E_memname(config.get<std::string>(unique_name, "gpu_mem_input_voltage")),
+    Ebar_memname(config.get<std::string>(unique_name, "gpu_mem_output_voltage")),
     info_memname(config.get<std::string>(unique_name, "gpu_mem_info")) {
-    const int num_elements = config.get<int>(unique_name, "num_elements");
-    if (num_elements != (cuda_number_of_dishes * cuda_number_of_polarizations))
-        throw std::runtime_error(
-            "The num_elements config setting must be "
-            + std::to_string(cuda_number_of_dishes * cuda_number_of_polarizations)
-            + " for the CUDA Baseband Beamformer");
+    const int num_dishes = config.get<int>(unique_name, "num_dishes");
+    if (num_dishes != (cuda_number_of_dishes))
+        throw std::runtime_error("The num_dishes config setting must be "
+                                 + std::to_string(cuda_number_of_dishes)
+                                 + " for the CUDA Baseband Beamformer");
     const int num_local_freq = config.get<int>(unique_name, "num_local_freq");
     if (num_local_freq != (cuda_number_of_frequencies))
         throw std::runtime_error("The num_local_freq config setting must be "
@@ -113,12 +109,19 @@ cudaBasebandBeamformer::cudaBasebandBeamformer(Config& config, const std::string
         throw std::runtime_error("The samples_per_data_set config setting must be "
                                  + std::to_string(cuda_number_of_timesamples)
                                  + " for the CUDA Baseband Beamformer");
-    const int num_beams = config.get<int>(unique_name, "num_beams");
-    if (num_beams != (cuda_number_of_beams))
-        throw std::runtime_error("The num_beams config setting must be "
-                                 + std::to_string(cuda_number_of_beams)
+    const int upchan_factor = config.get<int>(unique_name, "upchan_factor");
+    if (upchan_factor != (cuda_upchannelization_factor))
+        throw std::runtime_error("The upchan_factor config setting must be "
+                                 + std::to_string(cuda_upchannelization_factor)
                                  + " for the CUDA Baseband Beamformer");
 
+    const std::vector<float> freq_gains = config.get<std::vector<float>>(unique_name, "freq_gains");
+    std::vector<float16_t> freq_gains16(freq_gains.size());
+    for (std::size_t i = 0; i < freq_gains16.size(); i++)
+        freq_gains16[i] = freq_gains[i];
+    const void* const G_host = freq_gains16.data();
+    void* const G_memory = device.get_gpu_memory(G_memname, G_length);
+    CHECK_CUDA_ERROR(cudaMemcpy(G_memory, G_host, G_length, cudaMemcpyHostToDevice));
 
     set_command_type(gpuCommandType::KERNEL);
     const std::vector<std::string> opts = {
@@ -128,28 +131,26 @@ cudaBasebandBeamformer::cudaBasebandBeamformer(Config& config, const std::string
     build_ptx({kernel_symbol}, opts);
 }
 
-cudaBasebandBeamformer::~cudaBasebandBeamformer() {}
+cudaUpchannelizer_U16::~cudaUpchannelizer_U16() {}
 
-cudaEvent_t cudaBasebandBeamformer::execute(const int gpu_frame_id,
-                                            const std::vector<cudaEvent_t>& /*pre_events*/,
-                                            bool* const /*quit*/) {
+cudaEvent_t cudaUpchannelizer_U16::execute(const int gpu_frame_id,
+                                           const std::vector<cudaEvent_t>& /*pre_events*/,
+                                           bool* const /*quit*/) {
     pre_execute(gpu_frame_id);
 
-    void* const A_memory = device.get_gpu_memory_array(A_memname, gpu_frame_id, A_length);
+    void* const G_memory = device.get_gpu_memory_array(G_memname, gpu_frame_id, G_length);
     void* const E_memory = device.get_gpu_memory_array(E_memname, gpu_frame_id, E_length);
-    void* const s_memory = device.get_gpu_memory_array(s_memname, gpu_frame_id, s_length);
-    void* const J_memory = device.get_gpu_memory_array(J_memname, gpu_frame_id, J_length);
+    void* const Ebar_memory = device.get_gpu_memory_array(Ebar_memname, gpu_frame_id, Ebar_length);
     void* const info_memory = device.get_gpu_memory_array(info_memname, gpu_frame_id, info_length);
 
     record_start_event(gpu_frame_id);
 
     const char* exc_arg = "exception";
-    kernel_arg A_arg(A_memory, A_length);
+    kernel_arg G_arg(G_memory, G_length);
     kernel_arg E_arg(E_memory, E_length);
-    kernel_arg s_arg(s_memory, s_length);
-    kernel_arg J_arg(J_memory, J_length);
+    kernel_arg Ebar_arg(Ebar_memory, Ebar_length);
     kernel_arg info_arg(info_memory, info_length);
-    void* args[] = {&exc_arg, &A_arg, &E_arg, &s_arg, &J_arg, &info_arg};
+    void* args[] = {&exc_arg, &G_arg, &E_arg, &Ebar_arg, &info_arg};
 
     DEBUG("kernel_symbol: {}", kernel_symbol);
     DEBUG("runtime_kernels[kernel_symbol]: {}", static_cast<void*>(runtime_kernels[kernel_symbol]));
