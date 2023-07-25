c4790b398431eb2b5dca2a5e0eeb578ffea6b655
Dustin Lang <dstndstn@gmail.com>
2023-07-25 20:01:10 +0000

    Fix buffer.h includes in hsa and opencl

--- lib/hsa/hsaRfiMaskOutput.cpp
+++ lib/hsa/hsaRfiMaskOutput.cpp
@@ -1,6 +1,6 @@
 #include "hsaRfiMaskOutput.hpp"
 
-#include "buffer.h"               // for Buffer, mark_frame_empty, mark_frame_full, pass_metadata
+#include "buffer.hpp"               // for Buffer, mark_frame_empty, mark_frame_full, pass_metadata
 #include "bufferContainer.hpp"    // for bufferContainer
 #include "gpuCommand.hpp"         // for gpuCommandType, gpuCommandType::COPY_OUT
 #include "hsaCommand.hpp"         // for REGISTER_HSA_COMMAND, _factory_aliashsaCommand, hsaCom...
