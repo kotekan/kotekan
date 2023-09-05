b428a4517b1fb338c96f69dd55d7a1ccc3eb3795
Erik Schnetter <schnetter@gmail.com>
2023-09-05 16:29:09 -0400

    FindJulia.cmake: Reformat

--- cmake/FindJulia.cmake
+++ cmake/FindJulia.cmake
@@ -2,7 +2,8 @@
 # <https://github.com/JuliaInterop/libcxxwrap-julia/blob/main/FindJulia.cmake>
 
 # Original FindJulia.cmake from
-# <https://github.com/QuantStack/xtensor-julia-cookiecutter/blob/master/%7B%7Bcookiecutter.github_project_name%7D%7D/cmake/FindJulia.cmake>
+# <https://github.com/QuantStack/xtensor-julia-cookiecutter/
+# blob/master/%7B%7Bcookiecutter.github_project_name%7D%7D/cmake/FindJulia.cmake>
 
 if(Julia_FOUND)
     return()
@@ -56,33 +57,36 @@ if(Julia_EXECUTABLE)
     endif()
 
     if(DEFINED ENV{JULIA_INCLUDE_DIRS})
-        set(Julia_INCLUDE_DIRS
+        set(JULIA_INCLUDE_DIRS
             $ENV{JULIA_INCLUDE_DIRS}
             CACHE STRING "Location of Julia include files")
     elseif(Julia_EXECUTABLE)
         execute_process(
             COMMAND
                 ${Julia_EXECUTABLE} --startup-file=no -E
-                "julia_include_dir = joinpath(match(r\"(.*)(bin)\",${JULIA_HOME_NAME}).captures[1],\"include\",\"julia\")\n
-            if !isdir(julia_include_dir)  # then we're running directly from build\n
-            julia_base_dir_aux = splitdir(splitdir(${JULIA_HOME_NAME})[1])[1]  # useful for running-from-build\n
-            julia_include_dir = joinpath(julia_base_dir_aux, \"usr\", \"include\" )\n
-            julia_include_dir *= \";\" * joinpath(julia_base_dir_aux, \"src\", \"support\" )\n
-            julia_include_dir *= \";\" * joinpath(julia_base_dir_aux, \"src\" )\n
-            end\n
-            julia_include_dir"
-            OUTPUT_VARIABLE Julia_INCLUDE_DIRS)
+                "julia_include_dir = joinpath(match(r\"(.*)(bin)\",${JULIA_HOME_NAME}).captures[1],
+                                              \"include\",
+                                              \"julia\")\n
+                if !isdir(julia_include_dir)  # then we're running directly from build\n
+                # useful for running-from-build:\n
+                julia_base_dir_aux = splitdir(splitdir(${JULIA_HOME_NAME})[1])[1]\n
+                julia_include_dir = joinpath(julia_base_dir_aux, \"usr\", \"include\" )\n
+                julia_include_dir *= \";\" * joinpath(julia_base_dir_aux, \"src\", \"support\")\n
+                julia_include_dir *= \";\" * joinpath(julia_base_dir_aux, \"src\")\n
+                end\n
+                julia_include_dir"
+            OUTPUT_VARIABLE JULIA_INCLUDE_DIRS)
 
-        string(REGEX REPLACE "\"" "" Julia_INCLUDE_DIRS "${Julia_INCLUDE_DIRS}")
-        string(REGEX REPLACE "\n" "" Julia_INCLUDE_DIRS "${Julia_INCLUDE_DIRS}")
-        set(Julia_INCLUDE_DIRS
-            ${Julia_INCLUDE_DIRS}
+        string(REGEX REPLACE "\"" "" JULIA_INCLUDE_DIRS "${JULIA_INCLUDE_DIRS}")
+        string(REGEX REPLACE "\n" "" JULIA_INCLUDE_DIRS "${JULIA_INCLUDE_DIRS}")
+        set(JULIA_INCLUDE_DIRS
+            ${JULIA_INCLUDE_DIRS}
             CACHE PATH "Location of Julia include files")
     elseif(Julia_PREFIX)
-        set(Julia_INCLUDE_DIRS ${Julia_PREFIX}/include/julia)
+        set(JULIA_INCLUDE_DIRS ${Julia_PREFIX}/include/julia)
     endif()
-    set(Julia_INCLUDE_DIRS ${Julia_INCLUDE_DIRS};$ENV{includedir})
-    message(STATUS "Julia_INCLUDE_DIRS:   ${Julia_INCLUDE_DIRS}")
+    set(JULIA_INCLUDE_DIRS ${JULIA_INCLUDE_DIRS};$ENV{includedir})
+    message(STATUS "JULIA_INCLUDE_DIRS:   ${JULIA_INCLUDE_DIRS}")
 
     # ##############################################################################################
     # Julia Libraries #
@@ -96,38 +100,40 @@ if(Julia_EXECUTABLE)
         execute_process(
             COMMAND
                 ${Julia_EXECUTABLE} --startup-file=no -E
-                "${USING_LIBDL}\nabspath(Libdl.dlpath((ccall(:jl_is_debugbuild, Cint, ()) != 0) ? \"libjulia-debug\" : \"libjulia\"))"
-            OUTPUT_VARIABLE Julia_LIBRARY)
+                "${USING_LIBDL}\n
+                abspath(Libdl.dlpath((ccall(:jl_is_debugbuild, Cint, ()) != 0) ? \"libjulia-debug\"
+                                                                               : \"libjulia\"))"
+            OUTPUT_VARIABLE JULIA_LIBRARY)
 
-        string(REGEX REPLACE "\"" "" Julia_LIBRARY "${Julia_LIBRARY}")
-        string(REGEX REPLACE "\n" "" Julia_LIBRARY "${Julia_LIBRARY}")
-        string(STRIP "${Julia_LIBRARY}" Julia_LIBRARY)
+        string(REGEX REPLACE "\"" "" JULIA_LIBRARY "${JULIA_LIBRARY}")
+        string(REGEX REPLACE "\n" "" JULIA_LIBRARY "${JULIA_LIBRARY}")
+        string(STRIP "${JULIA_LIBRARY}" JULIA_LIBRARY)
 
         if(WIN32)
-            get_filename_component(Julia_LIBRARY_DIR ${Julia_LIBRARY} DIRECTORY)
-            get_filename_component(Julia_LIBRARY_DIR ${Julia_LIBRARY_DIR} DIRECTORY)
+            get_filename_component(JULIA_LIBRARY_DIR ${JULIA_LIBRARY} DIRECTORY)
+            get_filename_component(JULIA_LIBRARY_DIR ${JULIA_LIBRARY_DIR} DIRECTORY)
             find_library(
-                win_Julia_LIBRARY
+                win_JULIA_LIBRARY
                 NAMES libjulia.dll.a
-                PATHS "${Julia_LIBRARY_DIR}/lib"
+                PATHS "${JULIA_LIBRARY_DIR}/lib"
                 NO_DEFAULT_PATH)
-            set(Julia_LIBRARY "${win_Julia_LIBRARY}")
+            set(JULIA_LIBRARY "${win_JULIA_LIBRARY}")
         endif()
 
-        set(Julia_LIBRARY
-            "${Julia_LIBRARY}"
+        set(JULIA_LIBRARY
+            "${JULIA_LIBRARY}"
             CACHE PATH "Julia library")
     else()
         find_library(
-            Julia_LIBRARY
+            JULIA_LIBRARY
             NAMES libjulia.${Julia_VERSION_STRING}.dylib julia libjulia.dll.a libjulia
             CMAKE_FIND_ROOT_PATH_BOTH)
     endif()
 
-    get_filename_component(Julia_LIBRARY_DIR ${Julia_LIBRARY} DIRECTORY)
+    get_filename_component(JULIA_LIBRARY_DIR ${JULIA_LIBRARY} DIRECTORY)
 
-    message(STATUS "Julia_LIBRARY_DIR:    ${Julia_LIBRARY_DIR}")
-    message(STATUS "Julia_LIBRARY:        ${Julia_LIBRARY}")
+    message(STATUS "JULIA_LIBRARY_DIR:    ${JULIA_LIBRARY_DIR}")
+    message(STATUS "JULIA_LIBRARY:        ${JULIA_LIBRARY}")
 
     # ##############################################################################################
     # JULIA_HOME #
@@ -159,7 +165,7 @@ if(Julia_EXECUTABLE)
     # Check for Existence of Headers #
     # ##############################################################################################
 
-    find_path(Julia_MAIN_HEADER julia.h HINTS ${Julia_INCLUDE_DIRS})
+    find_path(Julia_MAIN_HEADER julia.h HINTS ${JULIA_INCLUDE_DIRS})
 
     # ##############################################################################################
     # Determine if we are on 32 or 64 bit #
@@ -185,6 +191,6 @@ endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     Julia
-    REQUIRED_VARS Julia_LIBRARY Julia_LIBRARY_DIR Julia_INCLUDE_DIRS Julia_MAIN_HEADER
+    REQUIRED_VARS JULIA_LIBRARY JULIA_LIBRARY_DIR JULIA_INCLUDE_DIRS Julia_MAIN_HEADER
     VERSION_VAR Julia_VERSION_STRING
     FAIL_MESSAGE "Julia not found")
