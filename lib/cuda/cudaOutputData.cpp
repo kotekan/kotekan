13746cf90d3bd429972713dd0103e04b7cab058a
Dustin Lang <dstndstn@gmail.com>
2023-09-13 20:02:41 +0000

    cudaOutputData: only copy metadata if we actually generated an output frame!

--- lib/cuda/cudaOutputData.cpp
+++ lib/cuda/cudaOutputData.cpp
@@ -120,12 +120,6 @@ void cudaOutputData::finalize_frame(int frame_id) {
     cudaCommand::finalize_frame(frame_id);
 
     if (in_buffer) {
-        DEBUG("Passing metadata from input (host) buffer {:s}[{:d}] to {:s}[{:d}]",
-              in_buffer->buffer_name, in_buffer_id, output_buffer->buffer_name, output_buffer_id);
-        pass_metadata(in_buffer, in_buffer_id, output_buffer, output_buffer_id);
-    }
-
-    if (in_buffer) {
         mark_frame_empty(in_buffer, unique_name.c_str(), in_buffer_id);
         in_buffer_id = (in_buffer_id + 1) % in_buffer->num_frames;
     }
@@ -134,8 +128,15 @@ void cudaOutputData::finalize_frame(int frame_id) {
         DEBUG("Did not generate output for input GPU frame {:d}", frame_id);
         return;
     }
-    DEBUG("Generating output {:s}[{:d}] for input GPU frame {:d}", output_buffer->buffer_name,
+
+    DEBUG("Generated CPU output {:s}[{:d}] for input GPU frame {:d}", output_buffer->buffer_name,
           output_buffer_id, frame_id);
+
+    if (in_buffer) {
+        DEBUG("Passing metadata from input (host) buffer {:s}[{:d}] to {:s}[{:d}]",
+              in_buffer->buffer_name, in_buffer_id, output_buffer->buffer_name, output_buffer_id);
+        pass_metadata(in_buffer, in_buffer_id, output_buffer, output_buffer_id);
+    }
 
     mark_frame_full(output_buffer, unique_name.c_str(), output_buffer_id);
     output_buffer_id = (output_buffer_id + 1) % output_buffer->num_frames;
