631018640228bc9f63b80284b3f09292cc5d2e40
Dustin Lang <dstndstn@gmail.com>
2023-09-22 18:16:11 +0000

    push BeamReformer initialization into instance==0 blocks

--- lib/cuda/cudaFRBBeamReformer.cpp
+++ lib/cuda/cudaFRBBeamReformer.cpp
@@ -93,12 +93,15 @@ cudaFRBBeamReformer::cudaFRBBeamReformer(Config& config, const std::string& uniq
     _gpu_mem_beamout = config.get<std::string>(unique_name, "gpu_mem_beamout");
     _cuda_streams =
         config.get_default<std::vector<int>>(unique_name, "cuda_streams", std::vector<int>());
-    for (size_t i = 0; i < _cuda_streams.size(); i++) {
-        if (_cuda_streams[i] >= device.get_num_streams()) {
-            ERROR("Error: cudaFRBBeamReformer's config setting cuda_streams must have all elements "
-                  "< number of streams on the device = {:d}",
-                  device.get_num_streams());
-        }
+    int nstreams = std::max((int)_cuda_streams.size(), 1);
+    if (inst == 0) {
+        for (size_t i = 0; i < _cuda_streams.size(); i++)
+            if (_cuda_streams[i] >= device.get_num_streams())
+                ERROR("Error: cudaFRBBeamReformer's config setting cuda_streams must have all elements "
+                      "< number of streams on the device = {:d}",
+                      device.get_num_streams());
+        if (_num_local_freq % nstreams != 0)
+            ERROR("Number of CUDA streams must evenly divide number of frequencies!");
     }
 
     gpu_buffers_used.push_back(std::make_tuple(_gpu_mem_beamgrid, true, true, false));
@@ -123,182 +126,180 @@ cudaFRBBeamReformer::cudaFRBBeamReformer(Config& config, const std::string& uniq
         std::abort();
     }
 
-    // HACK -- for Minimum-Viable-Product purposes, we're going to
-    // hard-code the FRB beamformer beam locations on the sky.  In
-    // turn, this requires knowing the actual frequencies we're
-    // processing!
-    float* beam_dra = (float*)malloc(_num_beams * sizeof(float));
-    assert(beam_dra);
-    float* beam_ddec = (float*)malloc(_num_beams * sizeof(float));
-    assert(beam_ddec);
-    float* freqs = (float*)malloc(_num_local_freq * sizeof(float));
-    assert(freqs);
+    if (inst == 0) {
+        // HACK -- for Minimum-Viable-Product purposes, we're going to
+        // hard-code the FRB beamformer beam locations on the sky.  In
+        // turn, this requires knowing the actual frequencies we're
+        // processing!
+        float* beam_dra = (float*)malloc(_num_beams * sizeof(float));
+        assert(beam_dra);
+        float* beam_ddec = (float*)malloc(_num_beams * sizeof(float));
+        assert(beam_ddec);
+        float* freqs = (float*)malloc(_num_local_freq * sizeof(float));
+        assert(freqs);
 
-    // TODO -- in the real thing this should be an updatable config
-    // or somesuch.  Tracking beams would update single elements of
-    // these arrays.
-    // We'll place beams on an RA,Dec grid, expressed in *degrees*
-    // from -1.0 to +1.0
-    float dra_min = -1.0;
-    float dra_max = +1.0;
-    float ddec_min = -1.0;
-    float ddec_max = +1.0;
-    int Nra = 50;
-    int Ndec = 100;
-    assert(Nra * Ndec == _num_beams);
-    assert(_num_beams == 5000);
-    for (int i = 0; i < Nra; i++) {
-        for (int j = 0; j < Ndec; j++) {
-            beam_dra[i * Ndec + j] = dra_min + (dra_max - dra_min) * (float)i / (Nra - 1);
-            beam_ddec[i * Ndec + j] = ddec_min + (ddec_max - ddec_min) * (float)j / (Ndec - 1);
+        // TODO -- in the real thing this should be an updatable config
+        // or somesuch.  Tracking beams would update single elements of
+        // these arrays.
+        // We'll place beams on an RA,Dec grid, expressed in *degrees*
+        // from -1.0 to +1.0
+        float dra_min = -1.0;
+        float dra_max = +1.0;
+        float ddec_min = -1.0;
+        float ddec_max = +1.0;
+        int Nra = 50;
+        int Ndec = 100;
+        assert(Nra * Ndec == _num_beams);
+        assert(_num_beams == 5000);
+        for (int i = 0; i < Nra; i++) {
+            for (int j = 0; j < Ndec; j++) {
+                beam_dra[i * Ndec + j] = dra_min + (dra_max - dra_min) * (float)i / (Nra - 1);
+                beam_ddec[i * Ndec + j] = ddec_min + (ddec_max - ddec_min) * (float)j / (Ndec - 1);
+            }
         }
-    }
 
-    // TODO -- the real frequencies should be passed in with the
-    // metadata, probably!
-    for (int i = 0; i < _num_local_freq; i++) {
-        freqs[i] = 600e6 + i * (1.2e9 / 65536.);
-    }
-
-    // TODO -- the dish spacings, in meters, should be passed in as config parameters!
-    float dish_spacing_ew = 6.3;
-    float dish_spacing_ns = 8.5;
-
-    // TODO -- the zenith distance of the dishes, in degrees.  We'll
-    // assume the dishes remain pointed at HA=0, ie, they're always on
-    // the meridian.
-    float bore_zd = 30.;
-
-    float16_t* host_phase = (float16_t*)malloc(phase_len);
-    assert(host_phase);
-
-    const char* beamphase_cache_fn = "beamphase-cache.bin";
-    FILE* f = fopen(beamphase_cache_fn, "r");
-    bool gotit = false;
-    if (f) {
-        INFO("Trying to read beamformer phase matrix from {:s}...", beamphase_cache_fn);
-        double t0 = gettime();
-        size_t nr = fread(host_phase, 1, phase_len, f);
-        if (nr != phase_len) {
-            INFO("Reading file {:s}: got {:d} bytes, but expected {:d}", beamphase_cache_fn, nr,
-                 phase_len);
-        } else {
-            gotit = true;
+        // TODO -- the real frequencies should be passed in with the
+        // metadata, probably!
+        for (int i = 0; i < _num_local_freq; i++) {
+            freqs[i] = 600e6 + i * (1.2e9 / 65536.);
         }
-        fclose(f);
-        INFO("That took {:g} sec", gettime() - t0);
-    }
-    if (!gotit) {
-        int M = _beam_grid_size / 2;
-        int N = M;
-        INFO("Computing beam-reformer phase matrix...");
-        double t0 = gettime();
-        compute_beam_reformer_phase(M, N, host_phase, _num_local_freq, _num_beams, beam_dra,
-                                    beam_ddec, freqs, dish_spacing_ew, dish_spacing_ns, bore_zd);
-        INFO("That took {:g} sec", gettime() - t0);
-        INFO("Computed beam-reformer phase matrix");
-        f = fopen(beamphase_cache_fn, "w+");
+
+        // TODO -- the dish spacings, in meters, should be passed in as config parameters!
+        float dish_spacing_ew = 6.3;
+        float dish_spacing_ns = 8.5;
+
+        // TODO -- the zenith distance of the dishes, in degrees.  We'll
+        // assume the dishes remain pointed at HA=0, ie, they're always on
+        // the meridian.
+        float bore_zd = 30.;
+
+        float16_t* host_phase = (float16_t*)malloc(phase_len);
+        assert(host_phase);
+
+        const char* beamphase_cache_fn = "beamphase-cache.bin";
+        FILE* f = fopen(beamphase_cache_fn, "r");
+        bool gotit = false;
         if (f) {
-            size_t nw = fwrite(host_phase, 1, phase_len, f);
-            if (nw != phase_len) {
-                INFO("Failed to write beamformer phase matrix to {:s}: {:s}", beamphase_cache_fn,
-                     strerror(errno));
-                fclose(f);
-                unlink(beamphase_cache_fn);
+            INFO("Trying to read beamformer phase matrix from {:s}...", beamphase_cache_fn);
+            double t0 = gettime();
+            size_t nr = fread(host_phase, 1, phase_len, f);
+            if (nr != phase_len) {
+                INFO("Reading file {:s}: got {:d} bytes, but expected {:d}", beamphase_cache_fn, nr,
+                     phase_len);
             } else {
-                if (fclose(f)) {
-                    INFO("Failed to close beamformer phase matrix in {:s}: {:s}",
-                         beamphase_cache_fn, strerror(errno));
+                gotit = true;
+            }
+            fclose(f);
+            INFO("That took {:g} sec", gettime() - t0);
+        }
+        if (!gotit) {
+            int M = _beam_grid_size / 2;
+            int N = M;
+            INFO("Computing beam-reformer phase matrix...");
+            double t0 = gettime();
+            compute_beam_reformer_phase(M, N, host_phase, _num_local_freq, _num_beams, beam_dra,
+                                        beam_ddec, freqs, dish_spacing_ew, dish_spacing_ns, bore_zd);
+            INFO("That took {:g} sec", gettime() - t0);
+            INFO("Computed beam-reformer phase matrix");
+            f = fopen(beamphase_cache_fn, "w+");
+            if (f) {
+                size_t nw = fwrite(host_phase, 1, phase_len, f);
+                if (nw != phase_len) {
+                    INFO("Failed to write beamformer phase matrix to {:s}: {:s}", beamphase_cache_fn,
+                         strerror(errno));
+                    fclose(f);
                     unlink(beamphase_cache_fn);
                 } else {
-                    INFO("Wrote beamformer phase matrix to {:s}", beamphase_cache_fn);
+                    if (fclose(f)) {
+                        INFO("Failed to close beamformer phase matrix in {:s}: {:s}",
+                             beamphase_cache_fn, strerror(errno));
+                        unlink(beamphase_cache_fn);
+                    } else {
+                        INFO("Wrote beamformer phase matrix to {:s}", beamphase_cache_fn);
+                    }
                 }
             }
         }
+
+        float16_t* phase_memory = (float16_t*)device.get_gpu_memory(_gpu_mem_phase, phase_len);
+
+        CHECK_CUDA_ERROR(cudaMemcpy(phase_memory, host_phase, phase_len, cudaMemcpyHostToDevice));
+
+        free(host_phase);
+        free(beam_dra);
+        free(beam_ddec);
+        free(freqs);
+
+        // for cublas_hgemmBatched, pre-compute the GPU array pointers.
+        if (_batched) {
+            // GPU-memory arrays of pointers to the input & output
+            // matrices (in GPU memory)
+            __half** in;
+            __half** out;
+            __half** ph;
+            in = (__half**)device.get_gpu_memory(unique_name + "_batch_in",
+                                                 _gpu_buffer_depth * _num_local_freq * sizeof(__half*));
+            out = (__half**)device.get_gpu_memory(
+                                                  unique_name + "_batch_out", _gpu_buffer_depth * _num_local_freq * sizeof(__half*));
+            ph = (__half**)device.get_gpu_memory(unique_name + "_batch_ph",
+                                                 _num_local_freq * sizeof(__half*));
+
+            // Temporary CPU-memory arrays of pointers to the input &
+            // output matrices (which live in GPU memory); these will get
+            // copied to in/out/ph.
+            __half* host_in[_gpu_buffer_depth * _num_local_freq];
+            __half* host_out[_gpu_buffer_depth * _num_local_freq];
+            __half* host_ph[_gpu_buffer_depth * _num_local_freq];
+
+            // Final CPU-side pointers to GPU memory
+            _gpu_in_pointers.resize(_gpu_buffer_depth);
+            _gpu_out_pointers.resize(_gpu_buffer_depth);
+
+            int freqs_per_stream = _num_local_freq / nstreams;
+
+            // loop over gpu frames
+            for (int gpu_frame_id = 0; gpu_frame_id < _gpu_buffer_depth; gpu_frame_id++) {
+                // GPU input & output memory buffers for this gpu frame #.
+                float16_t* gpu_in_base = (float16_t*)device.get_gpu_memory_array(
+                                                                                 _gpu_mem_beamgrid, gpu_frame_id, beamgrid_len);
+                float16_t* gpu_out_base = (float16_t*)device.get_gpu_memory_array(
+                                                                                  _gpu_mem_beamout, gpu_frame_id, beamout_len);
+                // Compute the per-frequency matrix offsets.
+                for (int f = 0; f < _num_local_freq; f++) {
+                    host_in[gpu_frame_id * _num_local_freq + f] = gpu_in_base + (size_t)f * _Td * rho;
+                    host_out[gpu_frame_id * _num_local_freq + f] =
+                        gpu_out_base + (size_t)f * _Td * _num_beams;
+                    if (gpu_frame_id == 0)
+                        host_ph[f] = phase_memory + (size_t)f * rho * _num_beams;
+                }
+                // loop over streams and save the final GPU memory pointers (which we haven't yet filled
+                // with data!)
+                for (int i = 0; i < nstreams; i++) {
+                    _gpu_in_pointers[gpu_frame_id].push_back(
+                                                             in + (gpu_frame_id * nstreams + i) * freqs_per_stream);
+                    _gpu_out_pointers[gpu_frame_id].push_back(
+                                                              out + (gpu_frame_id * nstreams + i) * freqs_per_stream);
+                    if (gpu_frame_id == 0)
+                        _gpu_phase_pointers.push_back(ph + i * freqs_per_stream);
+                }
+            }
+            // Now copy the GPU pointer offsets (that we computed on the CPU) over to the GPU!
+            CHECK_CUDA_ERROR(cudaMemcpy(in, host_in,
+                                        _gpu_buffer_depth * _num_local_freq * sizeof(__half*),
+                                        cudaMemcpyHostToDevice));
+            CHECK_CUDA_ERROR(cudaMemcpy(out, host_out,
+                                        _gpu_buffer_depth * _num_local_freq * sizeof(__half*),
+                                        cudaMemcpyHostToDevice));
+            CHECK_CUDA_ERROR(
+                             cudaMemcpy(ph, host_ph, _num_local_freq * sizeof(__half*), cudaMemcpyHostToDevice));
+        }
     }
 
-    float16_t* phase_memory = (float16_t*)device.get_gpu_memory(_gpu_mem_phase, phase_len);
-
-    CHECK_CUDA_ERROR(cudaMemcpy(phase_memory, host_phase, phase_len, cudaMemcpyHostToDevice));
-
-    free(host_phase);
-    free(beam_dra);
-    free(beam_ddec);
-    free(freqs);
-
-    int nstreams = std::max((int)_cuda_streams.size(), 1);
-    if (_num_local_freq % nstreams != 0) {
-        ERROR("Number of CUDA streams must evenly divide number of frequencies!");
-    }
     if (nstreams == 1) {
         // We MUST set the stream -- otherwise it uses the CUDA
         // default stream, which != our default compute stream!
         DEBUG("Set cublas stream {:d}", cuda_stream_id);
         cublasSetStream(handle, device.getStream(cuda_stream_id));
-    }
-
-    // for cublas_hgemmBatched, pre-compute the GPU array pointers.
-    if (_batched) {
-        // GPU-memory arrays of pointers to the input & output
-        // matrices (in GPU memory)
-        __half** in;
-        __half** out;
-        __half** ph;
-        in = (__half**)device.get_gpu_memory(unique_name + "_batch_in",
-                                             _gpu_buffer_depth * _num_local_freq * sizeof(__half*));
-        out = (__half**)device.get_gpu_memory(
-            unique_name + "_batch_out", _gpu_buffer_depth * _num_local_freq * sizeof(__half*));
-        ph = (__half**)device.get_gpu_memory(unique_name + "_batch_ph",
-                                             _num_local_freq * sizeof(__half*));
-
-        // Temporary CPU-memory arrays of pointers to the input &
-        // output matrices (which live in GPU memory); these will get
-        // copied to in/out/ph.
-        __half* host_in[_gpu_buffer_depth * _num_local_freq];
-        __half* host_out[_gpu_buffer_depth * _num_local_freq];
-        __half* host_ph[_gpu_buffer_depth * _num_local_freq];
-
-        // Final CPU-side pointers to GPU memory
-        _gpu_in_pointers.resize(_gpu_buffer_depth);
-        _gpu_out_pointers.resize(_gpu_buffer_depth);
-
-        int freqs_per_stream = _num_local_freq / nstreams;
-
-        // loop over gpu frames
-        for (int gpu_frame_id = 0; gpu_frame_id < _gpu_buffer_depth; gpu_frame_id++) {
-            // GPU input & output memory buffers for this gpu frame #.
-            float16_t* gpu_in_base = (float16_t*)device.get_gpu_memory_array(
-                _gpu_mem_beamgrid, gpu_frame_id, beamgrid_len);
-            float16_t* gpu_out_base = (float16_t*)device.get_gpu_memory_array(
-                _gpu_mem_beamout, gpu_frame_id, beamout_len);
-            // Compute the per-frequency matrix offsets.
-            for (int f = 0; f < _num_local_freq; f++) {
-                host_in[gpu_frame_id * _num_local_freq + f] = gpu_in_base + (size_t)f * _Td * rho;
-                host_out[gpu_frame_id * _num_local_freq + f] =
-                    gpu_out_base + (size_t)f * _Td * _num_beams;
-                if (gpu_frame_id == 0)
-                    host_ph[f] = phase_memory + (size_t)f * rho * _num_beams;
-            }
-            // loop over streams and save the final GPU memory pointers (which we haven't yet filled
-            // with data!)
-            for (int i = 0; i < nstreams; i++) {
-                _gpu_in_pointers[gpu_frame_id].push_back(
-                    in + (gpu_frame_id * nstreams + i) * freqs_per_stream);
-                _gpu_out_pointers[gpu_frame_id].push_back(
-                    out + (gpu_frame_id * nstreams + i) * freqs_per_stream);
-                if (gpu_frame_id == 0)
-                    _gpu_phase_pointers.push_back(ph + i * freqs_per_stream);
-            }
-        }
-        // Now copy the GPU pointer offsets (that we computed on the CPU) over to the GPU!
-        CHECK_CUDA_ERROR(cudaMemcpy(in, host_in,
-                                    _gpu_buffer_depth * _num_local_freq * sizeof(__half*),
-                                    cudaMemcpyHostToDevice));
-        CHECK_CUDA_ERROR(cudaMemcpy(out, host_out,
-                                    _gpu_buffer_depth * _num_local_freq * sizeof(__half*),
-                                    cudaMemcpyHostToDevice));
-        CHECK_CUDA_ERROR(
-            cudaMemcpy(ph, host_ph, _num_local_freq * sizeof(__half*), cudaMemcpyHostToDevice));
     }
 }
 
