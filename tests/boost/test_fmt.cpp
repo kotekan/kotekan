cb2baad31b84eb89b8fcea535a0dbbf2fc39a9b3
Dustin Lang <dstndstn@gmail.com>
2024-02-10 15:27:07 +0000

    tidy

--- tests/boost/test_fmt.cpp
+++ tests/boost/test_fmt.cpp
@@ -5,13 +5,17 @@
 #include "fmt.hpp" // for format
 #include "kotekanLogging.hpp"    // for DEBUG, INFO, ERROR, FATAL_ERROR, WARN
 #include "errors.h"
+#include <thread>
 
 //BOOST_AUTO_TEST_CASE(test1) {
 int main() {
     _global_log_level = 4;
     __enable_syslog = 0;
     
+    using namespace std::chrono_literals;
+
     std::chrono::time_point<std::chrono::steady_clock> period_start = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(2000ms);
     const auto now = std::chrono::steady_clock::now();
     const std::chrono::duration<double> diff = now - period_start;
     INFO_NON_OO("duration {}", diff);
