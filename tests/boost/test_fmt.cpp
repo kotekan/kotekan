5c299ad1a3f93e31705bb7e7cd3f463e7dfb6b82
Dustin Lang <dstndstn@gmail.com>
2024-02-10 17:06:16 +0000

    omg I am sick of clang format

--- tests/boost/test_fmt.cpp
+++ tests/boost/test_fmt.cpp
@@ -1,20 +1,23 @@
 //#define BOOST_TEST_MODULE "test_fmt"
 //#include <boost/test/included/unit_test.hpp>
 
-#include <chrono>     // for duration, operator-, seconds, operator/, operator>, tim...
-#include "fmt.hpp" // for format
-#include "kotekanLogging.hpp"    // for DEBUG, INFO, ERROR, FATAL_ERROR, WARN
 #include "errors.h"
+#include "kotekanLogging.hpp" // for DEBUG, INFO, ERROR, FATAL_ERROR, WARN
+
+#include "fmt.hpp" // for format
+
+#include <chrono> // for duration, operator-, seconds, operator/, operator>, tim...
 #include <thread>
 
-//BOOST_AUTO_TEST_CASE(test1) {
+// BOOST_AUTO_TEST_CASE(test1) {
 int main() {
     _global_log_level = 4;
     __enable_syslog = 0;
-    
+
     using namespace std::chrono_literals;
 
-    std::chrono::time_point<std::chrono::steady_clock> period_start = std::chrono::steady_clock::now();
+    std::chrono::time_point<std::chrono::steady_clock> period_start =
+        std::chrono::steady_clock::now();
     std::this_thread::sleep_for(2000ms);
     const auto now = std::chrono::steady_clock::now();
     const std::chrono::duration<double> diff = now - period_start;
