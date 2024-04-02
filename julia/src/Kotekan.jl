a1a310b95588ce2a8d1902a76f58e7dbb2c17d8a
Erik Schnetter <schnetter@gmail.com>
2024-04-02 16:23:43 -0400

    Beautify Julia code

--- julia/src/Kotekan.jl
+++ julia/src/Kotekan.jl
@@ -1,5 +1,4 @@
 # This is a placeholder - some Julia tools want the "src" directory to exist and want
 # a module with the same name as the package.  In the future we might actually build
 # out this package to interface the Kotekan code to Julia.
-module Kotekan
-end
+module Kotekan end
