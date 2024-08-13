a4d53962e5c2a580352e9ea868ae6f3a3f47aa75
James Mertens <jbcm627@gmail.com>
2024-08-13 20:01:09 +0000

    find lz4 lib

--- cmake/Findasdf-cxx.cmake
+++ cmake/Findasdf-cxx.cmake
@@ -1,6 +1,6 @@
 find_package(PkgConfig REQUIRED)
 
-pkg_check_modules(ASDF_CXX QUIET asdf-cxx)
+pkg_check_modules(ASDF_CXX asdf-cxx)
 
 message(STATUS "ASDF_CXX_INCLUDE_DIRS: ${ASDF_CXX_INCLUDE_DIRS}")
 message(STATUS "ASDF_CXX_LIBRARY_DIRS: ${ASDF_CXX_LIBRARY_DIRS}")
