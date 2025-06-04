66f2d002d78d6bbeec832677efe6e7b29455dbc7
Dustin Lang <dstn@cloud2.arbutus>
2025-06-04 17:31:37 +0000

    fix compiler warning on static complex float16 construction

--- lib/stages/FEngine.cpp
+++ lib/stages/FEngine.cpp
@@ -908,7 +908,7 @@ void FEngine::main_thread() {
                 for (int n = 0; n < num_dish_locations_ns * num_dish_locations_ew
                                         * num_polarizations * num_local_channels * U;
                      ++n)
-                    ((std::complex<float16_t>*)W1_frame)[n] = 1.;
+                    ((std::complex<float16_t>*)W1_frame)[n] = std::complex<float16_t>(float16_t(1.), float16_t(0.));
             }
             DEBUG("[{:d}] Done filling W1 buffer for U={:d}.", W1_frame_index, U);
 
