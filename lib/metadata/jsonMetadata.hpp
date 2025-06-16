6acf4c7b25236297294d38d9510c8d7fe0e16660
Roland Haas <rhaas@mail.ubc.ca>
2025-06-16 13:29:59 -0700

    jsonMetadata: JSON based science metadata

---
+++ lib/metadata/jsonMetadata.hpp
@@ -0,0 +1,124 @@
+#ifndef JSON_METADATA
+#define JSON_METADATA
+
+
+#include "json.hpp"
+
+#include <string>
+#include <vector>
+
+#pragma pack()
+
+// json based metadata structures to hold "physics" metadata, ie. data that
+// various at most once per frame
+
+namespace jsonMetadata {
+// these must match chimeMetadata and choordMetadata for now
+// in namespace for C++ const
+#define MAX_NUM_BEAMS 20
+const int CHORD_META_MAX_FREQ = 1024;
+
+typedef nlohmann::json metadata;
+
+// known types of metadata, use name variables instead of bare strings to have
+// the compiler catch (some) typos
+
+const std::string BEAM_COORD("BEAM_COORD");            // a struct beamCoord
+const std::string RIGHT_ASCENSION("RIGHT_ASCENSION");  // an array of float of size MAX_NUM_BEAMS
+const std::string DECLINATION("DECLINATION");          // an array of float of size MAX_NUM_BEAMS
+const std::string SCALING("SCALING");                  // an array of uint32 of size MAX_NUM_BEAMS
+const std::string FPGA_SEQ_NUM("FPGA_SEQ_NUM");        // an uint64
+const std::string NFREQ("NFREQ");                      // an int
+const std::string COARSE_FREQ("COARSE_FREQ");          // an array of int of size CHORD_META_MAX_FREQ
+
+struct beamCoord {
+    float right_ascension[MAX_NUM_BEAMS];
+    float declination[MAX_NUM_BEAMS];
+    uint32_t scaling[MAX_NUM_BEAMS];
+
+    beamCoord() {
+#ifdef DEBUG
+        for(auto& ra : right_ascension)
+            ra = std::nanf("");
+        for(auto& dec : declination)
+            dec = std::nanf("");
+        for(auto& scale : scaling)
+            scale = 0;
+#endif
+    }
+
+    // TODO: turn into a from_json function?
+    beamCoord(const metadata& md) {
+        {
+        const auto& ra(md[RIGHT_ASCENSION]);
+        if(ra.size() > MAX_NUM_BEAMS)
+                throw std::runtime_error("Number of beams request exceeds MAX_NUM_BEAMS");
+        size_t i = 0;
+        for(auto it = ra.cbegin() ; it != ra.cend() ; ++it) {
+            it->get_to(right_ascension[i++]);
+        }
+#ifdef DEBUG
+        for(size_t i = ra.size() ; i < MAX_NUM_BEAMS ; ++i) {
+            right_ascension[i] = std::nanf("");
+        }
+#endif
+        }
+
+        {
+        const auto& dec(md[DECLINATION]);
+        if(dec.size() > MAX_NUM_BEAMS)
+                throw std::runtime_error("Number of beams request exceeds MAX_NUM_BEAMS");
+        size_t i = 0;
+        for(auto it = dec.cbegin() ; it != dec.cend() ; ++it) {
+                it->get_to(declination[i]);
+        }
+#ifdef DEBUG
+        for(size_t i = dec.size() ; i < MAX_NUM_BEAMS ; ++i) {
+            declination[i] = std::nanf("");
+        }
+#endif
+        }
+
+        {
+        const auto& scale(md[SCALING]);
+        if(scale.size() > MAX_NUM_BEAMS)
+            throw std::runtime_error("Number of beams request exceeds MAX_NUM_BEAMS");
+        size_t i = 0;
+        for(auto it = scale.cbegin() ; it != scale.cend() ; ++it) {
+            it->get_to(scaling[i]);
+        }
+#ifdef DEBUG
+        for(size_t i = scale.size() ; i < MAX_NUM_BEAMS ; ++i) {
+            scaling[i] = 0;
+        }
+#endif
+        }
+    }
+};
+
+struct coarseFreq {
+        int nfreq;
+        int coarse_freq[CHORD_META_MAX_FREQ];
+
+        // TODO: turn into a from_json function?
+        coarseFreq(const metadata& md) {
+        md[NFREQ].get_to(nfreq);
+        const auto& freq(md[COARSE_FREQ]);
+        if(freq.size() > MAX_NUM_BEAMS)
+            throw std::runtime_error("Number of frequencies requested exceeds CHORD_META_MAX_FREQ");
+        size_t i = 0;
+        for(auto it = freq.cbegin() ; it != freq.cend() ; ++it) {
+            it->get_to(coarse_freq[i]);
+        }
+#ifdef DEBUG
+        for(size_t i = freq.size() ; i < CHORD_META_MAX_FREQ ; ++i) {
+            coarse_freq[i] = 0;
+        }
+#endif
+        }
+};
+
+
+}
+
+#endif
