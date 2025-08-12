8d222347594f35403d0a33bb3ae20590b2dbdf3b
Roland Haas <rhaas@mail.ubc.ca>
2025-08-11 21:01:13 -0700

    pyPlotN2: remove use of chimeMetadata data

--- lib/stages/pyPlotN2.hpp
+++ lib/stages/pyPlotN2.hpp
@@ -8,7 +8,6 @@
 #define PY_PLOT_N2_H
 
 #include "Config.hpp"          // for Config
-#include "ICETelescope.hpp"    // for ice_stream_id_t
 #include "Stage.hpp"           // for Stage
 #include "buffer.hpp"          // for Buffer
 #include "bufferContainer.hpp" // for bufferContainer
@@ -78,7 +77,6 @@ private:
     int gpu_id = -1;
     bool dump_plot = false;
     bool busy = false;
-    ice_stream_id_t stream_id;
 };
 
 #endif
