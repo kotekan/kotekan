9ada16a53f53bf83d590d9bcc089aa3f2971e529
Erik Schnetter <schnetter@gmail.com>
2025-09-03 15:18:06 -0400

    Remove leftover debugging code

--- lib/gpu/gpuCommand.hpp
+++ lib/gpu/gpuCommand.hpp
@@ -188,10 +188,6 @@ protected:
     /// State that is shared by instances of this command (at one point in the pipeline)
     std::shared_ptr<gpuCommandState> command_state;
 
-    /// Ensure serial execution of calls to the same instance
-    std::mutex execute_mutex;
-    std::unique_lock<std::mutex> execute_lock;
-
     /// The counter for the GPU frame we are currently processing.
     int64_t gpu_frame_id;
 
