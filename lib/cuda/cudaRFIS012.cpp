69efb1b622a9ad8370a73e513e4b5dbdd847c065
Roland Haas <rhaas@mail.ubc.ca>
2025-10-02 16:52:34 -0700

    cudaRFIS012: adjust to chordMetadata::freq_upchan_factor

--- lib/cuda/cudaRFIS012.cpp
+++ lib/cuda/cudaRFIS012.cpp
@@ -189,11 +189,17 @@ cudaEvent_t cudaRFIS012::execute(cudaPipelineState& /*pipestate*/,
 
     rfi_S012.set_metadata(voltage.get_metadata());
     const auto& rfi_S012_meta = rfi_S012.get_metadata();
+    auto freq_upchan_factor = rfi_S012_meta->get_freq_upchan_factor();
+    auto time_downsampling_fpga = rfi_S012_meta->get_time_downsampling_fpga();
+    assert(freq_upchan_factor.size() == static_cast<size_t>(rfi_S012_meta->get_nfreq()));
+    assert(time_downsampling_fpga.size() == static_cast<size_t>(rfi_S012_meta->get_nfreq()));
     assert(rfi_S012_meta->get_nfreq() >= 0);
     for (int freq = 0; freq < rfi_S012_meta->get_nfreq(); ++freq) {
-        rfi_S012_meta->freq_upchan_factor[freq] *= rfi_downsampling_factor;
-        rfi_S012_meta->time_downsampling_fpga[freq] *= rfi_downsampling_factor;
+        freq_upchan_factor[freq] *= rfi_downsampling_factor;
+        time_downsampling_fpga[freq] *= rfi_downsampling_factor;
     }
+    rfi_S012_meta->set_freq_upchan_factor(freq_upchan_factor);
+    rfi_S012_meta->set_time_downsampling_fpga(time_downsampling_fpga);
 
     // There is no poison value
     // rfi_S012.set_to_poison(0xff);
