8ecea2d91dda6d6a39821ed67a4e161e49d1664c
Roland Haas <rhaas@mail.ubc.ca>
2025-10-23 18:36:20 +0000

    chordMetadata: add to / from json and deepCopy functions

--- lib/metadata/chordMetadata.cpp
+++ lib/metadata/chordMetadata.cpp
@@ -45,6 +45,11 @@ void chordMetadata::set_from_frame_desc(
     }
 }
 
+void chordMetadata::deepCopy(std::shared_ptr<metadataObject> other) {
+    auto chord_other = std::dynamic_pointer_cast<chordMetadata>(other);
+    assert(chord_other);
+    *this = *chord_other;
+}
 
 struct chordMetadataFormat {
     int32_t max_dim;
