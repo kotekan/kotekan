868e2ad25b5a2cd19187abe885f03c6cc42ad826
Roland Haas <rhaas@mail.ubc.ca>
2025-10-23 18:35:50 +0000

    N2Metadata: add to / from json functions

--- lib/metadata/N2Metadata.cpp
+++ lib/metadata/N2Metadata.cpp
@@ -65,3 +65,45 @@ size_t N2Metadata::serialize(char* bytes) {
 
     return sizeof(N2MetadataFormat);
 }
+
+nlohmann::json N2Metadata::to_json() {
+    nlohmann::json rtn = {};
+    ::to_json(rtn, *this);
+    return rtn;
+}
+
+void to_json(nlohmann::json& j, const N2Metadata& m) {
+    j["fpga_start_tick"] = m.fpga_start_tick;
+    j["frame_start_time_ns"] = m.frame_start_time_ns;
+    j["frame_length_fpga_ticks"] = m.frame_length_fpga_ticks;
+
+    j["n_valid_fpga_ticks"] = m.n_valid_fpga_ticks;
+    j["n_rfi_fpga_ticks"] = m.n_rfi_fpga_ticks;
+
+    j["freq_id"] = m.freq_id; // this is an int in chordMetadata, maybe change later
+    j["freq_Hz"] = m.freq_Hz;
+    j["eop"] = m.eop;
+
+    j["num_elements"] = m.num_elements;
+    j["num_prod"] = m.num_prod;
+    j["num_ev"] = m.num_ev;
+    j["nfreq"] = m.nfreq;
+}
+
+void from_json(const nlohmann::json& j, N2Metadata& m) {
+    m.fpga_start_tick = j["fpga_start_tick"];
+    m.frame_start_time_ns = j["frame_start_time_ns"];
+    m.frame_length_fpga_ticks = j["frame_length_fpga_ticks"];
+
+    m.n_valid_fpga_ticks = j["n_valid_fpga_ticks"];
+    m.n_rfi_fpga_ticks = j["n_rfi_fpga_ticks"];
+
+    m.freq_id = j["freq_id"]; // this is an int in chordMetadata, maybe change later
+    m.freq_Hz = j["freq_Hz"];
+    m.eop = j["eop"];
+
+    m.num_elements = j["num_elements"];
+    m.num_prod = j["num_prod"];
+    m.num_ev = j["num_ev"];
+    m.nfreq = j["nfreq"];
+}
