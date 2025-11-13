e6c3910cd1f726f67854861270eae0c4ab8aa1ff
Roland Haas <rhaas@mail.ubc.ca>
2025-11-13 14:57:26 -0800

    bufferRecv: adjust to one frame_desc per buffer

--- lib/stages/bufferRecv.cpp
+++ lib/stages/bufferRecv.cpp
@@ -572,10 +572,10 @@ void connInstance::internal_read_callback() {
                                         strnlen(chord->dim_name[d], sizeof(chord->dim_name[d])));
                     }
 
-                    buf->allocate_new_frame_desc(frame_id, chord->type, chord->get_name(),
-                                                 dimensions, dimnames);
+                    buf->allocate_new_frame_desc(chord->type, chord->get_name(), dimensions,
+                                                 dimnames);
                     /* test that things are consistent */
-                    chord->check_frame_desc(buf->get_frame_desc(frame_id));
+                    chord->check_frame_desc(buf->get_frame_desc());
                 }
 
                 buf->mark_frame_full(producer_name, frame_id);
