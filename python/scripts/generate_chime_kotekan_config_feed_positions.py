f97998b79bb057668de62ced820273c3f5a5fd27
Aaron B. Pearlman <aaron.b.pearlman@gmail.com>
2025-11-27 12:15:09 -0500

    [VLBI Real-time Tracking Beamformer] Update kotekan config to use additional input (chan_id=32) (#1326)
    Summary of Changes:
    
    *  On Oct. 27, LayoutDB was updated so that CHIME input (chan_id=32) is no longer a non-sky channel (i.e., NoiseSource object) and now accurately reflects that it is a usable sky channel (i.e., CHIMEAntenna object).
    *  To enable this input to be used in the beamformer, the corresponding feed position in the config has been changed to the value from LayoutDB, as queried through ch_util (i.e., it is no longer [0.0, 0.0], which would indicate an unused feed in the beamformer).
    *  Comments have been added in the config files to make it more clear which input each of the feed positions corresponds to.

---
+++ python/scripts/generate_chime_kotekan_config_feed_positions.py
@@ -0,0 +1,86 @@
+"""
+generate_chime_kotekan_config_feed_positions.py
+
+Author: Aaron B. Pearlman (aaron.b.pearlman@physics.mcgill.ca)
+Date: 2025.10.29
+
+Helper script that queries the Layout Database (LayoutDB) for CHIME and generates the feed_positions section for the
+CHIME kotekan config file. This script must be run in an environment that has ch_util and chimedb installed.
+The environment must be able to access the LayoutDB through the CHIME database.
+
+The output of this script can be copied and pasted at the end of the CHIME kotekan config files (e.g.,
+chime_science_run_gpu.yaml and chime_science_run_gpu_no_output.yaml).
+
+Usage: python generate_chime_kotekan_config_feed_positions.py
+
+"""
+
+import numpy as np
+import datetime
+
+import ch_util
+from ch_util import ephemeris, tools
+
+import chimedb.core
+
+
+print("feed_positions:")
+
+# Get the latest correlator inputs from the Layout DB.
+chime_inputs = ch_util.tools.get_correlator_inputs(datetime.datetime.now(), correlator="chime")
+
+# The feed positions in the kotekan config file correspond to chan_ids [0-255], [512-767], [1024-1279], [1536-1791].
+# For a map, see: https://bao.chimenet.ca/doc/documents/538/2/get/chime_map_cyl_based.pdf?n=1942
+cyl1_chan_id = np.arange(0, 256, 1)
+cyl2_chan_id = np.arange(512, 768, 1)
+cyl3_chan_id = np.arange(1024, 1280, 1)
+cyl4_chan_id = np.arange(1536, 1792, 1)
+cyl_chan_id = np.concatenate((cyl1_chan_id, cyl2_chan_id, cyl3_chan_id, cyl4_chan_id))
+
+for i in cyl_chan_id:
+    
+    antenna_type = chime_inputs[i].__class__.__name__
+    id = chime_inputs[i].id
+    crate = chime_inputs[i].crate
+    slot = chime_inputs[i].slot
+    sma = chime_inputs[i].sma
+    corr_order = chime_inputs[i].corr_order
+    delay = chime_inputs[i].delay
+    input_sn = chime_inputs[i].input_sn
+    corr = chime_inputs[i].corr
+    
+    pos_x = 0.0
+    pos_y = 0.0
+
+    if ((antenna_type != "NoiseSource") and (antenna_type != "Blank") and (antenna_type != "HolographyAntenna")):
+        pos_x = chime_inputs[i].pos[0]
+        pos_y = chime_inputs[i].pos[1]
+    
+    antenna_str = f"# {antenna_type}(id={id}, crate={crate}, slot={slot}, sma={sma}, corr_order={corr_order}, delay={delay}, input_sn='{input_sn}', corr='{corr}')"
+
+    if (i == 0):
+        prefix = f"  [[{pos_x:.20f}, {pos_y:.20f}], "
+        
+        if ((pos_x == 0.0) and (pos_y == 0.0)):
+            prefix = f"  [[0.0, 0.0], "
+
+        pad = max(0, 58 - len(prefix))
+        print(prefix + " " * pad + antenna_str)
+
+    elif (i == 1791):
+        prefix = f"   [{pos_x:.20f}, {pos_y:.20f}]] "
+
+        if ((pos_x == 0.0) and (pos_y == 0.0)):
+            prefix = f"   [0.0, 0.0]] "
+
+        pad = max(0, 58 - len(prefix))
+        print(prefix + " " * pad + antenna_str)
+
+    else:
+        prefix = f"   [{pos_x:.20f}, {pos_y:.20f}], "
+
+        if ((pos_x == 0.0) and (pos_y == 0.0)):
+            prefix = f"   [0.0, 0.0], "
+
+        pad = max(0, 58 - len(prefix))
+        print(prefix + " " * pad + antenna_str)
