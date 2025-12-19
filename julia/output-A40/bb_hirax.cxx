908df04facb80798a44f2409c7f82c882911bd5a
Erik Schnetter <schnetter@gmail.com>
2025-12-19 12:32:12 -0500

    Regenerate kernels

--- julia/output-A40/bb_hirax.cxx
+++ julia/output-A40/bb_hirax.cxx
@@ -467,11 +467,9 @@ cudaEvent_t cudaBasebandBeamformer_hirax::execute(cudaPipelineState& /*pipestate
     {
         const std::shared_ptr<chordMetadata> J_meta = J_buffer.get_metadata();
 
-        // Since we do not use a ring buffer we need to set `meta->fpga_seq_num` and
-        // `meta->sample0_offset`
+        // Since we do not use a ring buffer we need to set `meta->fpga_seq_num`
         J_meta->set_fpga_seq_num(T_min);
-        J_meta->set_sample0_offset(T_min);
-        assert(J_meta->get_offset_downsampling() == 1);
+        assert(J_meta->get_time_downsampling_fpga() == 1);
     }
 
     // Copy inputs to device memory
