790e9e1757fb6afe5c7b8442f7195188bf19fa8a
Erik Schnetter <schnetter@gmail.com>
2026-02-14 11:29:30 -0500

    Clean up implementation of FRB2 beamformer

--- lib/cuda/cudaFRBBeamReformer.cpp
+++ lib/cuda/cudaFRBBeamReformer.cpp
@@ -1,76 +1,119 @@
-#include "cudaFRBBeamReformer.hpp"
+/**
+ * @file
+ * @brief CUDA FRB beamforming kernel, final stage
+ *  - cudaFRBBeamReformer : public cudaCommand
+ */
 
-#include "DataType.hpp"            // for float16_t, DataType
-#include "buffer.hpp"              // for GenericBuffer
-#include "cudaCommand.hpp"         // for cudaCommand, REGISTER_CUDA_COMMAND, _factory_aliascud...
-#include "cudaDeviceInterface.hpp" // for cudaDeviceInterface
-#include "div.hpp"                 // for div_noremainder, mod
-#include "driver_types.h"          // for CUevent_st, cudaEvent_t, CUstream_st
-#include "gpuCommand.hpp"          // for gpuCommandType
-#include "kotekanLogging.hpp"      // for DEBUG, ERROR
-#include "metadata.hpp"            // for metadataObject
-#include "ringbuffer.hpp"          // for RingBuffer
+#include "Config.hpp" // for Config
+#include "NDArrayBuffer.hpp"
+#include "NDArrayRingBuffer.hpp"
+#include "cudaCommand.hpp" // for cudaCommand, REGISTER_CUDA_COMMAND
+#include "div.hpp"
 
-#include <algorithm>         // for max
-#include <cassert>           // for assert
-#include <chordMetadata.hpp> // for chordMetadata, get_chord_metadata, metadata_is_chord
-#include <cstdlib>           // for abort
-#include <cublas_v2.h>       // for cublasCreate, cublasDestroy, cublasSetStream
-#include <fmt.hpp>           // for compile_string_to_view
-#include <memory>            // for shared_ptr, __shared_ptr_access
-#include <optional>          // for optional
-#include <tuple>             // for tuple, make_tuple
-#include <vector>            // for vector
+#include <array>
+#include <cstdlib>
+#include <cublas_api.h>   // for cublasContext, cublasHandle_t
+#include <cublas_v2.h>    // for cublasCreate, cublasDestroy, cublasSetStream
+#include <driver_types.h> // for cudaEvent_t
+#include <string>
+#include <vector>
 
-using kotekan::bufferContainer;
-using kotekan::Config;
-using kotekan::div_noremainder;
 using kotekan::mod;
+
+/**
+ * @class cudaFRBBeamReformer
+ * @brief cudaCommand for doing final FRB beamforming.  This follows the cudaFRBBeamFormer.
+ *
+ * Core code was developed by Nada El-Falou in https://github.com/nadafalou/CHORD/blob/main/mmul.cu
+ *
+ * The phase matrix for the beam locations uses the correct math but
+ * with a lot of placeholder assumptions.  This will need to get
+ * revisited in post-MVP development.
+ */
+class cudaFRBBeamReformer : public cudaCommand {
+public:
+    cudaFRBBeamReformer(kotekan::Config& config, const std::string& unique_name,
+                        kotekan::bufferContainer& host_buffers, cudaDeviceInterface& device,
+                        int inst);
+    ~cudaFRBBeamReformer();
+    int wait_on_precondition() override;
+    cudaEvent_t execute(cudaPipelineState& pipestate,
+                        const std::vector<cudaEvent_t>& pre_events) override;
+    void finalize_frame() override;
+
+private:
+    const bool poison_buffers;
+
+    const int frb1_max_num_times;
+    const int frb1_max_num_frequencies;
+    const int frb1_num_beams_P;
+    const int frb1_num_beams_Q;
+
+    // Number of time samples per frb2 beam packet
+    const int frb2_num_frequencies;
+    const int frb2_num_beams;
+    const int frb2_num_times;
+
+    // Kotekan buffer names
+    const std::string frb2_phase_name;
+    const std::string frb1_beams_name;
+    const std::string frb2_beams_name;
+
+    // Buffers
+    NDArrayBuffer<float16_t, 4> frb2_phase_buffer;
+    NDArrayRingBuffer<float16_t, 4> frb1_beams_buffer;
+    NDArrayBuffer<float16_t, 3> frb2_beams_buffer;
+
+    cublasHandle_t handle;
+
+    bool did_set_metadata;
+};
 
 REGISTER_CUDA_COMMAND(cudaFRBBeamReformer);
 
-cudaFRBBeamReformer::cudaFRBBeamReformer(Config& config, const std::string& unique_name,
-                                         bufferContainer& host_buffers, cudaDeviceInterface& device,
-                                         int inst) :
-    cudaCommand(config, unique_name, host_buffers, device, inst) {
+cudaFRBBeamReformer::cudaFRBBeamReformer(kotekan::Config& config, const std::string& unique_name,
+                                         kotekan::bufferContainer& host_buffers,
+                                         cudaDeviceInterface& device, const int instance_num) :
+    cudaCommand(config, unique_name, host_buffers, device, instance_num, no_cuda_command_state,
+                "cudaFRBBeamReformer"),
 
-    // Number of output beams
-    _num_beams = config.get<int>(unique_name, "num_beams");
-    // Number of input beams
-    _beam_grid_size_P = config.get<int>(unique_name, "beam_grid_size_P");
-    _beam_grid_size_Q = config.get<int>(unique_name, "beam_grid_size_Q");
-    num_input_beams = _beam_grid_size_P * _beam_grid_size_Q;
-    // Number of frequencies
-    _max_num_local_freq = config.get<int>(unique_name, "max_num_local_freq");
-    _num_local_freq = config.get<int>(unique_name, "num_local_freq");
-    assert(_num_local_freq <= _max_num_local_freq);
-    // Number of time samples
-    _Td = config.get<int>(unique_name, "samples_per_data_set");
+    poison_buffers(config.get_default<bool>(unique_name, "poison_buffers", false)),
 
-    // Input and output buffer names
-    _gpu_mem_beamgrid = config.get<std::string>(unique_name, "gpu_mem_beamgrid");
-    _gpu_mem_phase = config.get<std::string>(unique_name, "gpu_mem_phase");
-    _gpu_mem_beamout = config.get<std::string>(unique_name, "gpu_mem_beamout");
+    frb1_max_num_times(config.get<int>(unique_name, "frb1_max_num_times")),
+    frb1_max_num_frequencies(config.get<int>(unique_name, "frb1_max_num_frequencies")),
+    frb1_num_beams_P(config.get<int>(unique_name, "frb1_num_beams_P")),
+    frb1_num_beams_Q(config.get<int>(unique_name, "frb1_num_beams_Q")),
 
-    // Calculate buffer sizes (in bytes)
-    beamgrid_len = sizeof(float16_t) * num_input_beams * _max_num_local_freq * _Td;
-    phase_len = sizeof(float16_t) * num_input_beams * _num_beams * _num_local_freq;
-    beamout_len = sizeof(float16_t) * _num_beams * _num_local_freq * _Td;
+    frb2_num_frequencies(config.get<int>(unique_name, "frb2_num_frequencies")),
+    frb2_num_beams(config.get<int>(unique_name, "frb2_num_beams")),
+    frb2_num_times(config.get<int>(unique_name, "frb2_num_times")),
 
-    // Find input buffer used for signalling ring-buffer state
-    input_ringbuf_signal = dynamic_cast<RingBuffer*>(
-        host_buffers.get_generic_buffer(config.get<std::string>(unique_name, "in_signal")));
-    if (inst == 0)
-        input_ringbuf_signal->register_consumer(unique_name);
+    frb2_phase_name(config.get<std::string>(unique_name, "frb2_phase_name")),
+    frb1_beams_name(config.get<std::string>(unique_name, "frb1_beams_name")),
+    frb2_beams_name(config.get<std::string>(unique_name, "frb2_beams_name")),
 
-    // Add Graphviz entries for the GPU buffers used by this kernel
-    gpu_buffers_used.push_back(std::make_tuple(_gpu_mem_beamgrid, true, true, false));
-    gpu_buffers_used.push_back(std::make_tuple(_gpu_mem_phase, false, true, true));
-    gpu_buffers_used.push_back(std::make_tuple(_gpu_mem_beamout, true, false, true));
+    frb2_phase_buffer(frb2_phase_name, "W2",
+                      std::array<std::ptrdiff_t, 4>{frb1_num_beams_Q, frb1_num_beams_P,
+                                                    frb2_num_frequencies, frb2_num_beams},
+                      std::array<std::string, 4>{"beamQ", "beamP", "F", "R"}, *this,
+                      buffer_type_t::do_once),
+    frb1_beams_buffer(frb1_beams_name, "I",
+                      std::array<std::ptrdiff_t, 4>{frb1_max_num_times, frb1_max_num_frequencies,
+                                                    frb1_num_beams_Q, frb1_num_beams_P},
+                      std::array<std::string, 4>{"T", "F", "beamQ", "beamP"}, *this),
+    frb2_beams_buffer(
+        frb2_beams_name, "I2",
+        std::array<std::ptrdiff_t, 3>{frb2_num_beams, frb2_num_frequencies, frb2_num_times},
+        std::array<std::string, 3>{"R", "F", "T"}, *this),
 
-    // Kotekan stuff
+    did_set_metadata(false)
+
+{
+    frb2_phase_buffer.register_consumer();
+    frb1_beams_buffer.register_consumer();
+    frb2_beams_buffer.register_producer();
+
     set_command_type(gpuCommandType::KERNEL);
-    set_name("FRB_beamreformer");
 
     // Create cuBLAS handle
     cublasStatus_t ierr = cublasCreate(&handle);
@@ -91,49 +134,59 @@ cudaFRBBeamReformer::~cudaFRBBeamReformer() {
     cublasDestroy(handle);
 }
 
+
 int cudaFRBBeamReformer::wait_on_precondition() {
+    {
+        const int errcode = cudaCommand::wait_on_precondition();
+        if (errcode < 0)
+            return errcode;
+    }
+
     // Wait for data to be available in input ringbuffer
-    const std::ptrdiff_t input_bytes = beamgrid_len;
-    DEBUG("Input ring-buffer byte count: {:d}", input_bytes);
-    DEBUG("Waiting for input ringbuffer data for frame {:d}...", gpu_frame_id);
-    const std::optional<std::ptrdiff_t> val_in =
-        input_ringbuf_signal->wait_and_claim_readable(unique_name, instance_num, input_bytes);
-    DEBUG("Finished waiting for input for data frame {:d}.", gpu_frame_id);
-    if (!val_in.has_value())
-        return -1;
-    input_cursor = val_in.value();
-    DEBUG("Input ring-buffer byte offset: {:d}", input_cursor);
-    // Mod input cursor by the ringbuffer size
-    input_position = mod(input_cursor, input_ringbuf_signal->size);
-    // Assert that we don't wrap around!
-    assert(input_position + input_bytes <= input_ringbuf_signal->size);
-    DEBUG("Modded input ring-buffer byte offset: {:d}", input_position);
+    const std::ptrdiff_t T_ringbuf = frb2_num_times;
+    {
+        const int errcode =
+            frb1_beams_buffer.wait_and_claim_readable([&](const std::ptrdiff_t T_available) {
+                if (T_available < T_ringbuf)
+                    return read_descriptor_t{.claimed = 0, .read = 0};
+                return read_descriptor_t{.claimed = T_ringbuf, .read = T_ringbuf};
+            });
+        if (errcode < 0)
+            return errcode;
+    }
+
     return 0;
 }
 
-cudaEvent_t cudaFRBBeamReformer::execute(cudaPipelineState&, const std::vector<cudaEvent_t>&) {
+cudaEvent_t cudaFRBBeamReformer::execute(cudaPipelineState& /*pipestate*/,
+                                         const std::vector<cudaEvent_t>& /*pre_events*/) {
     pre_execute();
-
     record_start_event();
 
-    // Get buffer pointers
-    DEBUG("beamgrid_memory");
-    float16_t* const beamgrid_memory =
-        (float16_t*)device.get_gpu_memory(_gpu_mem_beamgrid + "_buffer", input_ringbuf_signal->size)
-        + div_noremainder(input_position, sizeof(float16_t));
-    DEBUG("phase_memory");
-    float16_t* const phase_memory =
-        (float16_t*)device.get_gpu_memory(_gpu_mem_phase + "_buffer", phase_len);
-    DEBUG("beamout_memory");
-    float16_t* const beamout_memory = (float16_t*)device.get_gpu_memory_array(
-        _gpu_mem_beamout + "_buffer", gpu_frame_id, _gpu_buffer_depth, beamout_len);
+    frb2_phase_buffer.check_metadata();
+    frb1_beams_buffer.check_metadata();
+    frb2_beams_buffer.check_metadata();
 
-    DEBUG("Running CUDA FRB BeamReformer on GPU frame {:d}: F={:d}, T={:d}, B={:d}, "
-          "num_input_beams={:d}",
-          gpu_frame_id, _num_local_freq, _Td, _num_beams, num_input_beams);
+    if (poison_buffers)
+        frb2_beams_buffer.set_to_poison(0xff); // 0xffff is a NaN16
+
+    // Get buffer pointers
+    const float16_t* const frb2_phase_memory = frb2_phase_buffer.get_ndarray().data();
+
+    const std::ptrdiff_t frb1_beams_offset = frb1_beams_buffer.get_read_valid().begin();
+    const std::ptrdiff_t frb1_beams_extent = frb1_beams_buffer.get_ndarray().get_extent(0);
+    const std::ptrdiff_t frb1_beams_stride = frb1_beams_buffer.get_ndarray().get_stride(0);
+    // Ensure there is no wrap-around
+    if (mod(frb1_beams_offset, frb1_beams_extent) + frb2_num_times > frb1_beams_stride)
+        std::abort();
+    const float16_t* const frb1_beams_memory =
+        frb1_beams_buffer.get_ndarray().data()
+        + frb1_beams_stride * mod(frb1_beams_offset, frb1_beams_extent);
+
+    float16_t* const frb2_beams_memory = frb2_beams_buffer.get_ndarray().data();
 
     // Calculate
-    //     Iout[T,F,Bout] = Iin[Bin,F0,T] * W[Bin,Bout,F]
+    //     Iout[T,F,Bout] = Iin[Bin,F0,T0] * W[Bin,Bout,F]
     //     C = A^T * B
     //
     //     C[m,n] = A^T[k,m] B[k,n]
@@ -157,84 +210,58 @@ cudaEvent_t cudaFRBBeamReformer::execute(cudaPipelineState&, const std::vector<c
     // matrices non-contiguous in memory. This is fine, cuBLAS
     // supports this.
 
-    const int m = _Td;
-    const int n = _num_beams;
-    const int k = num_input_beams;
+    // Matrix sizes (times and beams)
+    const int m = frb2_num_times;
+    const int n = frb2_num_beams;
+    const int k = frb1_num_beams_P * frb1_num_beams_Q;
 
-    const int lda = _max_num_local_freq * num_input_beams;
-    const int ldb = num_input_beams;
-    const int ldc = _Td * _num_local_freq;
+    // Matrix layouts (times and beams)
+    const int lda = frb1_beams_buffer.get_ndarray().get_stride(2);
+    const int ldb = frb2_phase_buffer.get_ndarray().get_stride(1);
+    const int ldc = frb2_beams_buffer.get_ndarray().get_stride(2);
 
-    const std::ptrdiff_t strideA = num_input_beams;
-    const std::ptrdiff_t strideB = num_input_beams;
-    const std::ptrdiff_t strideC = _Td;
+    // Batch strides (frequencies)
+    const std::ptrdiff_t strideA = frb1_beams_buffer.get_ndarray().get_stride(1);
+    const std::ptrdiff_t strideB = frb2_phase_buffer.get_ndarray().get_stride(2);
+    const std::ptrdiff_t strideC = frb2_beams_buffer.get_ndarray().get_stride(1);
 
     const float16_t alpha = 1;
     const float16_t beta = 0;
 
-    cublasStatus_t stat = cublasHgemmStridedBatched(
-        handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, k, &alpha, beamgrid_memory, lda, strideA,
-        phase_memory, ldb, strideB, &beta, beamout_memory, ldc, strideC, _num_local_freq);
+    cublasStatus_t stat =
+        cublasHgemmStridedBatched(handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, k, &alpha,
+                                  frb1_beams_memory, lda, strideA, frb2_phase_memory, ldb, strideB,
+                                  &beta, frb2_beams_memory, ldc, strideC, frb2_num_frequencies);
     if (stat != CUBLAS_STATUS_SUCCESS) {
         ERROR("Error at {:s}:{:d}: cublasHgemmStridedBatched: {:s}", __FILE__, __LINE__,
               cublasGetStatusString(stat));
         std::abort();
     }
 
-    const std::shared_ptr<metadataObject> in_mc = input_ringbuf_signal->get_metadata(0);
-    if (metadata_is_chord(in_mc)) {
-        const std::shared_ptr<chordMetadata> in_meta = get_chord_metadata(in_mc);
-        // Assert that input metadata array shape is as expected.
-        DEBUG("Input metadata: array shape {:s}, array type {:s}", in_meta->get_dimensions_string(),
-              in_meta->get_type_string());
-        assert(in_meta->type == kotekan::float16);
-        // Assert Ttilde x Fbar x beamQ x beamP
-        assert(in_meta->dims == 4);
-        if (!(in_meta->dim[1] == _max_num_local_freq))
-            ERROR("in dim=[{},{},{},{}] max_num_local_freq={}", in_meta->dim[0], in_meta->dim[1],
-                  in_meta->dim[2], in_meta->dim[3], _max_num_local_freq);
-        assert(in_meta->dim[1] == _max_num_local_freq);
-        assert(in_meta->dim[2] == _beam_grid_size_Q);
-        assert(in_meta->dim[3] == _beam_grid_size_P);
-        for (int d = in_meta->dims - 1; d >= 0; --d)
-            if (d == in_meta->dims - 1)
-                assert(in_meta->stride[d] == 1);
-            else
-                assert(in_meta->stride[d] == in_meta->stride[d + 1] * in_meta->dim[d + 1]);
-        // Set metadata on output buffer
-        std::shared_ptr<metadataObject> const out_mc = device.create_gpu_memory_array_metadata(
-            _gpu_mem_beamout + "_buffer", gpu_frame_id, in_mc->parent_pool);
-        std::shared_ptr<chordMetadata> const out_meta = get_chord_metadata(out_mc);
-        out_meta->deepCopy(in_meta);
-        // Output shape is (Ttilde x Fbar x beam) in float16
-        out_meta->set_name("frb2_beams");
-        out_meta->type = kotekan::float16;
-        out_meta->dims = 3;
-        out_meta->set_array_dimension(0, _num_beams, "R");
-        out_meta->set_array_dimension(1, _num_local_freq, "Fbar");
-        out_meta->set_array_dimension(2, _Td, "Ttilde");
-        for (int d = out_meta->dims - 1; d >= 0; --d)
-            if (d == out_meta->dims - 1)
-                out_meta->stride[d] = 1;
-            else
-                out_meta->stride[d] = out_meta->stride[d + 1] * out_meta->dim[d + 1];
-        DEBUG("Set output metadata: array shape {:s}, array type {:s}",
-              out_meta->get_dimensions_string(), out_meta->get_type_string());
+    // Update metadata
+    const std::shared_ptr<chordMetadata> frb2_beams_meta = frb2_beams_buffer.get_metadata();
+    // Since we do not use a ring buffer we need to set `meta->fpga_seq_num`
+    frb2_beams_meta->set_fpga_seq_num(frb1_beams_offset);
 
-        // Since we do not use a ring buffer we need to set `meta->fpga_seq_num`
-        assert(input_cursor % in_meta->sample_bytes() == 0);
-        out_meta->set_fpga_seq_num(in_meta->get_fpga_seq_num()
-                                   + in_meta->get_time_downsampling_fpga()
-                                         * div_noremainder(input_cursor, in_meta->sample_bytes()));
+    if (!did_set_metadata) {
+        did_set_metadata = true;
+        const std::shared_ptr<const chordMetadata> frb1_beams_meta =
+            frb1_beams_buffer.get_metadata();
+        frb2_beams_meta->set_time_downsampling_fpga(frb1_beams_meta->get_time_downsampling_fpga());
+        frb2_beams_meta->set_coarse_freq(frb1_beams_meta->get_coarse_freq());
+        frb2_beams_meta->set_freq_upchan_factor(frb1_beams_meta->get_freq_upchan_factor());
+        frb2_beams_meta->set_freq_upchan_index(frb1_beams_meta->get_freq_upchan_index());
     }
+
+    if (poison_buffers)
+        frb2_beams_buffer.check_for_poison(0xff); // 0xffff is a NaN16
 
     return record_end_event();
 }
 
 void cudaFRBBeamReformer::finalize_frame() {
     // Advance the input ringbuffer
-    const std::ptrdiff_t input_bytes = beamgrid_len;
-    DEBUG("Advancing input ringbuffer by {:d} bytes", input_bytes);
-    input_ringbuf_signal->finish_read(unique_name, instance_num, input_bytes);
+    frb1_beams_buffer.finish_read();
+
     cudaCommand::finalize_frame();
 }
