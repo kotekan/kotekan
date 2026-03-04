b3ae852ba71ef42897f0a9559126e6ff88127d2b
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2026-03-04 20:27:54 +0000

    cudaRFIS012 and cudaRFIS012tilde manually set both input and output offsets.

--- lib/cuda/cudaRFIS012.cpp
+++ lib/cuda/cudaRFIS012.cpp
@@ -210,24 +210,30 @@ cudaEvent_t cudaRFIS012::execute(cudaPipelineState& /*pipestate*/,
     const std::ptrdiff_t Tmin = voltage.get_read_valid().begin() % Tsize;
     assert(Tmin % 128 == 0);
     const std::ptrdiff_t T = voltage.get_read_valid().size();
+    assert(Tmin + T <= Tsize);
+    const std::ptrdiff_t T_offset = Tmin * voltage.get_ndarray().stride(0);
 
-    const int T_stride = rfi_S012.get_ndarray().stride(0);
-    const int F_stride = rfi_S012.get_ndarray().stride(1);
-    const int S_stride = rfi_S012.get_ndarray().stride(2);
+    const std::ptrdiff_t T_stride = rfi_S012.get_ndarray().stride(0);
+    const std::ptrdiff_t F_stride = rfi_S012.get_ndarray().stride(1);
+    const std::ptrdiff_t S_stride = rfi_S012.get_ndarray().stride(2);
     constexpr bool offset_encoded = true;
 
     // Current offset into the rfi_S012 buffer
     // n2k does not apply this itself
-    const int Trfi_offset = Tmin / rfi_downsampling_factor * T_stride;
+    const std::ptrdiff_t Trfi_offset = Tmin / rfi_downsampling_factor * T_stride;
+    
+    const auto& pl_mask_meta = pl_mask.get_metadata();
+    const std::ptrdiff_t Tpl_stride = pl_mask.get_ndarray().stride(0);
+    const std::ptrdiff_t Tpl_offset = Tmin / pl_mask_meta->get_time_downsampling_fpga() * Tpl_stride;;
 
-    n2k::launch_s0_kernel((ulong*)rfi_S012_memory + Trfi_offset, (const ulong*)pl_mask_memory, T,
-                          Tmin, Tsize, num_frequencies, num_dishes * num_polarizations,
+    n2k::launch_s0_kernel((ulong*)rfi_S012_memory + Trfi_offset, (const ulong*)(pl_mask_memory + Tpl_offset), T,
+                          0, Tsize, num_frequencies, num_dishes * num_polarizations,
                           rfi_downsampling_factor, F_stride, device.getStream(cuda_stream_id));
 #ifdef DEBUGGING
     CHECK_CUDA_ERROR(cudaStreamSynchronize(device.getStream(cuda_stream_id)));
 #endif
     n2k::launch_s12_kernel((ulong*)(rfi_S012_memory + S_stride + Trfi_offset),
-                           (const uint8_t*)voltage_memory, T, Tmin, Tsize, num_frequencies,
+                           (const uint8_t*)(voltage_memory + T_offset), T, 0, Tsize, num_frequencies,
                            num_dishes * num_polarizations, rfi_downsampling_factor, F_stride,
                            offset_encoded, device.getStream(cuda_stream_id));
 #ifdef DEBUGGING
