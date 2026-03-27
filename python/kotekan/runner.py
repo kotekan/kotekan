5ce58c534368943de58544b655d4d59021ea049f
Roland Haas <rhaas@mail.ubc.ca>
2026-03-27 13:51:29 -0700

    Python: remove Python2 support

--- python/kotekan/runner.py
+++ python/kotekan/runner.py
@@ -1,14 +1,5 @@
 """Use Python to run a kotekan instance, particularly for testing.
 """
-
-# === Start Python 2/3 compatibility
-from __future__ import absolute_import, division, print_function, unicode_literals
-from future.builtins import *  # noqa  pylint: disable=W0401, W0614
-from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
-
-# === End Python 2/3 compatibility
-
-
 import itertools
 import json
 import os
