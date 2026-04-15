43de95c0e7e04514d9d557b4279cdbc619a52bdb
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2026-04-15 23:10:26 +0000

    RfiMaskSum in tested.

--- lib/stages/RfiMaskSum.cpp
+++ lib/stages/RfiMaskSum.cpp
@@ -121,10 +121,11 @@ RfiMaskSum::RfiMaskSum(Config& config, const std::string& unique_name,
         // RFI downsampling factor checks
         if (!(_rfi_downsampling_factor > 0))
             FATAL_ERROR("rfi_downsampling_factor is not positive: {:d}", _rfi_downsampling_factor);
-
-        if (!(_rfi_downsampling_factor % 8 == 0))
-            FATAL_ERROR("rfi_downsampling_factor is not a multiple of 8: {:d}",
-                        _rfi_downsampling_factor);
+        /*
+                if (!(_rfi_downsampling_factor % 8 == 0))
+                    FATAL_ERROR("rfi_downsampling_factor is not a multiple of 8: {:d}",
+                                _rfi_downsampling_factor);
+                                */
 
         if (!(_sub_integration_ntime % _rfi_downsampling_factor == 0)) {
             FATAL_ERROR("sub_integration_ntime {} is not a multiple of rfi_downsampling_factor {}",
