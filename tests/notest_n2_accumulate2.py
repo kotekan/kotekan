a952d00619a03d76027c7ec31a77d744a0003fba
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2026-04-29 03:38:38 +0000

    new n2 test almost runs

--- tests/notest_n2_accumulate2.py
+++ tests/notest_n2_accumulate2.py
@@ -10,10 +10,11 @@ prod_config = {
     "num_local_freq": 12,
     "sub_integration_ntime": 8192,
     "num_polarizations": 2,
-    "num_dishes": 512,
+    "num_dishes": 64,
     "num_n2k_samples_to_accumulate": 2,
     "variance_mode": "EvenOddPosDef",
     "num_ev": 0,
+    "num_workers": 1
 }
 
 
@@ -40,6 +41,31 @@ def setup(request):
     )
     request.param["num_frames"] = 2 * config["buffer_depth"]
     return request.param
+
+
+def make_zeroed_chord_buffer(name, dtype, typename, shape, dim_names, seq0, dseq, num_frames, freq_ids=None, time_downsampling=None, extra_meta=None):
+
+    bufs = []
+
+    for idx in range(num_frames):
+        seq = seq0 + idx * dseq
+
+        data = np.zeros(shape, dtype=dtype)
+
+        meta = runner.chordbuffer.get_metadata(name, typename, dim_names)
+        meta['fpga_seq_num'] = seq
+        if freq_ids is not None:
+            meta['coarse_freq'] = freq_ids
+        if time_downsampling is not None:
+            meta['time_downsampling_fpga'] = time_downsampling
+
+        if extra_meta is not None:
+            for key, val in extra_meta.items():
+                meta[key] = val
+
+        bufs.append(ChordBuffer(data, meta))
+
+    return bufs
 
 
 @pytest.fixture(scope="module")
@@ -71,25 +97,15 @@ def corr_data(setup):
     num_int = config["samples_per_data_set"] // config["sub_integration_ntime"]
     num_freq = config["num_local_freq"]
 
-    bufs = []
+    freq_ids = np.arange(num_freq, dtype=np.int32)
 
-    shape = ((num_int, num_freq, num_blocks, blocksize, blocksize, 2),)
+    shape = (num_int, num_freq, num_blocks, blocksize, blocksize, 2)
 
-    for idx in range(num_frames):
-
-        seq_num = idx * config["samples_per_data_set"]
-
-        data = np.empty(*shape, dtype=np.int32)
-
-        meta = runner.chordbuffer.get_metadata(
-            "n2k_correlation", "int32", ("Tc", "F", "DPhi", "DPlo1", "DPlo2", "C")
-        )
-        meta["fpga_seq_num"] = seq_num
-        meta["time_downsampling_fpga"] = config["sub_integration_ntime"]
-
-        data[:, :, :, :, :, :] = 0.0
-
-        bufs.append(ChordBuffer(data, meta))
+    bufs = make_zeroed_chord_buffer("n2k_correlation", np.int32, "int32", shape,
+                                    ("Tc", "F", "DPhi", "DPlo1", "DPlo2", "C"),
+                                    0, config['samples_per_data_set'], setup['num_frames'],
+                                    freq_ids=freq_ids,
+                                    time_downsampling=config['sub_integration_ntime'])
 
     return bufs
 
@@ -123,25 +139,15 @@ def count_data(setup):
     num_int = config["samples_per_data_set"] // config["sub_integration_ntime"]
     num_freq = config["num_local_freq"]
 
-    bufs = []
+    shape = (num_int, num_freq, num_blocks, blocksize, blocksize)
 
-    shape = ((num_int, num_freq, num_blocks, blocksize, blocksize),)
+    bufs = make_zeroed_chord_buffer("n2k_counts", np.int32, "int32", shape,
+                                    ("Tc", "F", "D8Phi", "D8Plo1", "D8Plo2"),
+                                    0, config['samples_per_data_set'], setup['num_frames'],
+                                    time_downsampling=config['sub_integration_ntime'])
 
-    for idx in range(num_frames):
-
-        seq_num = idx * config["samples_per_data_set"]
-
-        data = np.empty(*shape, dtype=np.int32)
-
-        meta = runner.chordbuffer.get_metadata(
-            "n2k_counts", "int32", ("Tc", "F", "D8Phi", "D8Plo1", "D8Plo2")
-        )
-        meta["fpga_seq_num"] = seq_num
-        meta["time_downsampling_fpga"] = config["sub_integration_ntime"]
-
-        data[:, :, :, :, :] = config["sub_integration_ntime"]
-
-        bufs.append(ChordBuffer(data, meta))
+    for buf in bufs:
+        buf.data[...] = config['sub_integration_ntime']
 
     return bufs
 
@@ -167,24 +173,12 @@ def rficount_data(setup):
     num_int = config["samples_per_data_set"] // config["sub_integration_ntime"]
     num_freq = config["num_local_freq"]
 
-    bufs = []
-
-    shape = ((num_int, num_freq),)
-
-    for idx in range(num_frames):
-
-        seq_num = idx * config["samples_per_data_set"]
-
-        data = np.empty(*shape, dtype=np.int32)
-
-        meta = runner.chordbuffer.get_metadata("RFImask_count", "int32", ("Tc", "F"))
-
-        meta["fpga_seq_num"] = seq_num
-        meta["time_downsampling_fpga"] = config["sub_integration_ntime"]
-
-        data[:, :] = 0
-
-        bufs.append(ChordBuffer(data, meta))
+    shape = (num_int, num_freq)
+    
+    bufs = make_zeroed_chord_buffer("RFImask_count", np.int32, "int32", shape,
+                                    ("Tc", "F"),
+                                    0, config['samples_per_data_set'], setup['num_frames'],
+                                    time_downsampling=config['sub_integration_ntime'])
 
     return bufs
 
@@ -210,26 +204,12 @@ def plcount_data(setup):
     num_int = config["samples_per_data_set"] // config["sub_integration_ntime"]
     num_freq = config["num_local_freq"]
 
-    bufs = []
-
-    shape = ((num_int, num_freq),)
-
-    for idx in range(num_frames):
-
-        seq_num = idx * config["samples_per_data_set"]
-
-        data = np.empty(*shape, dtype=np.int32)
-
-        meta = runner.chordbuffer.get_metadata(
-            "pl_lost_counts_scalar", "int32", ("Tc", "F")
-        )
-
-        meta["fpga_seq_num"] = seq_num
-        meta["time_downsampling_fpga"] = config["sub_integration_ntime"]
-
-        data[:, :] = 0
-
-        bufs.append(ChordBuffer(data, meta))
+    shape = (num_int, num_freq)
+    
+    bufs = make_zeroed_chord_buffer("pl_lost_counts_scalar", np.int32, "int32", shape,
+                                    ("Tc", "F"),
+                                    0, config['samples_per_data_set'], setup['num_frames'],
+                                    time_downsampling=config['sub_integration_ntime'])
 
     return bufs
 
@@ -255,23 +235,18 @@ def rfiframemask_data(setup):
     num_int = config["samples_per_data_set"] // config["sub_integration_ntime"]
     num_freq = config["num_local_freq"]
 
-    bufs = []
+    shape = (num_int, num_freq)
+    
+    bufs = make_zeroed_chord_buffer("RFIFrameMask", np.uint8, "uint8", shape,
+                                    ("Tc", "F"),
+                                    0, config['samples_per_data_set'], setup['num_frames'],
+                                    time_downsampling=config['sub_integration_ntime'],
+                                    extra_meta={
+                                        "rfi_frame_excision_enabled": False,
+                                        "rfi_frame_excision_thresholds": np.empty((0,0), dtype=np.float32)})
 
-    shape = ((num_int, num_freq),)
-
-    for idx in range(num_frames):
-
-        seq_num = idx * config["samples_per_data_set"]
-
-        data = np.empty(*shape, dtype=np.uint8)
-
-        meta = runner.chordbuffer.get_metadata("RFIFrameMask", "uint8", ("Tc", "F"))
-        meta["fpga_seq_num"] = seq_num
-        meta["time_downsampling_fpga"] = config["sub_integration_ntime"]
-
-        data[:, :] = 1
-
-        bufs.append(ChordBuffer(data, meta))
+    for buf in bufs:
+        buf.data[...] = 1
 
     return bufs
 
@@ -306,6 +281,7 @@ def accum_data(
     config = setup["config"]
 
     tmpdir = tmpdir_factory.mktemp("n2accum")
+    tmpdir = "test-accum-data"
 
     # Make input buffer and write the files for it to read.
     corr_buffer = runner.ReadChordBuffer(str(tmpdir), corr_data)
@@ -349,55 +325,6 @@ def accum_data(
     test.run()
 
     yield dump_buffer.load()
-
-
-def count_lost_samples(plmask, config):
-    """
-    Count the lost (plmask = 0) samples in the PL mask, assuming values are the same for each element.
-
-    Parameters
-    ----------
-    plmask : ChordBuffer
-        The input PL mask
-    config : dict
-        The base config for the run
-
-    Returns
-    -------
-    ndarray (num_integrations, num_frequency), int32
-        The count of lost (0'd) samples in the PL mask, expanded to the full frequency range, downsampled by sub_integration_ntime in time.
-    """
-
-    # Number of integrations per frame
-    nint = config["samples_per_data_set"] // config["sub_integration_ntime"]
-
-    nf = config["num_local_freq"]
-
-    # Allocate data array
-    lost_counts = np.empty((nint, nf), dtype=np.int32)
-
-    # Loop over integrations
-    for tint in range(nint):
-
-        # Start and stop of sub integration in global time
-        t0 = tint * config["sub_integration_ntime"]
-        t1 = (tint + 1) * config["sub_integration_ntime"]
-
-        # t0 and t1 in the "slow" PL mask time.
-        tpl0 = t0 // 128
-        tpl1 = t1 // 128
-
-        # grab the pl slice for our integtation, count the 1's in each u8, cast to int32s, then
-        # sum over remaining time indices.
-        lost_counts_f4 = config["sub_integration_ntime"] - 2 * np.bitwise_count(
-            plmask.data[tpl0:tpl1, :, 0, 0, :]
-        ).astype(np.int32).sum(axis=(0, 2))
-
-        # place the counts into appropriate frequency bins.
-        for f in range(config["num_local_freq"]):
-            lost_counts[tint, f] = lost_counts_f4[f // 4]
-
-    return lost_counts
 
 
 def test_meta(accum_data, setup):
