7e57c21028cbf5f7e2b18e600a423d8c47ef0736
Roland Haas <rhaas@mail.ubc.ca>
2026-05-08 01:10:16 +0000

    parseReorderDefault: use "common" order names rather legacy ones
    removes impossible to reach check as well

--- lib/stages/parseReorderDefault.hpp
+++ lib/stages/parseReorderDefault.hpp
@@ -39,12 +39,8 @@ public:
 
 private:
 
-    static ElementOrder parseOrderStr(const std::string &ord_str);
-
     Buffer* const _out_buf;
     const std::string _name;
-    const std::string _input_order_str;
-    const std::string _output_order_str;
     const ElementOrder _input_order;
     const ElementOrder _output_order;
     const int _num_polarizations;
