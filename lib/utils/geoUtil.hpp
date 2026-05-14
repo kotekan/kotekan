18de4431e1675dd5ad00747418ea7617fdca3153
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2026-05-14 23:25:11 +0000

    lint

--- lib/utils/geoUtil.hpp
+++ lib/utils/geoUtil.hpp
@@ -1,7 +1,7 @@
 #ifndef GEO_UTIL_HPP
 #define GEO_UTIL_HPP
 
-#include "kotekanLogging.hpp"  // for kotekanLogging
+#include "kotekanLogging.hpp" // for kotekanLogging
 
 #include <array>
 
@@ -10,12 +10,11 @@ using mat3x3d_t = std::array<std::array<double, 3>, 3>;
 
 class GeoFrame : public kotekan::kotekanLogging {
 public:
-    GeoFrame(const std::string& log_level, const std::string& name,
-            double itrs_lat_deg, double itrs_lon_deg,
-            const vec3d_t& offset_m, const vec3d_t& x_axis, const vec3d_t& y_axis, const vec3d_t& z_axis);
+    GeoFrame(const std::string& log_level, const std::string& name, double itrs_lat_deg,
+             double itrs_lon_deg, const vec3d_t& offset_m, const vec3d_t& x_axis,
+             const vec3d_t& y_axis, const vec3d_t& z_axis);
 
 private:
-
     static mat3x3d_t make_R_topo_to_frame(const vec3d_t& x, const vec3d_t& y, const vec3d_t& z);
     static mat3x3d_t make_R_itrs_to_topo(double lat_deg, double lon_deg);
 
@@ -30,7 +29,7 @@ private:
     const mat3x3d_t R_itrs_to_topo;
 };
 
-vec3d_t vec3d_cross(const vec3d_t &a, const vec3d_t &b);
-double vec3d_dot(const vec3d_t &a, const vec3d_t &b);
+vec3d_t vec3d_cross(const vec3d_t& a, const vec3d_t& b);
+double vec3d_dot(const vec3d_t& a, const vec3d_t& b);
 
 #endif
