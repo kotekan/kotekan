81bb2311802328cbf943782ec723bbdcaf411b75
Roland Haas <rhaas@mail.ubc.ca>
2026-05-20 23:21:39 +0000

    SystemInterface: add code to supply a pretent hostname

--- lib/utils/SystemInterface.cpp
+++ lib/utils/SystemInterface.cpp
@@ -1,7 +1,11 @@
 #include "SystemInterface.hpp"
 
+#include <limits.h> // for HOST_NAME_MAX
 #include <string>   // for string, operator+
 #include <unistd.h> // for access, F_OK
+
+// used for testing
+static std::string fake_hostname;
 
 const std::string get_username() {
 
@@ -11,10 +15,20 @@ const std::string get_username() {
     return user;
 }
 
+void set_fake_hostname(const std::string hostname) {
+    fake_hostname = hostname;
+}
+
 const std::string get_hostname() {
 
-    std::string hostname(256, '\0');
-    gethostname(&hostname[0], 256);
+    std::string hostname(HOST_NAME_MAX, '\0');
+    if (fake_hostname.empty()) {
+        gethostname(&hostname[0], hostname.size());
+        hostname.back() = '\0'; // ensure NUL termination even if gethostname failed
+        hostname.resize(hostname.find('\0'));
+    } else {
+        hostname = fake_hostname;
+    }
 
-    return hostname.c_str();
+    return hostname;
 }
