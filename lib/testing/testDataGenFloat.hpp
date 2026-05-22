81e6cd75e36b17aabb919ad8aaf15d16c3cefbda
Roland Haas <rhaas@mail.ubc.ca>
2026-05-22 03:36:43 +0000

    testDataGenFloat: generate other types of floats if requested

--- lib/testing/testDataGenFloat.hpp
+++ lib/testing/testDataGenFloat.hpp
@@ -2,6 +2,7 @@
 #define TEST_DATA_GEN_FLOAT_H
 
 #include "Config.hpp"          // for Config
+#include "DataType.hpp"        // for datatype_t
 #include "Stage.hpp"           // for Stage
 #include "buffer.hpp"          // for Buffer
 #include "bufferContainer.hpp" // for bufferContainer
@@ -34,6 +35,7 @@ private:
     std::vector<int> _array_shape;
     std::vector<std::string> _dim_name;
     std::vector<ptrdiff_t> _dim_scaling;
+    kotekan::DataType _value_type;
     size_t _num_freq_in_frame;
     std::vector<uint32_t> _manual_freq_ids;
     int _meta_time_downsample_factor;
