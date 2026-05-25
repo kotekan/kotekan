0ac0b35d5dee6d48da546a6761f50349885ea019
Geoffrey Ryan <geoffrey.s.ryan@gmail.com>
2026-05-25 18:39:44 +0000

    setFRBBeams produces 2 buffers

--- lib/stages/FRBBeams.hpp
+++ lib/stages/FRBBeams.hpp
@@ -4,13 +4,13 @@
 #include <iostream>
 
 struct FRBBeam {
-    uint32_t idx;
+    uint64_t id;
     double x_dir_grid;
     double y_dir_grid;
 };
 
 inline std::string format_as(const FRBBeam& b) {
-    return fmt::format("FRBBeam(idx: {:d}, x_dir_grid: {:f}, y_dir_grid: {:f})", b.idx, b.x_dir_grid, b.y_dir_grid);
+    return fmt::format("FRBBeam(id: {:d}, x_dir_grid: {:f}, y_dir_grid: {:f})", b.id, b.x_dir_grid, b.y_dir_grid);
 }
 
 inline std::ostream& operator<<(std::ostream& os, const FRBBeam& b) {
@@ -20,13 +20,13 @@ inline std::ostream& operator<<(std::ostream& os, const FRBBeam& b) {
 
 inline void to_json(nlohmann::json& j, const FRBBeam& b) {
     j = {};
-    j.emplace("idx", b.idx);
+    j.emplace("id", b.id);
     j.emplace("x_dir_grid", b.x_dir_grid);
     j.emplace("y_dir_grid", b.y_dir_grid);
 }
 
 inline void from_json(const nlohmann::json& j, FRBBeam& b) {
-    b.idx = j.at("idx");
+    b.id = j.at("id");
     b.x_dir_grid = j.at("x_dir_grid");
     b.y_dir_grid = j.at("y_dir_grid");
 }
