c226bb5dc32e3e1ecf20fcd5aa79313ea37d82bf
Roland Haas <rhaas@mail.ubc.ca>
2026-06-05 23:13:23 +0000

    remove coarse_freq hack

--- lib/stages/hdf5FileReadSingleFile.cpp
+++ lib/stages/hdf5FileReadSingleFile.cpp
@@ -99,8 +99,6 @@ public:
                 dataset.getAttribute("dish_locations_y").read<std::vector<int>>();
 
             auto coarse_freq = dataset.getAttribute("coarse_freq").read<std::vector<int>>();
-            // convert to CHIME frequency id counting f_MHz = 800 - i/1024 * (800 - 400)
-            for(auto & f : coarse_freq) f = 2047 - f;
             const auto freq_upchan_factor =
                 dataset.getAttribute("freq_upchan_factor").read<std::vector<int>>();
             const auto freq_upchan_index =
