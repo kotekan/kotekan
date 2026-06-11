e66ac0886fe0e6988351c5c386e34ab18c463970
Erik Schnetter <schnetter@gmail.com>
2026-06-11 16:25:06 -0400

    Regenerate chimefrb kernels

--- julia/output/chimefrb_chime_U128.cxx
+++ julia/output/chimefrb_chime_U128.cxx
@@ -450,8 +450,8 @@ cudaCHIMEFRBBeamformer_chime_U128::execute(cudaPipelineState& /*pipestate*/,
             I_buffer.set_metadata(Ebar_buffer.get_metadata());
 
         const auto Ebar_meta = Ebar_buffer.get_metadata();
-        assert(Telescope::instance().get_grid_size_x() <= cuda_dish_layout_M);
-        assert(Telescope::instance().get_grid_size_y() <= cuda_dish_layout_N);
+        assert(Telescope::instance().get_grid_size_x() <= cuda_dish_layout_N);
+        assert(Telescope::instance().get_grid_size_y() <= cuda_dish_layout_M);
 
         // Allocate metadata of I buffer only once
         const bool I_has_metadata = I_buffer.has_metadata();
