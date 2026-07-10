8fc2b986f1578793b9dacbb3559759cbb3df006a
liam gray <lgray@phas.ubc.ca>
2026-07-10 00:31:15 +0000

    feat(cuda): re-generated chime frb kernels with updated phase buf params

--- lib/cuda/generated/cudaCHIMEFRBBeamformer_chime_U128.cpp
+++ lib/cuda/generated/cudaCHIMEFRBBeamformer_chime_U128.cpp
@@ -323,7 +323,7 @@ cudaCHIMEFRBBeamformer_chime_U128::cudaCHIMEFRBBeamformer_chime_U128(Config& con
     info_name(unique_name + "/gpu_mem_info"),
 
     W_buffer(W_name, W_quantity, reverse(W_lengths), reverse(W_labels), reverse(W_dimscalings),
-             *this, buffer_type_t::do_once),
+             *this),
     Ebar_buffer(Ebar_name, Ebar_quantity, reverse(Ebar_lengths), reverse(Ebar_labels),
                 reverse(Ebar_dimscalings), *this),
     I_buffer(I_name, I_quantity, reverse(I_lengths), reverse(I_labels), reverse(I_dimscalings),
