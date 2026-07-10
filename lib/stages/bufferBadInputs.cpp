5ee92c1f23416a80d1f1b9d59f0414c0cca88cbf
Liam Gray <lgray@phas.ubc.ca>
2026-07-10 12:48:30 -0700

    feat(bufferBadInputs): produce kotekan buffers at a regular cadence

--- lib/stages/bufferBadInputs.cpp
+++ lib/stages/bufferBadInputs.cpp
@@ -1,6 +1,7 @@
 #include "bufferBadInputs.hpp"
 
 #include "Config.hpp"         // for Config
+#include "N2Util.hpp"         // for frameID
 #include "StageFactory.hpp"   // for REGISTER_KOTEKAN_STAGE
 #include "Telescope.hpp"      // for Telescope, station_id_t
 #include "buffer.hpp"         // for Buffer
@@ -12,7 +13,7 @@
 #include <exception>  // for exception
 #include <functional> // for bind, function, _1
 #include <json.hpp>   // for json
-#include <memory>     // for __shared_ptr_access, shared_ptr
+#include <mutex>      // for lock_guard, mutex
 #include <stdint.h>   // for uint8_t
 
 using kotekan::bufferContainer;
@@ -25,9 +26,7 @@ REGISTER_KOTEKAN_STAGE(bufferBadInputs);
 
 bufferBadInputs::bufferBadInputs(Config& config_, const std::string& unique_name,
                                  bufferContainer& buffer_container) :
-    Stage(config_, unique_name, buffer_container, std::bind(&bufferBadInputs::main_thread, this)),
-    // Required since `frame_id` is used outside of the main loop
-    frame_id(get_buffer("out_buf")) {
+    Stage(config_, unique_name, buffer_container, std::bind(&bufferBadInputs::main_thread, this)) {
 
     num_elements = config.get<size_t>(unique_name, "num_elements");
 
@@ -38,6 +37,9 @@ bufferBadInputs::bufferBadInputs(Config& config_, const std::string& unique_name
     // reorder[beamformer_idx] = cylinder_idx;
     reorder.resize(num_elements);
 
+    // initialize the mask (1 == good)
+    input_mask = std::vector<uint8_t>(num_elements, 1u);
+
     const Telescope& tel = Telescope::instance();
 
     for (size_t beamformer_idx = 0; beamformer_idx < num_elements; ++beamformer_idx) {
@@ -46,19 +48,24 @@ bufferBadInputs::bufferBadInputs(Config& config_, const std::string& unique_name
         reorder.at(beamformer_idx) =
             tel.station_id_to_element_index(st_id, ElementOrder::CHIMECylinder);
     }
+
+    // Listen for bad input list updates
+    std::string badInputs = config.get<std::string>(unique_name, "updatable_config/bad_inputs");
+    configUpdater::instance().subscribe(
+        badInputs,
+        std::bind(&bufferBadInputs::update_bad_inputs_callback, this, std::placeholders::_1));
 }
 
 bufferBadInputs::~bufferBadInputs() {}
 
 bool bufferBadInputs::update_bad_inputs_callback(nlohmann::json& json) {
-    DEBUG("update_bad_inputs_callback(): Update to bad inputs list.");
+    INFO("update_bad_inputs_callback(): Received update to bad inputs list.");
 
-    // Get the next output frame
-    uint8_t* host_mask = (uint8_t*)out_buf->wait_for_empty_frame(unique_name, frame_id);
+    // hold lock for the entire update
+    std::lock_guard<std::mutex> lock(mtx);
+
     // Reset the mask (1 == good)
-    std::memset(host_mask, 1U, num_elements);
-
-    bool all_good = true;
+    std::fill(input_mask.begin(), input_mask.end(), 1u);
 
     try {
         bad_inputs = json["bad_inputs"].get<std::vector<int>>();
@@ -67,34 +74,45 @@ bool bufferBadInputs::update_bad_inputs_callback(nlohmann::json& json) {
         return false;
     }
 
-    // Add current bad input to the mask
+    // validate all inputs before changing the mask
     for (int element : bad_inputs) {
-        if (element < (int)num_elements && element >= 0) {
-            host_mask[reorder[element]] = 0;
-        } else {
-            ERROR("Got input with invalid index: {:d}", element);
-            all_good = false;
+        if (element >= (int)num_elements || element < 0) {
+            ERROR("Received input with invalid index: {:d}", element);
+            return false;
         }
     }
 
-    // Create new metadata
-    out_buf->allocate_new_metadata_object(frame_id);
-    // Set no. of bad inputs in the metadata
-    get_chord_metadata(out_buf, frame_id)->set_rfi_num_bad_inputs(bad_inputs.size());
-
-    out_buf->mark_frame_full(unique_name, frame_id);
+    // now update the mask
+    for (int element : bad_inputs) {
+        input_mask[reorder[element]] = 0;
+    }
+    num_bad_inputs = bad_inputs.size();
 
     DEBUG("update_bad_inputs_callback(): Bad inputs reordered and buffered.");
 
-    frame_id++;
-
-    return all_good;
+    return true;
 }
 
 void bufferBadInputs::main_thread() {
-    // Listen for bad input list updates
-    std::string badInputs = config.get<std::string>(unique_name, "updatable_config/bad_inputs");
-    configUpdater::instance().subscribe(
-        badInputs,
-        std::bind(&bufferBadInputs::update_bad_inputs_callback, this, std::placeholders::_1));
+    N2::frameID frame_id(out_buf);
+
+    while (!stop_thread) {
+        // get an output frame
+        uint8_t* out_frame = (uint8_t*)out_buf->wait_for_empty_frame(unique_name, frame_id);
+        if (out_frame == nullptr) {
+            return;
+        }
+
+        // Copy from the permanent buffer
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::copy_n(input_mask.begin(), num_elements, out_frame);
+        }
+
+        // Set metadata and release
+        out_buf->allocate_new_metadata_object(frame_id);
+        get_chord_metadata(out_buf, frame_id)->set_rfi_num_bad_inputs(num_bad_inputs);
+        out_buf->mark_frame_full(unique_name, frame_id);
+        frame_id++;
+    }
 }
