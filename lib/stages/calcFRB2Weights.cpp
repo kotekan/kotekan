f0ea83e12051dfb8e7e55d267e93adf57a2f3166
Roland Haas <rhaas@mail.ubc.ca>
2026-07-10 17:06:31 -0700

    calcFRB2Weights: set upchan frequencies based on voltage data

--- lib/stages/calcFRB2Weights.cpp
+++ lib/stages/calcFRB2Weights.cpp
@@ -83,6 +83,7 @@ class calcFRB2Weights : public kotekan::Stage {
 
     Buffer* const frb2_beam_positions_buffer;
     Buffer* const W2_buffer;
+    Buffer* const metadata_buffer;
 
 public:
     calcFRB2Weights(kotekan::Config& config, const std::string& unique_name,
@@ -92,7 +93,8 @@ public:
                   return const_cast<kotekan::Stage&>(stage).main_thread();
               }),
         frb2_beam_positions_buffer(get_buffer("frb2_beam_positions")),
-        W2_buffer(get_buffer("frb2_weights"))
+        W2_buffer(get_buffer("frb2_weights")),
+        metadata_buffer(get_buffer("metadata"))
     //
     {
         assert(frb2_beam_positions_buffer);
@@ -101,6 +103,7 @@ public:
             FATAL_ERROR("num_threads %d must be positive", num_threads);
         frb2_beam_positions_buffer->register_consumer(unique_name);
         W2_buffer->register_producer(unique_name);
+        metadata_buffer->register_consumer(unique_name);
 
         frb2_beam_positions_buffer->require_frame_desc(kotekan::NDArray<float, 2>::describe(
             "frb2_beam_positions", {frb2_num_beams, 2}, {"R", "X/Y"}, {1, 1}));
@@ -123,8 +126,15 @@ public:
         const Telescope& telescope = Telescope::instance();
 
         // Upchannelization schedule
-        const auto& upchan_schedule =
-            UpchannelizationSchedule::instance(config, upchannelization_schedule_name);
+        uint8_t const * const metadata_frame = metadata_buffer->wait_for_full_frame(unique_name, 0);
+        if(metadata_frame == nullptr)
+            return;
+        const auto& upchan_schedule = *[&]() {
+            const auto coarse_freq = get_chord_metadata(metadata_buffer, 0)->get_coarse_freq();
+            return &UpchannelizationSchedule::instance(config, upchannelization_schedule_name, coarse_freq);
+        }();
+        metadata_buffer->mark_frame_empty(unique_name, 0);
+        metadata_buffer->unregister_consumer(unique_name);
 
         // Calculate frequencies
         const auto& frequency_channels = upchan_schedule.get_frequency_channels();
