ea79ee5e96eb2f133ed2467171b5d330cc984078
Erik Schnetter <schnetter@gmail.com>
2026-08-11 19:23:54 -0400

    inventBFMask: Create bad feed masks continuously, not just once

--- lib/testing/inventBFMask.cpp
+++ lib/testing/inventBFMask.cpp
@@ -31,7 +31,8 @@
 class inventBFMask : public kotekan::Stage {
     const int num_dishes = config.get<int>(unique_name, "num_dishes");
     const int num_polarizations = config.get<int>(unique_name, "num_polarizations");
-    const int num_times = config.get<int>(unique_name, "num_times");
+    const std::int64_t bf_mask_lifetime_in_samples =
+        config.get<std::int64_t>(unique_name, "bf_mask_lifetime_in_samples");
     const std::string mode = config.get_default<std::string>(unique_name, "mode", "all_good");
     const std::vector<int> manual_bad_feeds =
         config.get_default<std::vector<int>>(unique_name, "manual_bad_feeds", {});
@@ -49,73 +50,91 @@ public:
         assert(buffer);
         buffer->register_producer(unique_name);
 
-        if ((mode != "all_good") && (mode != "all_bad") && (mode != "auto") && (mode != "manual")) {
+        if (!(mode == "all_good" || mode == "all_bad" || mode == "auto" || mode == "manual"))
             FATAL_ERROR("Bad mode: {:s}", mode);
-        }
     }
 
     virtual ~inventBFMask() {}
 
     void main_thread() override {
-        // Only invent a single frame
-        const int frame_index = 0;
-        const int frame_id = frame_index % buffer->num_frames;
+        for (int frame_index = 0;; ++frame_index) {
+            const int frame_id = frame_index % buffer->num_frames;
+            if (stop_thread)
+                return;
 
-        if (stop_thread)
-            return;
+            // Wait for buffer
+            DEBUG("[{:s}/{:d}] Waiting for buffer...", buffer->buffer_name, frame_index);
+            std::int8_t* const frame = static_cast<std::int8_t*>(
+                static_cast<void*>(buffer->wait_for_empty_frame(unique_name, frame_id)));
+            if (!frame)
+                return;
 
-        // Wait for buffer
-        DEBUG("[{:s}/{:d}] Waiting for buffer...", buffer->buffer_name, frame_index);
-        std::int8_t* const frame = static_cast<std::int8_t*>(
-            static_cast<void*>(buffer->wait_for_empty_frame(unique_name, frame_id)));
-        if (!frame)
-            return;
+            // Set metadata
+            buffer->require_frame_desc(kotekan::NDArray<std::int8_t, 3>::describe(
+                "bf_mask", {1, num_polarizations, num_dishes}, {"Tbf", "P", "D"},
+                {bf_mask_lifetime_in_samples, 1, 1}));
+            buffer->allocate_new_metadata_object(frame_id);
+            const auto& meta = get_chord_metadata(buffer->get_metadata(frame_id));
+            meta->set_from_frame_desc(buffer->get_frame_desc<kotekan::GenericNDArray>());
+            meta->set_fpga_seq_num(frame_index * bf_mask_lifetime_in_samples);
+            meta->set_time_downsampling_fpga(bf_mask_lifetime_in_samples);
 
-        // Set metadata
-        buffer->require_frame_desc(kotekan::NDArray<std::int8_t, 2>::describe(
-            "bf_mask", {num_polarizations, num_dishes}, {"P", "D"}, {1, 1}));
-        buffer->allocate_new_metadata_object(frame_id);
-        const auto& meta = get_chord_metadata(buffer->get_metadata(frame_id));
-        meta->set_from_frame_desc(buffer->get_frame_desc<kotekan::GenericNDArray>());
-        meta->set_fpga_seq_num(frame_index * num_times);
-        meta->set_time_downsampling_fpga(1);
+            const auto& tel = Telescope::instance().cast<CHORDTelescope>();
 
-        const Telescope& tel = Telescope::instance();
+            // Fill buffer
+            DEBUG("[{:s}/{:d}] Filling buffer...", buffer->buffer_name, frame_index);
+            if (mode == "all_good") {
 
-        // Fill buffer
-        DEBUG("[{:s}/{:d}] Filling buffer...", buffer->buffer_name, frame_index);
-        for (int polr = 0; polr < num_polarizations; ++polr) {
-            for (int dish = 0; dish < num_dishes; ++dish) {
-                const int idx = dish + num_dishes * polr;
-                assert(idx >= 0 && idx < std::ptrdiff_t(buffer->frame_size));
-                if (mode == "all_good")
-                    frame[idx] = 1; // dish is active
-                else if (mode == "all_bad")
-                    frame[idx] = 0; // dish is not active
-                else if (mode == "auto") {
-                    if (tel.cast<CHORDTelescope>().get_dish_at_idx(dish).type
-                        == DishType::ArrayDish)
-                        frame[idx] = 1;
-                    else
-                        frame[idx] = 0;
-                } else
-                    frame[idx] = 1;
-            }
-
-            if (mode == "manual") {
-                for (size_t j = 0; j < manual_bad_feeds.size(); j++) {
-                    frame[manual_bad_feeds.at(j)] = 0;
+                for (int polr = 0; polr < num_polarizations; ++polr) {
+                    for (int dish = 0; dish < num_dishes; ++dish) {
+                        const int idx = dish + num_dishes * polr;
+                        assert(idx >= 0 && idx < std::ptrdiff_t(buffer->frame_size));
+                        frame[idx] = 1; // dish is active
+                    }
                 }
+
+            } else if (mode == "all_bad") {
+
+                for (int polr = 0; polr < num_polarizations; ++polr) {
+                    for (int dish = 0; dish < num_dishes; ++dish) {
+                        const int idx = dish + num_dishes * polr;
+                        assert(idx >= 0 && idx < std::ptrdiff_t(buffer->frame_size));
+                        frame[idx] = 0; // dish is not active
+                    }
+                }
+
+            } else if (mode == "auto") {
+
+                for (int polr = 0; polr < num_polarizations; ++polr) {
+                    for (int dish = 0; dish < num_dishes; ++dish) {
+                        const int idx = dish + num_dishes * polr;
+                        assert(idx >= 0 && idx < std::ptrdiff_t(buffer->frame_size));
+                        // existing dishes are active, missing dishes are inactive
+                        frame[idx] = tel.get_dish_at_idx(dish).type == DishType::ArrayDish;
+                    }
+                }
+
+            } else if (mode == "manual") {
+
+                for (int polr = 0; polr < num_polarizations; ++polr) {
+                    for (int dish = 0; dish < num_dishes; ++dish) {
+                        const int idx = dish + num_dishes * polr;
+                        frame[idx] = 1;
+                    }
+                }
+                for (const auto feed : manual_bad_feeds) {
+                    assert(feed >= 0 && feed < std::ptrdiff_t(buffer->frame_size));
+                    frame[feed] = 0;
+                }
+
+            } else {
+                std::abort();
             }
+
+            // Mark buffer as full
+            DEBUG("[{:s}/{:d}] Marking buffer as full...", buffer->buffer_name, frame_index);
+            buffer->mark_frame_full(unique_name, frame_id);
         }
-
-        // Mark buffer as full
-        DEBUG("[{:s}/{:d}] Marking buffer as full...", buffer->buffer_name, frame_index);
-        buffer->mark_frame_full(unique_name, frame_id);
-
-        // Wait for shutdown (don't trigger a shutdown)
-        while (!stop_thread)
-            sleep(1);
     }
 };
 
