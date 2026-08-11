f9d47c1e6b0050bd8cc4f8844497f48652d719c2
Roland Haas <rhaas@mail.ubc.ca>
2026-08-11 14:37:59 -0700

    UpchannelizationSchedule: make coarse_freq input required

--- lib/stages/UpchannelizationSchedule.hpp
+++ lib/stages/UpchannelizationSchedule.hpp
@@ -51,7 +51,6 @@ class UpchannelizationSchedule : kotekan::kotekanLogging {
 
     ////////////////////////////////////////////////////////////////////////////////
 
-    std::vector<int> make_frequency_channels() const;
     std::map<int, int> make_frequency_channels_to_indices() const;
 
     std::vector<int> make_upchan_factors() const;
@@ -68,8 +67,8 @@ public:
     UpchannelizationSchedule& operator=(const UpchannelizationSchedule&) = delete;
     UpchannelizationSchedule& operator=(UpchannelizationSchedule&&) = delete;
 
-    UpchannelizationSchedule(kotekan::Config& config, const std::string& unique_name = "",
-                             const std::vector<int>& coarse_freq = std::vector<int>());
+    UpchannelizationSchedule(kotekan::Config& config, const std::string& unique_name,
+                             const std::vector<int>& coarse_freq);
 
     // Get the singleton instance
     static const UpchannelizationSchedule&
