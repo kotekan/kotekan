d78e160573a82599e99dcf723784b3bbcdb4afea
James Mertens <jbcm627@gmail.com>
2026-09-10 09:33:50 -0400

    testDataGen: per-instance timestamp; gains stages: require_frame_desc (#1648)

--- lib/stages/processFRBFeedGains.cpp
+++ lib/stages/processFRBFeedGains.cpp
@@ -41,7 +41,7 @@ void processFRBFeedGains::copy_upchannelize_f(const float* src_f, float16_t* dst
 }
 
 void processFRBFeedGains::set_frame_desc(Buffer* buf) {
-    buf->ensure_frame_desc(kotekan::NDArray<kotekan::GetType_t<kotekan::float16>, 5>::describe(
+    buf->require_frame_desc(kotekan::NDArray<kotekan::GetType_t<kotekan::float16>, 5>::describe(
         "W",
         {static_cast<ptrdiff_t>(num_local_freq * upchan_factor),
          static_cast<ptrdiff_t>(num_polarizations), static_cast<ptrdiff_t>(num_dishes_N),
